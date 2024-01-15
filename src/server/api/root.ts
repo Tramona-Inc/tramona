1250a62e442d2e7cb44bff4d1e1854bb1f52642a
Ben Tomlin <bentomlin101@gmail.com>
2024-01-15 04:49:34 -0800

    get all outgoing requests endpoint

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { usersRouter } from "./routers/usersRouter";
+import { requestsRouter } from "./routers/requestsRouter";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { usersRouter } from "./routers/usersRouter";
  */
 export const appRouter = createTRPCRouter({
   users: usersRouter,
+  requests: requestsRouter,
 });
 
 // export type definition of API
