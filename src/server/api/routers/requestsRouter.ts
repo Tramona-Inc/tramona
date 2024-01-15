c1cad2eef63e1a3caba2791120d9723b6b90f135
Ben Tomlin <bentomlin101@gmail.com>
2024-01-15 11:18:36 -0800

    slight refactor

--- src/server/api/routers/requestsRouter.ts
+++ src/server/api/routers/requestsRouter.ts
@@ -1,30 +1,27 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { requests, users } from "@/server/db/schema";
 import { TRPCError } from "@trpc/server";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const requestsRouter = createTRPCRouter({
   getAllOutgoing: protectedProcedure.query(async ({ ctx }) => {
-    const res = await ctx.db.query.users.findFirst({
-      where: eq(users.id, ctx.user.id),
-      columns: {},
-      with: {
-        requestsMade: {
-          columns: {
-            userId: false,
-          },
-          with: {
-            offers: {
-              columns: {},
-              with: {
-                property: {
-                  columns: {},
-                  with: {
-                    host: {
-                      columns: {
-                        image: true,
-                      },
+    const requestsMade = await ctx.db.query.requests
+      .findMany({
+        where: eq(requests.userId, ctx.user.id),
+        columns: {
+          userId: false,
+        },
+        with: {
+          offers: {
+            columns: {},
+            with: {
+              property: {
+                columns: {},
+                with: {
+                  host: {
+                    columns: {
+                      image: true,
                     },
                   },
                 },
@@ -32,25 +29,31 @@ export const requestsRouter = createTRPCRouter({
             },
           },
         },
-      },
-    });
+      })
+      .then((res) =>
+        res.map((request) => {
+          const hostImages = request.offers
+            .map((offer) => offer.property.host?.image)
+            .filter(Boolean);
 
-    const requestsMade = (res?.requestsMade ?? []).map((request) => {
-      const hostImages = request.offers
-        .map((offer) => offer.property.host?.image)
-        .filter(Boolean);
+          const numOffers = request.offers.length;
 
-      const numOffers = request.offers.length;
+          const { offers: _, ...requestExceptOffers } = request;
 
-      const { offers: _, ...requestExceptOffers } = request;
-
-      return { ...requestExceptOffers, hostImages, numOffers };
-    });
+          return { ...requestExceptOffers, hostImages, numOffers };
+        }),
+      );
 
     return {
       requestsMade,
     };
   }),
+
+  // getAllIncoming: roleRestrictedProcedure(["host", "admin"]).query(
+  //   async ({ ctx, input }) => {
+  //     // get the requests close to this users properties
+  //   },
+  // ),
 
   create: protectedProcedure
     .input(
