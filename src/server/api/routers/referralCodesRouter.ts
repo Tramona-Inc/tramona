6827ba6fcc77ddfa5204c7209d9c7102a8d9efaa
Ben Tomlin <bentomlin101@gmail.com>
2024-01-15 10:10:56 -0800

    start/stop using code

---
+++ src/server/api/routers/referralCodesRouter.ts
@@ -0,0 +1,76 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { referralCodes, users } from "@/server/db/schema";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+import { createSelectSchema } from "drizzle-zod";
+
+export const referralCodesRouter = createTRPCRouter({
+  startUsingCode: protectedProcedure
+    .input(
+      createSelectSchema(referralCodes).pick({
+        code: true,
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userDetails = await ctx.db.query.users.findFirst({
+        where: eq(users.id, ctx.user.id),
+        columns: {
+          referralCodeUsed: true,
+        },
+      });
+
+      if (userDetails?.referralCodeUsed) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You already used a referral code",
+        });
+      }
+
+      const code = await ctx.db.query.referralCodes.findFirst({
+        where: eq(referralCodes.code, input.code),
+        columns: {
+          ownerId: true,
+        },
+      });
+
+      if (!code) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid referral code",
+        });
+      }
+
+      const referralCodeOwner = await ctx.db.query.users.findFirst({
+        where: eq(users.id, code.ownerId),
+        columns: {
+          name: true,
+          email: true,
+        },
+      });
+
+      return {
+        codeOwnerName: referralCodeOwner?.name ?? "an anonymous person",
+      };
+    }),
+
+  stopUsingCode: protectedProcedure.query(async ({ ctx }) => {
+    const userDetails = await ctx.db.query.users.findFirst({
+      where: eq(users.id, ctx.user.id),
+      columns: {
+        referralCodeUsed: true,
+      },
+    });
+
+    if (!userDetails?.referralCodeUsed) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "You don't have a referral code",
+      });
+    }
+
+    await ctx.db
+      .update(users)
+      .set({ referralCodeUsed: null })
+      .where(eq(users.id, ctx.user.id));
+  }),
+});
