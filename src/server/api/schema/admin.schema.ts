0624e9f4dc5c5243c270d0b0466cd53afa5a01ff
zachuri <punsalang.zachary@gmail.com>
2024-01-16 20:35:07 -0800

    admin: general setup with dialog and form

---
+++ src/server/api/schema/admin.schema.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const createRequestInputSchema = z.object({
+  max_preferred_price: z.number().min(1),
+  location: z.string().min(1),
+  check_in: z.date(),
+  check_out: z.date(),
+  num_guests: z.number().default(1),
+  min_num_bed: z.number().default(1),
+  min_num_bedrooms: z.number().default(1),
+  property_type: z.number().default(1),
+  note: z.string(),
+});
+
+export type CreateRequestInput = z.TypeOf<typeof createRequestInputSchema>;
