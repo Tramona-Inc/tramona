e71ac23f3ca189929bf28738012556803446ddb5
zachuri <punsalang.zachary@gmail.com>
2024-01-16 23:58:20 -0800

    feature/auth: setup for the admin page to set a request/ need to fix createRequestRouter

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -23,12 +23,32 @@ import { api } from "@/utils/api";
 import { useForm } from "react-hook-form";
 import { type z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { cn } from "@/utils/utils";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { Badge } from "@/components/ui/badge";
 
 export default function Page() {
   const form = useForm<z.infer<typeof createRequestInputSchema>>({
     resolver: zodResolver(createRequestInputSchema),
     defaultValues: {
-      max_preferred_price: 0,
+      max_preferred_price: 1,
+      location: "",
+      num_guests: 1,
       min_num_bed: 1,
       min_num_bedrooms: 1,
       note: "",
@@ -45,21 +65,28 @@ export default function Page() {
         <p className="p-8">admin dashboard</p>
         <Dialog>
           <DialogTrigger asChild>
-            <Button>Open</Button>
+            <Button>+ Create a request</Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>Add Request</DialogTitle>
-              <DialogDescription>
-                Add personal request for yourself (Admin).
-              </DialogDescription>
+              <DialogTitle>
+                Request
+                <Badge
+                  variant="secondary"
+                  size="sm"
+                  className="ml-2 -translate-y-0.5 uppercase"
+                >
+                  Admin
+                </Badge>
+              </DialogTitle>
+              <DialogDescription>Add personal request</DialogDescription>
             </DialogHeader>
 
             {/* Ensure that the Form component wraps the relevant content */}
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-4"
+                className="mt-5 space-y-5"
               >
                 <FormField
                   control={form.control}
@@ -69,9 +96,12 @@ export default function Page() {
                       <FormLabel>Your max preferred price</FormLabel>
                       <FormControl>
                         <Input
-                          placeholder="shadcn"
+                          placeholder="120"
                           type={"number"}
                           {...field}
+                          onChange={(e) => {
+                            field.onChange(parseInt(e.target.value));
+                          }}
                         />
                       </FormControl>
                       <FormMessage />
@@ -81,17 +111,165 @@ export default function Page() {
 
                 <FormField
                   control={form.control}
-                  name="property_type"
+                  name="location"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Property Type</FormLabel>
+                      <FormLabel>Location</FormLabel>
                       <FormControl>
-                        <Input placeholder="shadcn" {...field} />
+                        <Input placeholder="Dubai" type="text" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
+
+                <section className="flex flex-col gap-5 md:flex-row">
+                  <FormField
+                    control={form.control}
+                    name="check_in"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Check-in date</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-[240px] pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) => date < new Date()}
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="check_out"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Check-in out</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-[240px] pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) => date < new Date()}
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </section>
+
+                <section className="flex flex-row gap-5">
+                  <FormField
+                    control={form.control}
+                    name="num_guests"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Guests</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="1"
+                            type={"number"}
+                            min={1}
+                            {...field}
+                            onChange={(e) => {
+                              field.onChange(parseInt(e.target.value));
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="min_num_bed"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Beds</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="1"
+                            type={"number"}
+                            min={1}
+                            {...field}
+                            onChange={(e) => {
+                              field.onChange(parseInt(e.target.value));
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="min_num_bedrooms"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Bedrooms</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="1"
+                            type={"number"}
+                            min={1}
+                            {...field}
+                            onChange={(e) => {
+                              field.onChange(parseInt(e.target.value));
+                            }}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </section>
 
                 <FormField
                   control={form.control}
@@ -100,12 +278,40 @@ export default function Page() {
                     <FormItem>
                       <FormLabel>Property Type</FormLabel>
                       <FormControl>
-                        <Input placeholder="shadcn" {...field} />
+                        <Select onValueChange={field.onChange}>
+                          <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Select a type" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="townhouse">Townhouse</SelectItem>
+                            <SelectItem value="apartment">Apartment</SelectItem>
+                            <SelectItem value="house">House</SelectItem>
+                          </SelectContent>
+                        </Select>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
+
+                <FormField
+                  control={form.control}
+                  name="note"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Note</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Any notes about the property"
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
                 <Button type="submit">Submit</Button>
               </form>
             </Form>
--- src/server/api/routers/adminRouter.ts
+++ src/server/api/routers/adminRouter.ts
@@ -1,4 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { createRequestInputSchema } from "../schema/admin.schema";
+import { requests } from "@/server/db/schema/tables/requests";
 
 export const adminRouter = createTRPCRouter({
   getRequests: protectedProcedure.query(async ({ ctx }) => {
@@ -6,4 +8,20 @@ export const adminRouter = createTRPCRouter({
       orderBy: (requests, { desc }) => [desc(requests.createdAt)],
     });
   }),
+
+  createRequest: protectedProcedure
+    .input(createRequestInputSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(requests).values({
+        userId: ctx.user.id,
+        maxPreferredPrice: input.max_preferred_price,
+        location: input.location,
+        checkIn: input.check_in,
+        checkOut: input.check_out,
+        numGuests: input.num_guests,
+        propertyType: input.property_type,
+        note: input.note,
+        resolvedAt: null,
+      });
+    }),
 });
