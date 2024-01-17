e879bcc1ceda24fe17480815403ca48d9d22b61a
czearing <czearing@outlook.com>
2024-01-16 21:07:35 -0800

    Quick fix

--- src/components/Header/AvatarDropdown.tsx
+++ src/components/Header/AvatarDropdown.tsx
@@ -11,6 +11,35 @@ import { type Session } from 'next-auth';
 import Link from 'next/link';
 import { Badge } from '../ui/badge';
 
+function DropdownTop({ session }: { session: Session }) {
+  const title = session.user.name ?? session.user.email ?? 'Anonymous';
+  const subtitle = session.user.name ? session.user.email : null;
+
+  return (
+    <div className="pb-1 pl-3">
+      <div className="font-medium">
+        {title}
+
+        <Badge variant="secondary" size="sm" className="ml-2 -translate-y-0.5 uppercase">
+          {session.user.role}
+        </Badge>
+      </div>
+
+      {subtitle && <p className="text-sm text-zinc-600">{subtitle}</p>}
+    </div>
+  );
+}
+
+function DropdownLink({ children, href }: React.PropsWithChildren<{ href: string }>) {
+  return (
+    <DropdownMenuItem>
+      <Link href={href} className="flex w-full items-center gap-2 py-2 pl-3">
+        {children}
+      </Link>
+    </DropdownMenuItem>
+  );
+}
+
 export default function AvatarDropdown({ session }: { session: Session }) {
   return (
     <DropdownMenu>
@@ -46,34 +75,5 @@ export default function AvatarDropdown({ session }: { session: Session }) {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  );
-}
-
-function DropdownTop({ session }: { session: Session }) {
-  const title = session.user.name ?? session.user.email ?? 'Anonymous';
-  const subtitle = session.user.name ? session.user.email : null;
-
-  return (
-    <div className="pb-1 pl-3">
-      <div className="font-medium">
-        {title}
-
-        <Badge variant="secondary" size="sm" className="ml-2 -translate-y-0.5 uppercase">
-          {session.user.role}
-        </Badge>
-      </div>
-
-      {subtitle && <p className="text-sm text-zinc-600">{subtitle}</p>}
-    </div>
-  );
-}
-
-function DropdownLink({ children, href }: React.PropsWithChildren<{ href: string }>) {
-  return (
-    <DropdownMenuItem>
-      <Link href={href} className="flex w-full items-center gap-2 py-2 pl-3">
-        {children}
-      </Link>
-    </DropdownMenuItem>
   );
 }
