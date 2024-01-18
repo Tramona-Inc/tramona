d265592c5d043bfd8f74f73f5b09d456194e2614
Ben Tomlin <bentomlin101@gmail.com>
2024-01-18 13:14:31 -0800

    migrate New Request form

--- src/pages/requests/index.tsx
+++ src/pages/requests/index.tsx
@@ -1,3 +1,4 @@
+import NewRequestForm from "@/components/requests/NewRequestForm";
 import { requireAuth } from "@/server/auth";
 import Head from "next/head";
 
@@ -10,6 +11,7 @@ export default function Page() {
         <title>Your Requests | Tramona</title>
       </Head>
       <p className="p-8">your requests</p>
+      <NewRequestForm />
     </>
   );
 }
