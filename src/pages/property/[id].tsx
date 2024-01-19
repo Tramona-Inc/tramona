41a2fdf2ac635bf10ee221e770473cb066c2bf59
zachuri <punsalang.zachary@gmail.com>
2024-01-18 18:21:21 -0800

    feature/admin-offer: able to display properties

--- src/pages/property/[id].tsx
+++ src/pages/property/[id].tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { api } from "@/utils/api";
+import { preconnect } from "react-dom";
 
 export default function Page() {
   const router = useRouter();
-  return <p>Property ID: {router.query.id}</p>;
+
+  const propertyId = parseInt(router.query.id as string);
+
+  const { data: property } = api.properties.getById.useQuery({
+    id: propertyId,
+  });
+
+  return property ? (
+    <div>
+      <h1>{property.hostName}</h1>
+      <p>{property.name}</p>
+    </div>
+  ) : (
+    <h1>Property doesn&apos;t exist</h1>
+  );
 }
