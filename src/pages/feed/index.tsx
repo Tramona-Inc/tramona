3bc48812956769367abe5c895e5bcb4d9836fbd5
Manas Acharekar <manas.acharekar98@gmail.com>
2024-01-26 14:19:32 -0500

    feed: handle no offers returned

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,4 +1,5 @@
 import FeedCard from "@/components/feed/feed-card";
+import { Button } from "@/components/ui/button";
 import { api } from "@/utils/api";
 import Head from "next/head";
 
@@ -9,16 +10,24 @@ export default function Dashboard() {
     <>
       <Head>Tramona - Feed</Head>
       <section className="container my-10">
-        <h1>Social Feed</h1>
-        <div className="grid gap-5 md:grid-cols-2">
-          {offers ? (
-            offers.map((offer) => {
-              return <FeedCard key={offer.id} offer={offer} />;
-            })
-          ) : (
-            <h1>No public</h1>
-          )}
-        </div>
+        <h1 className="pb-5 text-4xl font-bold lg:pb-10 lg:text-5xl">
+          Social Feed
+        </h1>
+        {isLoading ? (
+          <Button variant="ghost" isLoading disabled>
+            Loading...
+          </Button>
+        ) : (
+          <div className="grid gap-5 md:grid-cols-2">
+            {offers?.length ? (
+              offers.map((offer) => {
+                return <FeedCard key={offer.id} offer={offer} />;
+              })
+            ) : (
+              <h2 className="text-xl">No offers to show</h2>
+            )}
+          </div>
+        )}
       </section>
     </>
   );
