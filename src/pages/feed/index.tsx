8a03d9e66a27d7afae56574000f760f5b2893229
Manas Acharekar <manas.acharekar98@gmail.com>
2024-02-02 17:38:55 -0500

    feed: update feed cards with new design

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,24 +1,30 @@
+import Head from "next/head";
+
 import FeedCard from "@/components/feed/feed-card";
 import { Button } from "@/components/ui/button";
+
 import { api } from "@/utils/api";
-import Head from "next/head";
 
 export default function Dashboard() {
   const { data: offers, isLoading } = api.offers.getAllOffers.useQuery();
 
   return (
     <>
-      <Head>Tramona - Feed</Head>
+      <Head>
+        <title>Social Feed | Tramona</title>
+      </Head>
+
       <section className="container my-10">
-        <h1 className="pb-5 text-4xl font-bold lg:pb-10 lg:text-5xl">
+        <h1 className="pb-5 text-center text-4xl font-bold lg:text-5xl">
           Social Feed
         </h1>
+
         {isLoading ? (
           <Button variant="ghost" isLoading disabled>
             Loading...
           </Button>
         ) : (
-          <div className="grid gap-5 md:grid-cols-2">
+          <div className="grid grid-cols-1 place-items-center gap-5">
             {offers?.length ? (
               offers.map((offer) => {
                 return <FeedCard key={offer.id} offer={offer} />;
