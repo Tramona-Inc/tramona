a11a2f1fc8adf5040e49c2d69c0ea72758f636f5
Manas Acharekar <manas.acharekar98@gmail.com>
2024-02-06 22:10:35 -0500

    welcome: updated step 1

--- src/pages/auth/welcome.tsx
+++ src/pages/auth/welcome.tsx
@@ -1,15 +1,17 @@
 import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
 import { Stepper, StepperItem } from "@/components/ui/stepper";
 import { useStepper } from "@/components/ui/use-stepper";
 import type { StepperConfig } from "@/components/ui/stepper";
 import { Button } from "@/components/ui/button";
 import Undone from "@/components/_icons/UndoneIcon";
-import Calender from "@/components/_icons/CalenderIcon";
-import Piggy from "@/components/_icons/PiggyIcon";
-import { fakeLiveDeals } from "@/fake-data/live-deals";
-import Image from "next/image";
+import { CalendarCheck, BadgeDollarSign, PiggyBank } from "lucide-react";
+import OfferCard from "@/components/offer-card/OfferCard";
+import { liveFeedOffers } from "@/components/offer-card/data";
+
 import { cn, sleep } from "@/utils/utils";
-import { useRouter } from "next/router";
 
 function StepperContentLayout({
   children,
@@ -26,75 +28,60 @@ function StepperContentLayout({
 }
 
 function Step1(): JSX.Element {
+  // Get 3 offers with the highest discount %
+  const offers = liveFeedOffers
+    .filter((offer) => offer.discountPercent > 30)
+    .sort((a, b) => b.discountPercent - a.discountPercent)
+    .slice(0, 3);
+
   return (
-    <StepperContentLayout>
-      <p className="text-center text-3xl font-bold">
-        Welcome to <br /> the New Era of Travel !
-      </p>
-      <p className="text-center text-xl font-bold">
-        Here are some things you <br /> should know about Tramona
-      </p>
-      <div className="mx-auto flex w-[85%] flex-col gap-4">
-        <div className="flex flex-row items-center gap-4">
-          <div className="h-30 w-30">
-            <Calender />
+    <StepperContentLayout className="container gap-4 text-balance">
+      <div className="flex flex-col gap-10 text-center">
+        <h1 className="text-4xl font-bold lg:text-5xl">
+          Welcome to the New Era of Travel!
+        </h1>
+        <p className="text-xl font-bold lg:text-2xl">
+          Here are some things you should know about Tramona
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center gap-3 text-pretty lg:gap-4">
+        <div className="flex max-w-[500px] items-center gap-4">
+          <div>
+            <CalendarCheck className="h-8 w-8 text-primary lg:h-10 lg:w-10" />
           </div>
-          <p className="">
+          <p className="font-medium lg:text-lg">
             Tramona works by matching you with one of the many hosts in our
-            network that has a vacancy
+            network that has a vacancy.
           </p>
         </div>
-        <div className="flex flex-row items-center gap-4">
-          <div className="h-30 w-30">
-            <Piggy />
+
+        <div className="flex max-w-[500px] items-center gap-4">
+          <div>
+            <BadgeDollarSign className="h-8 w-8 text-primary lg:h-10 lg:w-10" />
           </div>
-          <p className="">
+          <p className="font-medium lg:text-lg">
+            We also talk to the host directly, avoiding those nasty fees other
+            companies add on.
+          </p>
+        </div>
+
+        <div className="flex max-w-[500px] items-center gap-4">
+          <div>
+            <PiggyBank className="h-8 w-8 text-primary lg:h-10 lg:w-10" />
+          </div>
+          <p className="font-medium lg:text-lg">
             Because of this, we can guarantee you a price that is not available
-            anywhere else. <span className="bg-gold px-1">Make a request</span>{" "}
-            and see for yourself
+            anywhere else. <mark className="bg-gold px-1">Make a request</mark>{" "}
+            and see for yourself.
           </p>
         </div>
       </div>
-      <div className="my-4 flex flex-1 flex-col gap-4 self-center rounded-lg bg-[#3843D0] p-4">
-        {fakeLiveDeals
-          .slice(2)
-          .map(({ minutesAgo, tramonaPrice, oldPrice }, idx) => (
-            <div
-              className="flex flex-row gap-4 rounded-lg border-2 bg-white p-4 text-black"
-              key={idx}
-            >
-              <Image
-                src={"/assets/images/fake-properties/owners/2.png"}
-                alt=""
-                className="inset-0 border border-none object-contain object-center"
-                width={70}
-                height={70}
-              />
-              <div className="relative grid lg:grid-cols-2">
-                <span className="absolute right-[-1rem] top-[-1rem] text-xs text-[#192185]">
-                  {minutesAgo}min ago
-                </span>
-                <div className="flex flex-col items-center justify-center">
-                  <p className="font-bold">Tatiana is going to Paris 🇫🇷</p>
-                  <p className="text-xs">
-                    Tramona Price:{" "}
-                    <span className="font-bold text-[#3843D0] lg:text-lg">
-                      {tramonaPrice}$/per night
-                    </span>{" "}
-                  </p>
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <s className="text-zinc-400">Old price: {oldPrice}$</s>
-                </div>
-                <p className="text-xs text-[#3843D0]">
-                  {Math.round(
-                    100 * (1 - (tramonaPrice ?? 0) / (oldPrice ?? 0)),
-                  )}
-                  % Off!
-                </p>
-              </div>
-            </div>
-          ))}
+
+      <div className="my-4 flex min-w-full flex-1 flex-col space-y-4 self-center rounded-3xl bg-primary p-4 md:min-w-[400px] lg:min-w-[500px] xl:min-w-[600px]">
+        {offers.map((offer) => (
+          <OfferCard key={offer.id} offer={offer} />
+        ))}
       </div>
     </StepperContentLayout>
   );
@@ -214,7 +201,9 @@ export default function Welcome() {
 
   return (
     <>
-      <Head>Welcome</Head>
+      <Head>
+        <title>Welcome | Tramona</title>
+      </Head>
       <div className="mx-auto flex w-full flex-col gap-4 px-5 py-10 lg:px-80">
         <Stepper activeStep={activeStep} responsive={false}>
           {steps.map((step, index) => (
