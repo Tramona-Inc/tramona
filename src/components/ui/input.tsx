b1ad0a2630e43fa3960cdcf1f4845b47e80346c0
Ben Tomlin <bentomlin101@gmail.com>
2024-02-10 20:31:27 -0800

    fix password input eyeball toggle

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -41,11 +41,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     const passwordEyeballButton = (
       <button
         type="button"
+        tabIndex={0}
         onClick={(e) => {
-          e.preventDefault();
+          e.stopPropagation();
           setShowingPassword(!showingPassword);
         }}
-        className="absolute bottom-0 right-0 grid h-10 w-10 place-items-center rounded-full text-zinc-600 hover:bg-black/10 focus-visible:bg-black/10"
+        onMouseDown={(e) => e.preventDefault()}
+        onMouseUp={(e) => e.preventDefault()}
+        className="pointer-events-auto absolute bottom-0 right-0 grid h-10 w-10 place-items-center rounded-full text-zinc-600 hover:bg-black/10 focus-visible:bg-black/10"
       >
         {showingPassword ? <VisibleIcon /> : <HiddenIcon />}
       </button>
