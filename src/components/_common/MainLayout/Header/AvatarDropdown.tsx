dea10b6bfe857732e82c66d42b16c63b2aac9007
zachuri <punsalang.zachary@gmail.com>
2024-02-11 20:00:09 -0800

    feature/signup-in-forget-pass: on signout callback goes back to home page

--- src/components/_common/MainLayout/Header/AvatarDropdown.tsx
+++ src/components/_common/MainLayout/Header/AvatarDropdown.tsx
@@ -1,18 +1,4 @@
-import { signOut } from "next-auth/react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../../../ui/dropdown-menu";
-import UserAvatar from "../../UserAvatar";
-import { type Session } from "next-auth";
-import Link from "next/link";
-import { Badge } from "../../../ui/badge";
-import { usePathname } from "next/navigation";
 import { cn } from "@/utils/utils";
-import { type ForwardRefExoticComponent } from "react";
 import {
   HomeIcon,
   LogOutIcon,
@@ -22,6 +8,20 @@ import {
   UserCogIcon,
   type LucideProps,
 } from "lucide-react";
+import { type Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { type ForwardRefExoticComponent } from "react";
+import { Badge } from "../../../ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../../ui/dropdown-menu";
+import UserAvatar from "../../UserAvatar";
 
 function DropdownTop({ session }: { session: Session }) {
   const title = session.user.name ?? session.user.email ?? "Anonymous";
@@ -141,7 +141,7 @@ export default function AvatarDropdown({ session }: { session: Session }) {
         </> */}
 
         <DropdownMenuItem
-          onClick={() => signOut()}
+          onClick={() => signOut({ callbackUrl: `${window.location.origin}` })}
           className="group flex w-full cursor-pointer items-center gap-2 py-2 pl-3 text-destructive focus:bg-destructive focus:text-white"
         >
           <LogOutIcon className="opacity-50 group-focus:opacity-100" />
