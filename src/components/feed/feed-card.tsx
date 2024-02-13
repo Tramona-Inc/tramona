fd846b3822a22b372aae5eef60ab39e66b4a3315
Manas Acharekar <manas.acharekar98@gmail.com>
2024-02-13 17:55:05 -0500

    temporary change to social feed (uses mock data)

--- src/components/feed/feed-card.tsx
+++ src/components/feed/feed-card.tsx
@@ -28,19 +28,24 @@ import {
 } from "lucide-react";
 import CarouselDots from "./carousel-dots";
 
-import type { AppRouter } from "@/server/api/root";
-import type { inferRouterOutputs } from "@trpc/server";
-import { formatCurrency, getDiscountPercentage } from "@/utils/utils";
+// import type { AppRouter } from "@/server/api/root";
+// import type { inferRouterOutputs } from "@trpc/server";
+import { getDiscountPercentage } from "@/utils/utils";
+import { type LiveFeedOffer } from "../offer-card/data";
 
 // Plugin for relative time
 dayjs.extend(relativeTime);
 
-export type FeedWithInfo =
-  inferRouterOutputs<AppRouter>["offers"]["getAllOffers"][number];
-
 type Props = {
-  offer: FeedWithInfo;
+  offer: LiveFeedOffer;
 };
+
+// export type FeedWithInfo =
+//   inferRouterOutputs<AppRouter>["offers"]["getAllOffers"][number];
+
+// type Props = {
+//   offer: FeedWithInfo;
+// };
 
 // function ModalButton({ icon, text }: { icon: ReactNode; text: string }) {
 //   return (
@@ -94,10 +99,11 @@ export default function FeedCard({ offer }: Props) {
     });
   }, [api]);
 
-  const name = offer.request.madeByUser.name?.split(" ") ?? [""];
+  // const name = offer.request.madeByUser.name?.split(" ") ?? [""];
+  const name = offer.hostName;
 
   // Format the time when the offer was completed at
-  const offerDate = dayjs(offer.request.resolvedAt).fromNow();
+  // const offerDate = dayjs(offer.request.resolvedAt).fromNow();
 
   return (
     <Card className="w-[450px] lg:w-[500px]">
@@ -106,12 +112,15 @@ export default function FeedCard({ offer }: Props) {
           <UserAvatar
             name={name[0]}
             email={undefined}
-            image={offer.request.madeByUser.image}
+            image={offer.hostPicUrl}
+            // image={offer.request.madeByUser.image}
           />
           <div>
-            <p className="font-semibold">Booked by {name[0]}</p>
+            {/* <p className="font-semibold">Booked by {name[0]}</p> */}
+            <p className="font-semibold">Booked by Anonymous</p>
             <p className="text-sm text-secondary-foreground/50">
-              {offer.request.madeByUser.name} - {offerDate}
+              {/* {offer.request.madeByUser.name} - {offerDate} */}
+              Hosted by {offer.hostName}
             </p>
           </div>
         </div>
@@ -122,8 +131,9 @@ export default function FeedCard({ offer }: Props) {
 
       <CardContent>
         <Carousel setApi={setApi} className="-mx-4">
-          <CarouselContent className="max-h-[500px] min-h-[500px]">
-            {offer.property.imageUrls.map((image, idx) => (
+          {/* <CarouselContent className="max-h-[500px] min-h-[500px]"> */}
+          <CarouselContent>
+            {/* {offer.property.imageUrls.map((image, idx) => (
               <CarouselItem key={idx} className="flex justify-center">
                 <Image
                   src={image}
@@ -133,7 +143,16 @@ export default function FeedCard({ offer }: Props) {
                   style={{ objectFit: "cover" }}
                 />
               </CarouselItem>
-            ))}
+            ))} */}
+            <CarouselItem>
+              <Image
+                src={offer.imageUrl}
+                alt={`${offer.hostName}'s property`}
+                width={750}
+                height={750}
+                style={{ objectFit: "cover" }}
+              />
+            </CarouselItem>
           </CarouselContent>
           {count !== 0 && <CarouselDots count={count} current={current} />}
           <CarouselNext
@@ -149,23 +168,25 @@ export default function FeedCard({ offer }: Props) {
         <div className="mx-2 mb-1 mt-4 flex items-center justify-between">
           <div className="text-center text-secondary-foreground/50">
             <p className="text-2xl font-semibold line-through lg:text-3xl">
-              {formatCurrency(offer.property.originalNightlyPrice)}
+              {/* {formatCurrency(offer.property.originalNightlyPrice)} */}$
+              {offer.originalPrice}
             </p>
             <p className="text-sm tracking-tight">Airbnb price</p>
           </div>
           <div className="text-center text-secondary-foreground/50">
             <p className="text-2xl font-semibold text-primary lg:text-3xl">
-              {formatCurrency(offer.totalPrice)}
+              {/* {formatCurrency(offer.totalPrice)} */}${offer.tramonaPrice}
             </p>
             <p className="text-sm tracking-tight">Our price</p>
           </div>
           <div className="bg-primary px-4 py-2 text-zinc-50 lg:px-5 lg:py-3">
             <p className="text-lg font-semibold lg:text-xl">
-              {getDiscountPercentage(
+              {/* {getDiscountPercentage(
                 offer.property.originalNightlyPrice,
                 offer.totalPrice,
-              )}
-              % OFF
+              )} */}
+              {getDiscountPercentage(offer.originalPrice, offer.tramonaPrice)}%
+              OFF
             </p>
           </div>
         </div>
