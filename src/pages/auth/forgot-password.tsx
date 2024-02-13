f5f1f7d4f569e975bdfef03b0ce1d29407c4181b
zachuri <punsalang.zachary@gmail.com>
2024-02-13 01:11:24 -0800

    feature/signup-in-forget-pass: added reset password page linking with support

--- src/pages/auth/forgot-password.tsx
+++ src/pages/auth/forgot-password.tsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+  })
+  .required();
 
 export default function ForgotPassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleSubmit = async ({ email }: z.infer<typeof formSchema>) => {
+    return null;
+  };
+
   return (
-    <div>ForgotPassword</div>
-  )
+    <div className="flex h-screen flex-col items-center justify-center">
+      <section className="flex max-w-sm flex-col space-y-5">
+        <h1 className="text-4xl font-bold tracking-tight">
+          Reset your password
+        </h1>
+        <p>
+          Enter your email address, and we&apos;ll send you a link to get back
+          into your account.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-7"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email address</FormLabel>
+                  <FormControl>
+                    <Input {...field} autoFocus />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormMessage />
+
+            <Link
+              href="/support"
+              className="font-medium text-blue-600 underline underline-offset-2 hover:text-blue-300"
+            >
+              Need support?
+            </Link>
+
+            <Button type="submit" className="w-full">
+              Send link
+            </Button>
+          </form>
+        </Form>
+      </section>
+    </div>
+  );
 }
