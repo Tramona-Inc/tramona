a649f1baab260930a2e1c7a69f30acfee313cbcd
zachuri <punsalang.zachary@gmail.com>
2024-02-14 22:53:00 -0800

    feature/my-trips: updated the grid to be 2 on lg screens

--- src/pages/my-trips/upcoming.tsx
+++ src/pages/my-trips/upcoming.tsx
@@ -14,7 +14,7 @@ export default function Upcoming() {
     <div className="container flex flex-col gap-10 py-10">
       <h1 className="text-4xl font-bold">Upcoming</h1>
 
-      <div className="grid gap-8 md:grid-cols-2">
+      <div className="grid gap-8 lg:grid-cols-2">
         {isLoading ? (
           <>Loading ...</>
         ) : (
