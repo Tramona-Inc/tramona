9b4f810052e8239a0e2bdaebbafe1fef7f715cad
artifactz1 <punsalang.art@gmail.com>
2024-02-19 17:26:32 -0800

    email_templates: was able to set up and make initial email template / need to do the form functionality for now

--- src/components/email-templates/welcome.tsx
+++ src/components/email-templates/welcome.tsx
@@ -1,150 +1,71 @@
 import {
-  Body,
-  Button,
-  Column,
   Container,
   Head,
-  Heading,
+  Hr,
   Html,
-  Img,
-  Link,
-  Preview,
-  Row,
   Section,
+  Tailwind,
   Text,
+  Preview,
+  Button,
+  Link,
 } from "@react-email/components";
-import * as React from "react";
+import TramonaIcon from "../_icons/TramonaIcon";
 
-interface NetlifyWelcomeEmailProps {
-  steps?: {
-    id: number;
-    Description: React.ReactNode;
-  }[];
-  links?: string[];
-}
-
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "";
-
-const PropDefaults: NetlifyWelcomeEmailProps = {
-  steps: [
-    {
-      id: 1,
-      Description: (
-        <li className="mb-20" key={1}>
-          <strong>Deploy your first project.</strong>{" "}
-          <Link>Connect to Git, choose a template</Link>, or manually deploy a
-          project you&apos;ve been working on locally.
-        </li>
-      ),
-    },
-    {
-      id: 2,
-      Description: (
-        <li className="mb-20" key={2}>
-          <strong>Check your deploy logs.</strong> Find out what&apos;s included
-          in your build and watch for errors or failed deploys.{" "}
-          <Link>Learn how to read your deploy logs</Link>.
-        </li>
-      ),
-    },
-    {
-      id: 3,
-      Description: (
-        <li className="mb-20" key={3}>
-          <strong>Choose an integration.</strong> Quickly discover, connect, and
-          configure the right tools for your project with 150+ integrations to
-          choose from. <Link>Explore the Integrations Hub</Link>.
-        </li>
-      ),
-    },
-    {
-      id: 4,
-      Description: (
-        <li className="mb-20" key={4}>
-          <strong>Set up a custom domain.</strong> You can register a new domain
-          and buy it through Netlify or assign a domain you already own to your
-          site. <Link>Add a custom domain</Link>.
-        </li>
-      ),
-    },
-  ],
-  links: ["Visit the forums", "Read the docs", "Contact an expert"],
-};
-
-export const NetlifyWelcomeEmail = ({
-  steps = PropDefaults.steps,
-  links = PropDefaults.links,
-}: NetlifyWelcomeEmailProps) => {
+export default function VerifyEmailLink({
+  name,
+  url,
+}: {
+  name: string | null;
+  url: string;
+}) {
   return (
-    <Html className="Help">
-      <Head />
-      <Preview>Netlify Welcome</Preview>
-
-      <Body className="bg-offwhite font-sans text-base">
-        <Img
-          src={`${baseUrl}/static/netlify-logo.png`}
-          width="184"
-          height="75"
-          alt="Netlify"
-          className="mx-auto my-20"
-        />
-        <Container className="p-45 bg-white">
-          <Heading className="my-0 text-center leading-8">
-            Welcome to Netlify
-          </Heading>
-
-          <Section>
-            <Row>
-              <Text className="text-base">
-                Congratulations! You&apos;re joining over 3 million developers
-                around the world who use Netlify to build and ship sites,
-                stores, and apps.
+    <Html lang="en">
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <Preview>Referal Amount</Preview>;
+      <Tailwind>
+        <Container className="bg-slate mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] bg-slate-200  p-10">
+          <div className="flex flex-row items-center gap-x-4">
+            <TramonaIcon />
+            <h3>Tramona</h3>
+          </div>
+          <div className="mt-4 rounded bg-white p-8">
+            <div>
+              <h3>Welcome to Tramona!</h3>
+              <Text>Hi {name},</Text>
+              <Text>
+                Your referral network is paying off. You just earned $() from a
+                referral. Your total earned is now $().
               </Text>
+              <Text>
+                Thanks,
+                <br />
+                Tramona Team
+              </Text>
+            </div>
 
-              <Text className="text-base">Here&apos;s how to get started:</Text>
-            </Row>
-          </Section>
-
-          <ul>{steps?.map(({ Description }) => Description)}</ul>
-
-          <Section className="text-center">
-            <Button className="bg-brand rounded-lg px-[18px] py-3 text-white">
-              Go to your dashboard
+            <Button
+              className="items-center rounded bg-[#2563EB] px-10 
+              py-3 text-center text-sm text-white"
+              href={url}
+            >
+              Check your cash back balance
             </Button>
-          </Section>
+            {url}
+          </div>
 
-          <Section className="mt-45">
-            <Row>
-              {links?.map((link) => (
-                <Column key={link}>
-                  <Link className="font-bold text-black underline">{link}</Link>{" "}
-                  <span className="text-green-500">→</span>
-                </Column>
-              ))}
-            </Row>
-          </Section>
-        </Container>
-
-        <Container className="mt-20">
-          <Section>
-            <Row>
-              <Column className="px-20 text-right">
-                <Link>Unsubscribe</Link>
-              </Column>
-              <Column className="text-left">
-                <Link>Manage Preferences</Link>
-              </Column>
-            </Row>
-          </Section>
-          <Text className="mb-45 text-center text-gray-400">
-            Netlify, 44 Montgomery Street, Suite 300 San Francisco, CA
+          <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+          <Text>
+            Questions or faq? Contact us at <Link>info@tramona.com</Link> If
+            you'd rather not receive this kind of email, Don't want any more
+            emails from Tramona?
+            <Link> Unsubcribe.</Link>
           </Text>
+          <Text>© 2024 Tramona</Text>
         </Container>
-      </Body>
+      </Tailwind>
     </Html>
   );
-};
-
-export default NetlifyWelcomeEmail;
+}
