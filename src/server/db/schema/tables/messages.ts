31fab5a6ed538639f82bb2e29c1c51794f3cfd25
zachuri <punsalang.zachary@gmail.com>
2024-02-19 16:21:08 -0800

    feature/messaging: finalizing schema

--- src/server/db/schema/tables/messages.ts
+++ src/server/db/schema/tables/messages.ts
@@ -1,5 +1,6 @@
 import {
   boolean,
+  index,
   integer,
   pgEnum,
   pgTable,
@@ -21,15 +22,22 @@ export const conversations = pgTable("conversations", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
-export const messages = pgTable("offers", {
-  id: serial("id").primaryKey(),
-  conversationId: integer("conversation_id")
-    .notNull()
-    .references(() => conversations.id),
-  userId: text("user_id")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  message: varchar("message", { length: 1500 }).notNull(),
-  isEdit: boolean("is_edit").default(false),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const messages = pgTable(
+  "messages",
+  {
+    id: serial("id").primaryKey(),
+    conversationId: integer("conversation_id")
+      .notNull()
+      .references(() => conversations.id),
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id, { onDelete: "set null" }),
+    message: varchar("message", { length: 1500 }).notNull(),
+    isEdit: boolean("is_edit").default(false),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (t) => ({
+    conversationIndex: index("conversationIndex").on(t.conversationId),
+    userIndex: index("userIndex").on(t.userId),
+  }),
+);
