c5e13110cffff43383cbdb201e02b60d2133983a
zachuri <punsalang.zachary@gmail.com>
2024-02-27 21:39:20 -0800

    bugfix/messaging: added comment to disable useEffect dependency

--- src/components/messages/ChatMessages.tsx
+++ src/components/messages/ChatMessages.tsx
@@ -18,6 +18,7 @@ export default function ChatMessages({
 
     // Fetch initial messages when the component mounts
     void fetchInitialMessages(conversationId);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [conversationId]);
 
   return (
