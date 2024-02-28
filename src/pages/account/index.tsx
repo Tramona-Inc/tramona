1111c87e1db0f85cc074086a6f0e4ab25a5f3dbd
Manas Acharekar <manas.acharekar98@gmail.com>
2024-02-28 15:17:31 -0500

    fix metadata for account page
    Co-authored-by: Zachary <zachuri@users.noreply.github.com>

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -10,7 +10,9 @@ export default function MyAccount() {
 
   return (
     <>
-      <Head>My Account | Tramona</Head>
+      <Head>
+        <title>My Account | Tramona</title>
+      </Head>
       <div className="min-h-[calc(100vh-5rem)] gap-10 space-y-5 bg-zinc-100 px-5 pt-5 lg:flex lg:space-y-0">
         <AccountSidebar />
         <div className="w-full space-y-5">
