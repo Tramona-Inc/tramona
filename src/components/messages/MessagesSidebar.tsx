8c0b28e4ca3bc3a38472ec18b0878f1d8df972a5
zachuri <punsalang.zachary@gmail.com>
2024-03-05 00:07:18 -0800

    feature/host-url-signup: added logic to make sure read is only showed when messages is greater than 0

--- src/components/messages/MessagesSidebar.tsx
+++ src/components/messages/MessagesSidebar.tsx
@@ -60,7 +60,7 @@ export function MessageConversation({
       <div className="ml-4 md:ml-2">
         <h1 className="text-xl font-bold">{displayParticipants}</h1>
         <p className="line-clamp-1 flex flex-row items-center gap-1 text-sm text-muted-foreground">
-          {!messages[0]?.read && messages[0]?.userId !== session?.user.id && (
+          {messages.length > 0 && !messages[0]?.read && messages[0]?.userId !== session?.user.id && (
             <div className="rounded-full bg-blue-500 p-1" />
           )}
           {messages[0]?.userId === session?.user.id && "You: "}
