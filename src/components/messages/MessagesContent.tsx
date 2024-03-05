b47b0cfb57dff87d26239a400f25d583b6b9e108
zachuri <punsalang.zachary@gmail.com>
2024-03-05 11:35:28 -0800

    feature/host-url-signup: offers message button now works going to a certain conversation

--- src/components/messages/MessagesContent.tsx
+++ src/components/messages/MessagesContent.tsx
@@ -1,10 +1,5 @@
-import {
-  useConversation,
-  type Conversation,
-} from "@/utils/store/conversations";
+import { type Conversation } from "@/utils/store/conversations";
 import { cn } from "@/utils/utils";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import ChatHeader from "./ChatHeader";
 import ChatInput from "./ChatInput";
 import ChatMessages from "./ChatMessages";
@@ -18,28 +13,6 @@ export default function MessagesContent({
   selectedConversation,
   setSelected,
 }: ContentProps) {
-  // Getting state from conversation state (not messages state)
-  const { query } = useRouter();
-
-  const conversations = useConversation((state) => state.conversationList);
-
-  // allows us to open message from query
-  useEffect(() => {
-    if (query.conversationId && conversations.length > 0) {
-      const conversationIdToSelect = parseInt(query.conversationId as string);
-
-      // Find the conversation with the matching ID
-      const selectedConversation = conversations.find(
-        (conversation) => conversation.id === conversationIdToSelect,
-      );
-
-      // Set the selected conversation in your Zustand store
-      if (selectedConversation) {
-        setSelected(selectedConversation);
-      }
-    }
-  }, [conversations, query.conversationId, setSelected]);
-
   return (
     <div
       className={cn(
--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,12 +1,18 @@
 import MessagesContent from "@/components/messages/MessagesContent";
 import MessagesSidebar from "@/components/messages/MessagesSidebar";
-import { type Conversation } from "@/utils/store/conversations";
+import {
+  useConversation,
+  type Conversation,
+} from "@/utils/store/conversations";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function MessagePage() {
-  useSession({ required: true });
+  const [isViewed, setIsViewd] = useState(false);
+
+  const conversations = useConversation((state) => state.conversationList);
 
   const [selectedConversation, setSelectedConversation] =
     useState<Conversation | null>(null);
@@ -14,6 +20,29 @@ export default function MessagePage() {
   const selectConversation = (conversation: Conversation | null) => {
     setSelectedConversation(conversation);
   };
+
+  const { query } = useRouter();
+
+  useSession({ required: true });
+
+  // Allows us to open message from url query
+  useEffect(() => {
+    if (query.conversationId && conversations.length > 0 && !isViewed) {
+      const conversationIdToSelect = parseInt(query.conversationId as string);
+      const conversationToSelect = conversations.find(
+        (conversation) => conversation.id === conversationIdToSelect,
+      );
+
+      if (
+        conversationToSelect &&
+        selectedConversation?.id !== conversationToSelect.id
+      ) {
+        setSelectedConversation(conversationToSelect);
+      }
+
+      setIsViewd(true);
+    }
+  }, [conversations, isViewed, query.conversationId, selectedConversation?.id]);
 
   return (
     <>
