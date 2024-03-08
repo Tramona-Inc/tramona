2ac2576375bf95563e2857e524c3fb8cb4199a5e
zachuri <punsalang.zachary@gmail.com>
2024-03-07 16:46:17 -0800

    feature/host-payout: added general layout

--- src/pages/host/payout.tsx
+++ src/pages/host/payout.tsx
@@ -1,15 +1,14 @@
 import Head from "next/head";
 
 export default function Payout() {
-
-  
-
   return (
     <>
       <Head>
         <title>Host Payout | Tramona</title>
       </Head>
-      <div className="">hello</div>
+      <main className="container flex h-screen flex-col items-center justify-center">
+        <h2 className="fond-bold text-4xl">Payout</h2>
+      </main>
     </>
   );
 }
