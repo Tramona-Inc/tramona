cafbd307c363671c197093de495240e10f2ad8c8
zachuri <punsalang.zachary@gmail.com>
2024-03-07 16:40:16 -0800

    feature/host-payout: fixed lint errors

--- src/pages/host/index.tsx
+++ src/pages/host/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Page() {
   return (
-    >
+    <>
       <Head>Host Dashboard | Tramona</Head>
       <p className="p-8">host dashboard</p>
     </>
