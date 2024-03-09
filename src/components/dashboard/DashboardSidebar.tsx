f09bcee99cd57f856a47aa3d18ca09a0831555e8
Manas Acharekar <manas.acharekar98@gmail.com>
2024-03-08 19:16:54 -0500

    rearrange links

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -30,10 +30,9 @@ export default function DashboardSidebar() {
   return (
     <div className="sticky top-0 col-span-2 hidden max-h-[100vh] flex-col justify-center gap-5 border-r-2 border-zinc-100 px-5 lg:flex 2xl:col-span-1">
       <SidebarLink href={"/"}>Overview</SidebarLink>
-      <SidebarLink href={"/my-trips"}>My Trips</SidebarLink>
       <SidebarLink href={"/requests"}>Request/offers</SidebarLink>
+      <SidebarLink href={"/my-trips"}>My Trips</SidebarLink>
       <SidebarLink href={"/messages"}>Messages</SidebarLink>
-      <SidebarLink href={"/faq"}>FAQ</SidebarLink>
     </div>
   );
 }
