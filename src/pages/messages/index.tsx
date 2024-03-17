99823e2b0a390a675f79e7f41867e7605acb6e98
zachuri <punsalang.zachary@gmail.com>
2024-03-16 22:55:16 -0700

    feature/message-realtime-conversations: added url query method to link to convo back to message page for url host

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,13 +1,16 @@
 import DashboardLayout from "@/components/_common/Layout/DashboardLayout";
 import MessagesContent from "@/components/messages/MessagesContent";
 import MessagesSidebar from "@/components/messages/MessagesSidebar";
-import { type Conversation } from "@/utils/store/conversations";
+import {
+  useConversation,
+  type Conversation,
+} from "@/utils/store/conversations";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function MessageDisplay() {
-
   const [selectedConversation, setSelectedConversation] =
     useState<Conversation | null>(null);
 
@@ -15,29 +18,28 @@ function MessageDisplay() {
     setSelectedConversation(conversation);
   };
 
-  // TODO: Allows us to open message from url query
+  // * Allows us to open message from url query
+  const [isViewed, setIsViewd] = useState(false);
+  const conversations = useConversation((state) => state.conversationList);
+  const { query } = useRouter();
 
-  // const [isViewed, setIsViewd] = useState(false);
-  // const conversations = useConversation((state) => state.conversationList);
-  // const { query } = useRouter();
+  useEffect(() => {
+    if (query.conversationId && conversations.length > 0 && !isViewed) {
+      const conversationIdToSelect = query.conversationId as string;
+      const conversationToSelect = conversations.find(
+        (conversation) => conversation.id === conversationIdToSelect,
+      );
 
-  // useEffect(() => {
-  //   if (query.conversationId && conversations.length > 0 && !isViewed) {
-  //     const conversationIdToSelect = query.conversationId as string;
-  //     const conversationToSelect = conversations.find(
-  //       (conversation) => conversation.id === conversationIdToSelect,
-  //     );
+      if (
+        conversationToSelect &&
+        selectedConversation?.id !== conversationToSelect.id
+      ) {
+        setSelectedConversation(conversationToSelect);
+      }
 
-  //     if (
-  //       conversationToSelect &&
-  //       selectedConversation?.id !== conversationToSelect.id
-  //     ) {
-  //       setSelectedConversation(conversationToSelect);
-  //     }
-
-  //     setIsViewd(true);
-  //   }
-  // }, [conversations, isViewed, query.conversationId, selectedConversation?.id]);
+      setIsViewd(true);
+    }
+  }, [conversations, isViewed, query.conversationId, selectedConversation?.id]);
 
   return (
     <div className="flex min-h-screen-minus-header">
