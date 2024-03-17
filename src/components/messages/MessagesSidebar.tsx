b767c2a2356fcee2b49c1add7af69cc1ece97a34
zachuri <punsalang.zachary@gmail.com>
2024-03-16 20:57:14 -0700

    feature/message-realtime-conversations: added listener to parent of sidebar/ separte sidebar conversation to its own component

--- src/components/messages/MessagesSidebar.tsx
+++ src/components/messages/MessagesSidebar.tsx
@@ -1,138 +1,16 @@
-import { type MessageDbType } from "@/types/supabase.message";
+import { MessageDbType } from "@/types/supabase.message";
 import { api } from "@/utils/api";
 import {
   useConversation,
   type Conversation,
 } from "@/utils/store/conversations";
-import { useMessage, type ChatMessageType } from "@/utils/store/messages";
+import { ChatMessageType, useMessage } from "@/utils/store/messages";
 import supabase from "@/utils/supabase-client";
 import { errorToast } from "@/utils/toasts";
-import { cn } from "@/utils/utils";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import Spinner from "../_common/Spinner";
-import UserAvatar from "../_common/UserAvatar";
-
-export function MessageConversation({
-  conversation,
-  isSelected,
-  setSelected,
-}: {
-  conversation: Conversation;
-  isSelected: boolean;
-  setSelected: (arg0: Conversation) => void;
-}) {
-  const optimisticIds = useMessage((state) => state.optimisticIds);
-
-  const setConversationToTop = useConversation(
-    (state) => state.setConversationToTop,
-  );
-
-  const handlePostgresChange = async (payload: { new: MessageDbType }) => {
-    if (!optimisticIds.includes(payload.new.id)) {
-      const { data, error } = await supabase
-        .from("user")
-        .select("name, email, image")
-        .eq("id", payload.new.user_id)
-        .single();
-      if (error) {
-        errorToast(error.message);
-      } else {
-        const newMessage: ChatMessageType = {
-          id: payload.new.id,
-          conversationId: payload.new.conversation_id,
-          userId: payload.new.user_id,
-          message: payload.new.message,
-          isEdit: payload.new.is_edit,
-          createdAt: new Date(payload.new.created_at),
-          read: payload.new.read,
-          user: data,
-        };
-
-        setConversationToTop(payload.new.conversation_id, newMessage);
-      }
-    }
-  };
-
-  useEffect(() => {
-    const channel = supabase
-      .channel(`${conversation.id}`)
-      .on(
-        "postgres_changes",
-        {
-          event: "INSERT",
-          schema: "public",
-          table: "messages",
-        },
-        (payload: { new: MessageDbType }) => void handlePostgresChange(payload),
-      )
-      .subscribe();
-
-    return () => {
-      void channel.unsubscribe();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const { participants, messages, id } = conversation;
-
-  const displayParticipants = participants
-    .map((participant) => participant.name)
-    .join(", ");
-
-  const { data: session } = useSession();
-
-  const { mutateAsync: setMessageToReadMutate } =
-    api.messages.setMessageToRead.useMutation();
-
-  const setConversationReadState = useConversation(
-    (state) => state.setConversationReadState,
-  );
-
-  function handleSelected() {
-    if (session?.user.id !== messages[0]?.userId && messages[0]?.id) {
-      void setMessageToReadMutate({ messageId: messages[0]?.id });
-    }
-    // Update local state to true
-    setConversationReadState(conversation.id);
-
-    setSelected(conversation);
-  }
-
-  return (
-    <div
-      className={cn(
-        "flex items-center justify-start border-b px-4 py-6 hover:cursor-pointer hover:bg-zinc-200 lg:p-8",
-        isSelected && "bg-zinc-100",
-      )}
-      onClick={() => handleSelected()}
-    >
-      <UserAvatar
-        email={participants[0]?.email ?? ""}
-        image={participants[0]?.image ?? ""}
-        name={participants[0]?.name ?? ""}
-      />
-
-      <div className="ml-4 md:ml-2">
-        <h1 className="font-bold">{displayParticipants}</h1>
-        <p className="line-clamp-1 flex flex-row items-center gap-1 text-sm text-muted-foreground">
-          {messages.length > 0 &&
-            !messages[0]?.read &&
-            messages[0]?.userId !== session?.user.id && (
-              <span className="rounded-full bg-blue-500 p-1" />
-            )}
-          {messages[0]?.userId === session?.user.id && "You: "}
-          {messages[0]?.message ?? ""}
-        </p>
-        {session?.user.role === "admin" && (
-          <p className="line-clamp-1 text-sm uppercase text-muted-foreground">
-            Conversation Id: {id}
-          </p>
-        )}
-      </div>
-    </div>
-  );
-}
+import { SidebarConversation } from "./SidebarConversation";
 
 export type SidebarProps = {
   selectedConversation: Conversation | null;
@@ -160,14 +38,87 @@ export default function MessagesSidebar({
     if (fetchedConversations) {
       setConversationList(fetchedConversations);
     }
-  }, [fetchedConversations, setConversationList]);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetchedConversations]);
+
+  const optimisticIds = useMessage((state) => state.optimisticIds);
+
+  const setConversationToTop = useConversation(
+    (state) => state.setConversationToTop,
+  );
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const handlePostgresChange = async (payload: { new: MessageDbType }) => {
+      if (!optimisticIds.includes(payload.new.id)) {
+        const { data, error } = await supabase
+          .from("user")
+          .select("name, email, image")
+          .eq("id", payload.new.user_id)
+          .single();
+        if (error) {
+          errorToast(error.message);
+        } else {
+          const newMessage: ChatMessageType = {
+            id: payload.new.id,
+            conversationId: payload.new.conversation_id,
+            userId: payload.new.user_id,
+            message: payload.new.message,
+            isEdit: payload.new.is_edit,
+            createdAt: new Date(payload.new.created_at),
+            read: payload.new.read,
+            user: data,
+          };
+
+          setConversationToTop(payload.new.conversation_id, newMessage);
+        }
+      }
+    };
+
+    const fetchConversationIds = async () => {
+      if (session) {
+        const { data: conversationIds, error } = await supabase
+          .from("conversation_participants")
+          .select("conversation_id")
+          .eq("user_id", session.user.id);
+
+        if (error) {
+          errorToast(error.message);
+        } else {
+          const channels = conversationIds.map((conversationId) =>
+            supabase
+              .channel(conversationId.conversation_id)
+              .on(
+                "postgres_changes",
+                {
+                  event: "INSERT",
+                  schema: "public",
+                  table: "messages",
+                },
+                (payload: { new: MessageDbType }) =>
+                  void handlePostgresChange(payload),
+              )
+              .subscribe(),
+          );
+
+          return () => {
+            channels.forEach((channel) => void channel.unsubscribe());
+          };
+        }
+      }
+    };
+
+    void fetchConversationIds();
+  }, [optimisticIds, session, setConversationToTop]);
 
   return (
     <div className="w-96 border-r">
       {!isLoading ? (
         conversations && conversations.length > 0 ? (
           conversations.map((conversation) => (
-            <MessageConversation
+            <SidebarConversation
               key={conversation.id}
               conversation={conversation}
               isSelected={selectedConversation?.id === conversation.id}
--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,19 +1,12 @@
 import DashboardLayout from "@/components/_common/Layout/DashboardLayout";
 import MessagesContent from "@/components/messages/MessagesContent";
 import MessagesSidebar from "@/components/messages/MessagesSidebar";
-import {
-  useConversation,
-  type Conversation,
-} from "@/utils/store/conversations";
+import { type Conversation } from "@/utils/store/conversations";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function MessageDisplay() {
-  const [isViewed, setIsViewd] = useState(false);
-
-  const conversations = useConversation((state) => state.conversationList);
 
   const [selectedConversation, setSelectedConversation] =
     useState<Conversation | null>(null);
@@ -22,29 +15,32 @@ function MessageDisplay() {
     setSelectedConversation(conversation);
   };
 
-  const { query } = useRouter();
+  // TODO: Allows us to open message from url query
 
-  // Allows us to open message from url query
-  useEffect(() => {
-    if (query.conversationId && conversations.length > 0 && !isViewed) {
-      const conversationIdToSelect = query.conversationId as string;
-      const conversationToSelect = conversations.find(
-        (conversation) => conversation.id === conversationIdToSelect,
-      );
+  // const [isViewed, setIsViewd] = useState(false);
+  // const conversations = useConversation((state) => state.conversationList);
+  // const { query } = useRouter();
 
-      if (
-        conversationToSelect &&
-        selectedConversation?.id !== conversationToSelect.id
-      ) {
-        setSelectedConversation(conversationToSelect);
-      }
+  // useEffect(() => {
+  //   if (query.conversationId && conversations.length > 0 && !isViewed) {
+  //     const conversationIdToSelect = query.conversationId as string;
+  //     const conversationToSelect = conversations.find(
+  //       (conversation) => conversation.id === conversationIdToSelect,
+  //     );
 
-      setIsViewd(true);
-    }
-  }, [conversations, isViewed, query.conversationId, selectedConversation?.id]);
+  //     if (
+  //       conversationToSelect &&
+  //       selectedConversation?.id !== conversationToSelect.id
+  //     ) {
+  //       setSelectedConversation(conversationToSelect);
+  //     }
+
+  //     setIsViewd(true);
+  //   }
+  // }, [conversations, isViewed, query.conversationId, selectedConversation?.id]);
 
   return (
-    <div className="min-h-screen-minus-header flex">
+    <div className="flex min-h-screen-minus-header">
       <MessagesSidebar
         selectedConversation={selectedConversation}
         setSelected={selectConversation}
