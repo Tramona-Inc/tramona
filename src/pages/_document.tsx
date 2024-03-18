4a13e44c02b160b65eb20c611dc090c7dd1509f0
annsts <an3245@nyu.edu>
2024-03-18 19:20:20 -0400

    rm change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,12 +16,6 @@ export default function Document() {
           strategy="afterInteractive"
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_KEY}&libraries=places&callback=PlacesAutocomplete&loading=async`}
         />
-        <Script id="PlacesAutocomplete" strategy="afterInteractive">
-          {`
-    window.PlacesAutocomplete = function() {
-    }
-  `}
-        </Script>
       </body>
     </Html>
   );
