44f60e71aeeac79a421d4e810fd060683b7bdb56
Ben Tomlin <bentomlin101@gmail.com>
2024-03-25 11:03:24 -0700

    test

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,10 @@ import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
+export const config = {
+  runtime: "edge",
+};
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
