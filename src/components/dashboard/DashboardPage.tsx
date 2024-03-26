daad5a4ddb488add4567d664860c59e329e45064
zachuri <punsalang.zachary@gmail.com>
2024-03-26 13:54:23 -0700

    master: updated dashboard page with bens -> useMaybeSendUnsentRequests

--- src/components/dashboard/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage.tsx
@@ -2,8 +2,11 @@ import Head from "next/head";
 
 import DashboardOverview from "@/components/dashboard/DashboardOverview";
 import DashboardLayout from "../_common/Layout/DashboardLayout";
+import { useMaybeSendUnsentRequests } from '@/utils/useMaybeSendUnsentRequests';
 
 export default function DashboardPage() {
+  useMaybeSendUnsentRequests();
+
   return (
     <DashboardLayout type="guest">
       <Head>
