2e97e2a9aae6fcd0eeeeac73dc4276918d72eefd
zachuri <punsalang.zachary@gmail.com>
2024-03-28 20:31:00 -0700

    feature/host-dashboard-property: fix layout for displaying the dialog and header

--- src/pages/host/properties.tsx
+++ src/pages/host/properties.tsx
@@ -16,22 +16,24 @@ export default function Properties() {
 
   return (
     <DashboadLayout type="host">
-      <h1 className="text-h1">Properties</h1>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button>Add a Property</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Add your Property</DialogTitle>
-            <DialogDescription>
-              Please input all the information necessary for travellers to start
-              booking with your property.
-            </DialogDescription>
-          </DialogHeader>
-          <HostPropertyForm setOpen={setOpen}/>
-        </DialogContent>
-      </Dialog>
+      <div className="mx-10 flex flex-row justify-between">
+        <h1 className="text-4xl font-bold">Properties</h1>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button>Add a Property</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add your Property</DialogTitle>
+              <DialogDescription>
+                Please input all the information necessary for travellers to
+                start booking with your property.
+              </DialogDescription>
+            </DialogHeader>
+            <HostPropertyForm setOpen={setOpen} />
+          </DialogContent>
+        </Dialog>
+      </div>
     </DashboadLayout>
   );
 }
