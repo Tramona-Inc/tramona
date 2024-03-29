d339b2ddd2714ceb18976fde1d62ecab149668be
Neal Uehara <ueharaneal@gmail.com>
2024-03-29 14:01:08 -0700

    text fix

--- src/components/_common/Header/index.tsx
+++ src/components/_common/Header/index.tsx
@@ -52,7 +52,7 @@ function LargeHeader(props: HeaderProps) {
       <div className="flex flex-1 justify-end gap-5">
         {props.type === "dashboard" ? (
           <Button asChild variant="darkOutline">
-            <Link href="/">Switch to Home page</Link>
+            <Link href="/">Switch to Homepage</Link>
           </Button>
         ) : (
           <Button asChild variant="darkOutline">
