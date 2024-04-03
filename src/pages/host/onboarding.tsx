e23d8da01ba8ce9085e18af426f84818721218c1
zachuri <punsalang.zachary@gmail.com>
2024-04-03 15:05:56 -0700

    feature/new-host-signup-flow: progress is not displaying correctly

--- src/pages/host/onboarding.tsx
+++ src/pages/host/onboarding.tsx
@@ -5,6 +5,15 @@ import { useHostOnboarding } from "@/utils/store/host-onboarding";
 
 export default function Onboarding() {
   const progress = useHostOnboarding((state) => state.progress);
+  const setProgress = useHostOnboarding((state) => state.setProgress);
+
+  function inc() {
+    setProgress(progress + 1);
+  }
+
+  function dec() {
+    setProgress(progress - 1);
+  }
 
   return (
     <>
@@ -14,7 +23,21 @@ export default function Onboarding() {
           <Onboarding1 />
         </div>
         <div className="flex justify-end border-t-[5px] p-5">
+          <div
+            className="flex h-4 w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700"
+            role="progressbar"
+          >
+            <div
+              className={`w-[${progress * 10}%] flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full bg-blue-600 text-center text-xs text-white transition duration-500 dark:bg-blue-500`}
+            >
+              {progress * 10}
+            </div>
+          </div>
+
           <Button>Get Started</Button>
+          {progress}
+          <Button onClick={() => inc()}>+</Button>
+          <Button onClick={() => dec()}>-</Button>
         </div>
       </div>
     </>
