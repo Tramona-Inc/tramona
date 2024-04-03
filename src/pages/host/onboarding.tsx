27babbf4c76199e1639c0ac35a0724928f1c2782
zachuri <punsalang.zachary@gmail.com>
2024-04-03 15:52:50 -0700

    feature/new-host-signup-flow: working progress bar with navigation buttons at the bottom

--- src/pages/host/onboarding.tsx
+++ src/pages/host/onboarding.tsx
@@ -1,19 +1,12 @@
 import Header from "@/components/_common/Header";
 import Onboarding1 from "@/components/host/onboarding/Onboarding1";
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import { useHostOnboarding } from "@/utils/store/host-onboarding";
 
 export default function Onboarding() {
   const progress = useHostOnboarding((state) => state.progress);
   const setProgress = useHostOnboarding((state) => state.setProgress);
-
-  function inc() {
-    setProgress(progress + 1);
-  }
-
-  function dec() {
-    setProgress(progress - 1);
-  }
 
   return (
     <>
@@ -22,22 +15,12 @@ export default function Onboarding() {
         <div className="w-full flex-grow max-sm:container lg:grid lg:grid-cols-2">
           <Onboarding1 />
         </div>
-        <div className="flex justify-end border-t-[5px] p-5">
-          <div
-            className="flex h-4 w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700"
-            role="progressbar"
-          >
-            <div
-              className={`w-[${progress * 10}%] flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full bg-blue-600 text-center text-xs text-white transition duration-500 dark:bg-blue-500`}
-            >
-              {progress * 10}
-            </div>
-          </div>
-
-          <Button>Get Started</Button>
-          {progress}
-          <Button onClick={() => inc()}>+</Button>
-          <Button onClick={() => dec()}>-</Button>
+        <Progress value={progress * 10} className="h-2 w-full rounded-none" />
+        <div className="flex justify-between p-5">
+          <Button variant={"ghost"} onClick={() => setProgress(progress - 1)}>
+            Back
+          </Button>
+          <Button onClick={() => setProgress(progress + 1)}>Get Started</Button>
         </div>
       </div>
     </>
