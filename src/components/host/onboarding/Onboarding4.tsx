eee717a487c7043b0d8c5bfa076bd86b543c1290
zachuri <punsalang.zachary@gmail.com>
2024-04-03 22:21:41 -0700

    feature/new-host-signup-flow: setup new onboarding 4 with custom input form

--- src/components/host/onboarding/Onboarding4.tsx
+++ src/components/host/onboarding/Onboarding4.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { InputTogether } from "@/components/ui/input-together";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Onboarding4() {
   return (
-    <>
-      <div>Onboarding4</div>
-      <div>work in progress</div>
-    </>
-  )
+    <div className="mb-5 flex w-full flex-col items-center justify-center gap-5 max-lg:container">
+      <div className="mt-10 flex flex-col gap-5">
+        <h1 className="text-4xl font-bold">
+          Where&apos;s your property located?
+        </h1>
+
+        <Select>
+          <SelectTrigger className="">
+            <SelectValue placeholder="Country /region" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">United States</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <div className="rounded-lg border">
+          <InputTogether 
+            className="rounded-t-lg border-none"
+            placeholder="Steet address"
+          />
+          <InputTogether 
+            className="border-none"
+            placeholder="Apt, suite, unit (if applicable)"
+          />
+          < InputTogether className="border-none" placeholder="City / town" />
+          < InputTogether className="border-none" placeholder="State / territory" />
+          <InputTogether 
+            className="rounded-b-lg border-none"
+            placeholder="ZIP code"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/host/onboarding.tsx
+++ src/pages/host/onboarding.tsx
@@ -1,6 +1,7 @@
 import Onboarding1 from "@/components/host/onboarding/Onboarding1";
 import Onboarding2 from "@/components/host/onboarding/Onboarding2";
 import Onboarding3 from "@/components/host/onboarding/Onboarding3";
+import Onboarding4 from "@/components/host/onboarding/Onboarding4";
 import { useHostOnboarding } from "@/utils/store/host-onboarding";
 import OnboardingLayout from "../../components/host/onboarding/layout";
 
@@ -12,6 +13,7 @@ export default function Onboarding() {
       {progress === 0 && <Onboarding1 />}
       {progress === 1 && <Onboarding2 />}
       {progress === 2 && <Onboarding3 />}
+      {progress === 3 && <Onboarding4 />}
     </OnboardingLayout>
   );
 }
