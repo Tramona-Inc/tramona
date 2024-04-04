1fa2b5fdc76c8bbf82c61439cf03456532d364c6
annsts <an3245@nyu.edu>
2024-04-03 22:43:02 -0400

    form

---
+++ src/components/requests/RequestRefreshButton.tsx
@@ -0,0 +1,24 @@
+import React, { forwardRef } from 'react';
+import { Button } from "../ui/button";
+import { ReloadIcon } from '@radix-ui/react-icons';
+
+interface RequestRefreshButtonProps {
+  onClick: () => void;
+}
+
+export const RequestRefreshButton = forwardRef<HTMLButtonElement, RequestRefreshButtonProps>(({ onClick }, ref) => {
+  return (
+    <Button
+      ref={ref} 
+      className="rounded-full pr-3 flex items-center justify-center gap-2"
+      onClick={onClick}
+    >
+      <ReloadIcon className="icon-size" aria-hidden /> 
+      I don't like my offers
+    </Button>
+  );
+});
+
+RequestRefreshButton.displayName = 'RequestRefreshButton';
+
+
---
+++ src/components/requests/RequestRefreshForm.tsx
@@ -0,0 +1,404 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { api } from "@/utils/api";
+import { errorToast, successfulAdminOfferToast } from "@/utils/toasts";
+import { capitalize, getNumNights, plural } from "@/utils/utils";
+import { Textarea } from "../ui/textarea";
+import {
+  optional,
+  zodInteger,
+  zodNumber,
+  zodString,
+  zodUrl,
+} from "@/utils/zod-utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+import TagSelect from "../_common/TagSelect";
+import { type OfferWithProperty } from "../requests/[id]/OfferCard";
+import { useSession } from "next-auth/react";
+import ErrorMsg from "../ui/ErrorMsg";
+import axios from "axios";
+import {
+  ALL_PROPERTY_AMENITIES,
+  ALL_PROPERTY_SAFETY_ITEMS,
+  ALL_PROPERTY_STANDOUT_AMENITIES,
+  ALL_PROPERTY_TYPES,
+  type Request,
+} from "@/server/db/schema";
+
+const calculatePriceOptions = (oldPrice: number): number[] => {
+  if (oldPrice >= 0 && oldPrice < 400) {
+    return [50, 75, 100];
+  } else if (oldPrice >= 400 && oldPrice < 650) {
+    return [75, 100, 150];
+  } else if (oldPrice >= 650 && oldPrice < 900) {
+    return [100, 150, 200];
+  } else if (oldPrice >= 900) {
+    return [200, 225, 250];
+  }
+  return [50, 75, 100];
+};
+
+const formSchema = z.object({
+  preferences: zodString({ maxLen: Infinity }),
+  price: zodNumber(),
+  propertyLinks: z.object({ value: optional(zodUrl()) }).array(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+export default function RequestRefreshForm({
+  afterSubmit,
+  request,
+}: {
+  afterSubmit?: () => void;
+  request: Request;
+}) {
+  const { data: session } = useSession();
+  const updateRequest = api.requests.updateRequest.useMutation();
+  const {
+    data: checkResult,
+    isLoading,
+    isError,
+  } = api.requests.checkRequestUpdate.useQuery(
+    {
+      requestId: request.id,
+    },
+    {
+      enabled: !!request.id,
+    },
+  );
+
+  const numberOfNights = getNumNights(request.checkIn, request.checkOut);
+  const old_price = request
+    ? Math.round(request.maxTotalPrice / numberOfNights / 100)
+    : 1;
+
+  const priceOptions: number[] = calculatePriceOptions(old_price);
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      propertyLinks: [{ value: "" }],
+      price: 0,
+    },
+  });
+
+  const imageUrlInputs = useFieldArray({
+    name: "propertyLinks",
+    control: form.control,
+  });
+
+  const { watch } = form;
+
+  const onSubmit = async (data: FormSchema) => {
+    let submissionData = {
+      preferences: data.preferences,
+      updatedPriceNightlyUSD: data.price * 100,
+      propertyLinks: data.propertyLinks
+        .map((urlObj) => urlObj.value)
+        .filter((url): url is string => !!url),
+    };
+
+    await updateRequest.mutateAsync({
+      requestId: request.id,
+      updatedRequestInfo: submissionData,
+    });
+
+    afterSubmit?.();
+  };
+
+  const [currentStep, setCurrentStep] = useState(0);
+  const nextStep = async () => {
+    let isValid = false;
+    switch (currentStep) {
+      case 0:
+        isValid = await form.trigger("price");
+        break;
+      case 1:
+        isValid = await form.trigger("preferences");
+        break;
+      case 2:
+        isValid = true;
+        break;
+    }
+    if (isValid) {
+      setCurrentStep((prevStep) => Math.min(prevStep + 1, 3));
+    }
+  };
+
+  const prevStep = () =>
+    setCurrentStep((prevStep) => Math.max(prevStep - 1, 0));
+  const renderStep = () => {
+    switch (currentStep) {
+      case 0:
+        return (
+          <>
+            <h3 className="text-lg font-semibold">Refreshing your request</h3>
+            <div className="flex items-center m-2">
+              <div className="flex md:h-4 md:w-5 h-6 w-12 items-center text-xs justify-center rounded-full bg-black text-white mb-4">
+                1
+              </div>
+              <span className="ml-4 leading-5 text-base">
+                In order to get more offers for your request, you need to update
+                your request price.
+              </span>
+            </div>
+            <span className="text-gray-500">
+              We recommend that you update the price by
+            </span>
+            <div className="my-2 flex w-full justify-center space-x-2">
+              {priceOptions.map((price: number) => (
+                <button
+                  className={`w-full gap-x-4 rounded-lg border px-8 py-2 text-sm ${form.watch("price").toString() === price.toString() ? "border-blue-500 bg-blue-100" : "border-gray-400"}`}
+                  key={price}
+                  type="button"
+                  onClick={() =>
+                    form.setValue("price", price, { shouldValidate: true })
+                  }
+                >
+                  ${price}
+                </button>
+              ))}
+            </div>
+            <div className="flex justify-center">
+              <div className="my-2 flex w-5/6 items-center">
+                <div className="flex-1 border-t border-gray-200"></div>
+                <span className="px-3 text-neutral-900">or</span>
+                <div className="flex-1 border-t border-gray-200"></div>
+              </div>
+            </div>
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Enter custom price</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      onChange={(e) => {
+                        field.onChange(e.target.value);
+                      }}
+                      inputMode="decimal"
+                      prefix="$"
+                      placeholder="0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </>
+        );
+
+      case 1:
+        return (
+          <>
+            <h3 className="text-lg font-semibold">Additional Information</h3>
+            <div className="flex items-center m-2">
+              <div className="flex md:h-4 md:w-8 h-6 w-20 items-center text-xs justify-center rounded-full bg-black text-white md:mb-4 mb-10">
+                2
+              </div>
+              <span className="ml-4 leading-5 text-base">
+              Please enter your preferences on what you are looking for in your
+              new offers or tell us what you didn&apos;t like about your other
+              offers.
+              </span>
+            </div>
+            <FormField
+              control={form.control}
+              name="preferences"
+              render={({ field }) => (
+                <FormItem className="col-span-full">
+                  <FormControl>
+                    <Textarea {...field} className="resize-y" rows={10} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </>
+        );
+      case 2:
+        return (
+          <FormItem className="col-span-full space-y-1">
+            <div className="flex items-center m-2">
+              <div className="flex md:h-4 md:w-8 h-6 w-20 items-center text-xs justify-center rounded-full bg-black text-white md:mb-4 mb-8">
+                3
+              </div>
+              <span className="ml-4 leading-5 text-base">
+              Are you currently eyeing any properties on Airbnb/Vrbo? Enter the
+              link below and we will try to get you that exact stay at a
+              discount!
+              </span>
+            </div>
+
+            <FormLabel></FormLabel>
+            <div className="space-y-2 p-2">
+              {imageUrlInputs.fields.map((field, index) => (
+                <div key={field.id} className="relative">
+                  <FormField
+                    control={form.control}
+                    name={`propertyLinks.${index}.value`}
+                    render={({ field }) => (
+                      <FormItem className="mb-2">
+                        <FormControl>
+                          <Input
+                            {...field}
+                            inputMode="url"
+                            placeholder={`URL ${index + 1}`}
+                            className={`w-full rounded-md border px-4 py-2 focus:border-blue-500 focus:ring-blue-500 ${
+                              index !== 0 ? "pr-16" : ""
+                            }`}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {index !== 0 && (
+                    <button
+                      type="button"
+                      onClick={() => imageUrlInputs.remove(index)}
+                      className="absolute right-2 top-2 pt-0.5 text-sm text-red-500 hover:text-red-700"
+                    >
+                      Remove
+                    </button>
+                  )}
+                </div>
+              ))}
+              {imageUrlInputs.fields.length < 5 && (
+                <button
+                  type="button"
+                  className="text-blue-500 hover:underline"
+                  onClick={() => imageUrlInputs.append({ value: "" })}
+                >
+                  + Add additional
+                </button>
+              )}
+            </div>
+          </FormItem>
+        );
+
+      case 3:
+        return (
+          <div className="max-w-xl mx-auto p-4 bg-white rounded-lg border border-grey-500">
+            <h3 className="text-center text-xl font-semibold mb-4">
+              Review Your Details
+            </h3>
+            <div className="space-y-4">
+              <div className="flex justify-between items-center">
+                <strong>Price Preference Updated To:</strong>
+                <span className="text-gray-600">${watch("price") + old_price} per night</span>
+              </div>
+              {watch("preferences") && (
+                <div className="flex justify-between items-center">
+                  <strong>Your Additional Preferences:</strong>
+                  <p className="text-gray-600 truncate" title={watch("preferences")}>
+                    {watch("preferences").length > 50 ? watch("preferences").substring(0, 47) + '...' : watch("preferences")}
+                  </p>
+                </div>
+              )}
+              <div className="flex justify-between items-center">
+                <strong>Links:</strong>
+                <span className="text-gray-600">
+                  {watch("propertyLinks").some((url) => url.value?.trim() !== "")
+                    ? "Provided"
+                    : "None Provided"}
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+        
+        
+
+      default:
+        return null;
+    }
+  };
+
+  if (checkResult?.alreadyUpdated) {
+    return (
+      <>
+        <h1 className="text-center text-lg font-semibold">Thank you! </h1>
+        <hr className="my-2 h-px border-0 bg-gray-200 dark:bg-gray-700"></hr>
+        <div className="my-10">
+          <div className="p-18">
+            <p>
+              You've already updated your request. We'll get back to you
+              shortly.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <div>
+          {currentStep > 0 && (
+            <div>
+              <button
+                onClick={prevStep}
+                className="flex items-center gap-x-2 py-2 text-sm text-gray-700 sm:w-auto"
+              >
+                <svg
+                  className="h-4 w-4 rtl:rotate-180"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 16"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 1L2 8L9 15"
+                  />
+                </svg>
+                <span>Go back</span>
+              </button>
+              <hr className="my-2 h-px border-0 bg-gray-200 dark:bg-gray-700"></hr>
+            </div>
+          )}
+        </div>
+
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          {renderStep()}
+
+          <div className="mt-16 flex justify-center">
+            {currentStep < 3 && (
+              <Button type="button" onClick={nextStep}>
+                Next
+              </Button>
+            )}
+            {currentStep === 3 && <Button type="submit">Submit</Button>}
+          </div>
+        </form>
+
+        <div className="mt-4 flex justify-center space-x-1">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <div
+              key={index}
+              className={`h-2 w-2 rounded-full ${currentStep === index ? "bg-black" : "bg-gray-300"}`}
+            />
+          ))}
+        </div>
+      </Form>
+    </>
+  );
+}
