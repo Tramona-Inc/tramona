4ea46487cd17a4eed78a989637edd655d1f91b28
Ben Tomlin <bentomlin101@gmail.com>
2024-04-08 06:17:26 -0700

    add guesty properties + tweak/fix offer page ui

--- src/components/offers/OfferPage.tsx
+++ src/components/offers/OfferPage.tsx
@@ -111,16 +111,25 @@ export default function OfferPage({
           </h1>
           <div className="space-y-2">
             <div className="flex flex-wrap items-center gap-1">
-              <Badge variant="secondary" icon={<StarFilledIcon />}>
-                {property.avgRating} ({property.numRatings})
-              </Badge>
+              {property.numRatings > 0 && (
+                <Badge variant="secondary" icon={<StarFilledIcon />}>
+                  {property.avgRating} ({property.numRatings})
+                </Badge>
+              )}
               <Badge variant="secondary">
                 {plural(property.numBedrooms, "bedroom")}
               </Badge>
               <Badge variant="secondary">
                 {plural(property.numBeds, "bed")}
               </Badge>
-              <Badge variant="secondary">{property.propertyType}</Badge>
+              {property.numBathrooms && (
+                <Badge variant="secondary">
+                  {plural(property.numBathrooms, "bathroom")}
+                </Badge>
+              )}
+              <Badge variant="secondary">{property.roomType}</Badge>
+            </div>
+            <div className="flex flex-wrap items-center gap-1">
               {property.amenities.map((amenity) => (
                 <Badge variant="secondary" key={amenity}>
                   {amenity}
