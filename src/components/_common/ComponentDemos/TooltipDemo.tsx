5a2a56442952eeafa5ee5418f6f360514be290de
Ben Tomlin <bentomlin101@gmail.com>
2024-04-11 18:02:44 -0700

    remove tooltip provider (alr one in root layout)

--- src/components/_common/ComponentDemos/TooltipDemo.tsx
+++ src/components/_common/ComponentDemos/TooltipDemo.tsx
@@ -1,22 +1,19 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/ui/tooltip";
 
 export function TooltipDemo() {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant="outline">Hover</Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Add to library</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  )
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline">Hover</Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Add to library</p>
+      </TooltipContent>
+    </Tooltip>
+  );
 }
