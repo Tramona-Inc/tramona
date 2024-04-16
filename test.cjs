6045e7dd4f193b01e1e1b94419e5da5d0f1ac065
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-04-16 10:49:39 -0700

    added url scrape

--- test.cjs
+++ test.cjs
@@ -1,9 +1,26 @@
-const scrapedData = require('./scraped-dates-minus.json');
+const scrapedData = require('./scrape-url.json');
 
 const keysArray = Object.keys(scrapedData);
-const start = keysArray.indexOf(`Ozark Spring Cabin Rock Bluff 03 King Bed, Spa Tub`);
-const end = keysArray.indexOf(`New Remodel, Slps 2, Modern feel, Historic Town`)
-console.log(end - start);
+
+// const start = keysArray.indexOf(`ATL Downtown! City View! Onsite Parking 1B1B!`);
+// const end = keysArray.indexOf(`Atlanta Downtown City View and Free Parking`)
+// console.log(start, end);
+
+const seenKeys = {};
+let hasDuplicates = false;
+
+for (let key of keysArray) {
+    if (seenKeys[key]) {
+        console.log(`Duplicate key found: ${key}`);
+        hasDuplicates = true;
+    } else {
+        seenKeys[key] = true;
+    }
+}
+
+if (!hasDuplicates) {
+    console.log("No duplicate keys found.");
+}
 
 
 console.log(Object.keys(scrapedData).length);
