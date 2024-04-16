9192406a47f527fd307b82815c4f4f6f7ea8a9b3
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-04-15 22:55:13 -0700

    removed unused imports

--- src/components/host/onboarding/Summary7.tsx
+++ src/components/host/onboarding/Summary7.tsx
@@ -1,5 +1,4 @@
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useHostOnboarding } from "@/utils/store/host-onboarding";
 import { useState } from "react";
 
