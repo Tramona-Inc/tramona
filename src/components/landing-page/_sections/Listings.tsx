56d98080d52c282fac0e08d0d0d90c8c1d248a8b
zachuri <punsalang.zachary@gmail.com>
2024-04-19 15:20:29 -0700

    feature/home-page-query: fix the layout to show properties

--- src/components/landing-page/_sections/Listings.tsx
+++ src/components/landing-page/_sections/Listings.tsx
@@ -13,7 +13,7 @@ export default function Listings() {
   } = api.properties.getAllInfiniteScroll.useInfiniteQuery(
     {},
     {
-      // the cursor from where to start fetching the posts
+      // the cursor from where to start fetching thecurrentProperties 
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     },
   );
@@ -27,7 +27,7 @@ export default function Listings() {
   });
 
   useEffect(() => {
-    // if the user reaches the bottom of the page, and there are more posts to fetch, fetch them
+    // if the user reaches the bottom of the page, and there are more currentProperties to fetch, fetch them
     if (
       entry?.isIntersecting &&
       properties?.pages.length &&
@@ -38,8 +38,8 @@ export default function Listings() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [entry]);
 
-  // memoize the posts, so that they don't get re-rendered on every re-render
-  const posts = useMemo(
+  // memoize the currentProperties, so that they don't get re-rendered on every re-render
+  const currentProperties = useMemo(
     () => properties?.pages.flatMap((page) => page.data) ?? [],
     [properties],
   );
@@ -58,51 +58,49 @@ export default function Listings() {
     //     <Spinner />
     //   )}
     // </div>
-    <section className="flex justify-center p-5">
-      <div className="grid gap-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {isLoading ? (
-          // if we're still fetching the initial posts, display the loader
-          <div className="flex justify-center">
-            <Spinner />
-          </div>
-        ) : !!posts.length ? (
-          // if there are posts to show, display them
-          <>
-            {posts.map((post, i) => (
-              <>
-                {i === posts.length - 1 ? (
-                  <div ref={ref} key={post.id} className="cursor-pointer">
-                    <HomeOfferCard key={post.id} property={post} />
-                  </div>
-                ) : (
-                  <div key={post.id} className="cursor-pointer">
-                    <HomeOfferCard property={post} />
-                  </div>
-                )}
-              </>
-            ))}
-
-            {isFetchingNextPage && (
-              <div className="flex justify-center">
-                <Spinner />
-              </div>
-            )}
-
-            {!isFetchingNextPage &&
-              properties?.pages.length &&
-              !properties.pages[properties.pages.length - 1]?.nextCursor && (
-                <div className="text-center opacity-60">
-                  <p className="text-xs md:text-sm">No more posts to load</p>
+    <section className="grid gap-10 gap-y-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      {isLoading ? (
+        // if we're still fetching the initial currentProperties, display the loader
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      ) : !!currentProperties.length ? (
+        // if there are currentProperties to show, display them
+        <>
+          {currentProperties.map((post, i) => (
+            <>
+              {i === currentProperties.length - 1 ? (
+                <div ref={ref} key={post.id} className="cursor-pointer">
+                  <HomeOfferCard key={post.id} property={post} />
+                </div>
+              ) : (
+                <div key={post.id} className="cursor-pointer">
+                  <HomeOfferCard property={post} />
                 </div>
               )}
-          </>
-        ) : (
-          // if there are no posts to show, display a message
-          <div className="flex justify-center">
-            <p className="text-sm text-white/60">No posts to show</p>
-          </div>
-        )}
-      </div>
+            </>
+          ))}
+
+          {isFetchingNextPage && (
+            <div className="flex justify-center">
+              <Spinner />
+            </div>
+          )}
+
+          {!isFetchingNextPage &&
+            properties?.pages.length &&
+            !properties.pages[properties.pages.length - 1]?.nextCursor && (
+              <div className="text-center opacity-60">
+                <p className="text-xs md:text-sm">No more currentProperties to load</p>
+              </div>
+            )}
+        </>
+      ) : (
+        // if there are no currentProperties to show, display a message
+        <div className="flex justify-center">
+          <p className="text-sm text-white/60">No currentProperties to show</p>
+        </div>
+      )}
     </section>
   );
 }
