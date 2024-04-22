864533a3e536e9a3fb37c2fc8289c2fdee85e7f0
Manas Acharekar <manas.acharekar98@gmail.com>
2024-04-22 19:30:00 -0400

    mobile search bar progress

--- src/components/landing-page/SearchBar/SearchBar.tsx
+++ src/components/landing-page/SearchBar/SearchBar.tsx
@@ -1,9 +1,11 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import DesktopSearchBar from "../SearchBar/DesktopSearchBar";
+import MobileSearchBar from "./MobileSearchBar";
 
 export default function SearchBar() {
   return (
     <>
+      {/* Desktop search bar */}
       <div className="flex items-center justify-center bg-white">
         <Tabs defaultValue={"search"}>
           <TabsList noBorder className="flex items-center justify-center">
@@ -22,6 +24,11 @@ export default function SearchBar() {
           </TabsContent>
         </Tabs>
       </div>
+
+      {/* Mobile search bar */}
+      {/* <div className="lg:hidden">
+        <MobileSearchBar />
+      </div> */}
     </>
   );
 }
