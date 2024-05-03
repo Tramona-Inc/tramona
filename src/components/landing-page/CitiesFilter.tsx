c30830634e72a4512c1909bdd16f83bd3153a73f
zachuri <punsalang.zachary@gmail.com>
2024-05-03 12:18:40 -0700

    feature/bidding-flow: updated the cities filter names

--- src/components/landing-page/CitiesFilter.tsx
+++ src/components/landing-page/CitiesFilter.tsx
@@ -47,14 +47,13 @@ export const cities: CitiesLatLong[] = [
   { id: "atlanta", label: "Atlanta", long: -84.388, lat: 33.749 },
   { id: "austin", label: "Austin", long: -97.7431, lat: 30.2672 },
   { id: "miami", label: "Miami", long: -80.1917902, lat: 25.7616798 },
-  { id: "la", label: "LA", long: -118.2437, lat: 34.0522 },
   {
     id: "palm_springs_area",
     label: "Palm Springs Area",
     long: -116.5453,
     lat: 33.8303,
   },
-  { id: "vegas", label: "Vegas", long: -115.1398, lat: 36.1699 },
+  { id: "las vegas", label: "Las Vegas", long: -115.1398, lat: 36.1699 },
   { id: "sf", label: "San francisco", long: -122.4194, lat: 37.7749 },
   { id: "boston", label: "Boston", long: -71.0589, lat: 42.3601 },
 ];
