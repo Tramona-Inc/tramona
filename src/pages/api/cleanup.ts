2c7701b3b6308e4523a10910ad33c90a3a5e897f
Ben Tomlin <bentomlin101@gmail.com>
2024-05-08 16:03:58 -0700

    add cleanup cron + rename the other cron

---
+++ src/pages/api/cleanup.ts
@@ -0,0 +1,7 @@
+import { db } from "@/server/db";
+import { bookedDates } from "@/server/db/schema";
+import { lt } from "drizzle-orm";
+
+export default async function handler() {
+  await db.delete(bookedDates).where(lt(bookedDates.date, new Date()));
+}
