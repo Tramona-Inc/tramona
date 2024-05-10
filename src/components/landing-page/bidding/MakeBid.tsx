e802d2a0d6a7bc2c0ae342f6cf7d2f691728decd
zachuri <punsalang.zachary@gmail.com>
2024-05-10 15:36:41 -0700

    feature/coutner: commented all the parts of verification

--- src/components/landing-page/bidding/MakeBid.tsx
+++ src/components/landing-page/bidding/MakeBid.tsx
@@ -1,13 +1,9 @@
 import Spinner from "@/components/_common/Spinner";
-import IdentityModal from "@/components/_utils/IdentityModal";
 import { Button } from "@/components/ui/button";
 import { api } from "@/utils/api";
 import { useBidding } from "@/utils/store/bidding";
 import { cn } from "@/utils/utils";
 import { ChevronLeft } from "lucide-react";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 import BiddingConfirmation from "./BiddingConfirmation";
 import BiddingStep1 from "./BiddingStep1";
 import BiddingStep2 from "./BiddingStep2";
@@ -20,61 +16,62 @@ function MakeBid({ propertyId }: { propertyId: number }) {
   const step = useBidding((state) => state.step);
   const setStep = useBidding((state) => state.setStep);
 
-  const [message, setMessage] = useState("");
-  const { status, data: session, update } = useSession();
+  // ! Later make the verification not skiappable
+  // ! Uncomment to display verificaiton
+  // const [message, setMessage] = useState("");
+  // const { status, data: session, update } = useSession();
   // const verificationStatus = session?.user.isIdentityVerified;
-  const verificationStatus = "true"; // ! Later make it not skippable
 
-  useEffect(() => {
-    switch (verificationStatus) {
-      case "pending":
-        void update();
-        // setMessage(
-        //   "Your identity verification is still pending. Please allow 2-3 minutes for processing. Contact support if this takes longer.",
-        // );
-        break;
-      case "false":
-        setMessage("To start making offers, help us verify your identity.");
-        break;
-      // case "true":
-      //   setMessage(""); // Reset message if verified
-      //   setCompleted(true); // Set completed to true
-      //   break;
-    }
-  }, [verificationStatus]);
+  // useEffect(() => {
+  //   switch (verificationStatus) {
+  //     case "pending":
+  //       void update();
+  //       // setMessage(
+  //       //   "Your identity verification is still pending. Please allow 2-3 minutes for processing. Contact support if this takes longer.",
+  //       // );
+  //       break;
+  //     case "false":
+  //       setMessage("To start making offers, help us verify your identity.");
+  //       break;
+  //     // case "true":
+  //     //   setMessage(""); // Reset message if verified
+  //     //   setCompleted(true); // Set completed to true
+  //     //   break;
+  //   }
+  // }, [verificationStatus]);
 
-  // if (verificationStatus === "pending") {
-  //   void update();
+  // // if (verificationStatus === "pending") {
+  // //   void update();
+  // // }
+
+  // // if (verificationStatus === "false" || verificationStatus === "pending") {
+  // if (verificationStatus === "false") {
+  //   return (
+  //     <>
+  //       <div className="flex flex-col items-center">
+  //         <h1 className="text-md font-semibold tracking-tight md:text-3xl">
+  //           Identity Verification
+  //         </h1>
+  //         <div style={{ position: "relative", width: "50%", height: "200px" }}>
+  //           <Image
+  //             src="/assets/images/welcome/identity.png"
+  //             alt="Identity Verification"
+  //             layout="fill"
+  //             quality={100}
+  //             className="object-cover object-center"
+  //           />
+  //         </div>
+
+  //         <div className="mx-auto max-w-md">
+  //           <p className="my-2 break-words text-base text-black">{message}</p>
+  //         </div>
+
+  //         <hr className="mx-auto my-4 h-px w-[90%] border-0 bg-gray-300 md:my-10"></hr>
+  //         <IdentityModal />
+  //       </div>
+  //     </>
+  //   );
   // }
-
-  // if (verificationStatus === "false" || verificationStatus === "pending") {
-  if (verificationStatus === "false") {
-    return (
-      <>
-        <div className="flex flex-col items-center">
-          <h1 className="text-md font-semibold tracking-tight md:text-3xl">
-            Identity Verification
-          </h1>
-          <div style={{ position: "relative", width: "50%", height: "200px" }}>
-            <Image
-              src="/assets/images/welcome/identity.png"
-              alt="Identity Verification"
-              layout="fill"
-              quality={100}
-              className="object-cover object-center"
-            />
-          </div>
-
-          <div className="mx-auto max-w-md">
-            <p className="my-2 break-words text-base text-black">{message}</p>
-          </div>
-
-          <hr className="mx-auto my-4 h-px w-[90%] border-0 bg-gray-300 md:my-10"></hr>
-          <IdentityModal />
-        </div>
-      </>
-    );
-  }
 
   return (
     <>
