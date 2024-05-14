317e56c94774847e75ba43ec87f3b43e0dc200a1
zachuri <punsalang.zachary@gmail.com>
2024-05-14 11:54:35 -0700

    feature/settings: able to display the cards

---
+++ src/components/settings/AddPaymentInfoForm.tsx
@@ -0,0 +1,97 @@
+import { Button } from "@/components/ui/button";
+import { api } from "@/utils/api";
+import {
+  AddressElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { type StripeError } from "@stripe/stripe-js";
+import { useState } from "react";
+
+export default function AddPaymentInfoForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [errorMessage, setErrorMessage] = useState<String | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  const { mutateAsync: confirmSetupIntentMutation } =
+    api.stripe.confirmSetupIntent.useMutation();
+
+  const { mutateAsync: createSetupIntentMutation } =
+    api.stripe.createSetupIntent.useMutation();
+
+  const { mutateAsync: createBiddingMutate } =
+    api.biddings.create.useMutation();
+
+  const handleError = (error: StripeError) => {
+    setLoading(false);
+    setErrorMessage(error.message);
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setLoading(true);
+
+    // Trigger form validation and wallet collection
+    const { error: submitError } = await elements.submit();
+    if (submitError) {
+      handleError(submitError);
+      return;
+    }
+
+    // Create payment method using the elements.
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      elements,
+    });
+
+    if (error) {
+      setErrorMessage(error.message ?? "An error occurred.");
+      return;
+    }
+
+    // If payment method is successfully created, proceed with setup intent confirmation.
+    const setupIntent = await createSetupIntentMutation({
+      paymentMethod: paymentMethod.id,
+    });
+
+    if (setupIntent) {
+      await confirmSetupIntentMutation({
+        setupIntent: setupIntent.id,
+      });
+    }
+
+    console.log("reached");
+
+    if (error) {
+      // This point is only reached if there's an immediate error when
+      // confirming the payment. Show the error to your customer (for example, payment details incomplete)
+      handleError(error);
+    } else {
+      // Your customer is redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer is redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <AddressElement options={{ mode: "billing" }} />
+      <PaymentElement />
+      <Button className={"mt-5"} type="submit" disabled={!stripe || loading}>
+        Add Payment Method
+      </Button>
+      {errorMessage && <div>{errorMessage}</div>}
+    </form>
+  );
+}
--- src/pages/settings/payment-information.tsx
+++ src/pages/settings/payment-information.tsx
@@ -1,11 +1,53 @@
 import SettingsLayout from "@/components/_common/Layout/SettingsLayout";
+import AddPaymentInfoForm from "@/components/settings/AddPaymentInfoForm";
+import { api } from "@/utils/api";
+import { useStripe } from "@/utils/stripe-client";
+import { Elements } from "@stripe/react-stripe-js";
+import { StripeElementsOptions } from "@stripe/stripe-js";
+import Stripe from "stripe";
+
+function Card({ payment }: { payment: Stripe.PaymentMethod }) {
+  return (
+    <div key={payment.id} className="flex justify-between">
+      **** **** **** {payment.card?.last4}{" "}
+      <span className="capitalize">{payment.card?.brand}</span>{" "}
+      {payment.card?.exp_month}/{payment.card?.exp_year}
+    </div>
+  );
+}
 
 export default function PaymentInformation() {
+  const options: StripeElementsOptions = {
+    mode: "setup",
+    currency: "usd",
+    paymentMethodCreation: "manual",
+  };
+
+  const stripePromise = useStripe();
+
+  const { data: payments } = api.stripe.getListOfPayments.useQuery();
+
   return (
     <SettingsLayout>
-      <div className="mx-auto my-8 max-w-4xl">
+      <div className="mx-auto my-8 max-w-4xl space-y-5">
         <div className="space-y-4 rounded-lg border bg-white p-4">
           <h1 className="text-lg font-bold">Payment Method</h1>
+
+          <Elements stripe={stripePromise} options={options}>
+            <AddPaymentInfoForm />
+          </Elements>
+        </div>
+
+        <div className="space-y-4 rounded-lg border bg-white p-4">
+          <h1 className="text-lg font-bold">Saved Cards</h1>
+
+          {payments && payments.cards.data.length > 0 && (
+            <div className="space-y-5">
+              {payments.cards.data.map((payment) => (
+                <Card key={payment.id} payment={payment} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </SettingsLayout>
