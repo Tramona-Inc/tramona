70ad6ab64ee9c411c49329766be670f0a736b25d
Ben Tomlin <bentomlin101@gmail.com>
2024-05-14 15:59:25 -0700

    fix infinite scrolling

--- src/components/landing-page/_sections/Listings.tsx
+++ src/components/landing-page/_sections/Listings.tsx
@@ -72,23 +72,18 @@ export default function Listings() {
   ));
 
   return (
-    <section className="grid grid-cols-1 gap-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <section className="relative grid grid-cols-1 gap-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {isLoading ? (
         // if we're still fetching the initial currentProperties, display the loader
         <>{skeletons}</>
       ) : currentProperties.length > 0 ? (
         // if there are currentProperties to show, display them
         <>
-          {currentProperties.map((property, i) => (
-            <div
-              ref={i === currentProperties.length - 1 ? ref : undefined}
-              key={property.id}
-            >
-              <HomeOfferCard property={property} />
-            </div>
+          {currentProperties.map((property) => (
+            <HomeOfferCard key={property.id} property={property} />
           ))}
-
           {isFetchingNextPage && skeletons}
+          <div ref={ref} className="absolute bottom-[200vh]"></div>
         </>
       ) : (
         <div className="col-span-full flex min-h-80 items-center justify-center gap-4">
