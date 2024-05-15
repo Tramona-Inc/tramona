0a66862cc7d516a4cf59755a99994fbe85f13f30
Ben Tomlin <bentomlin101@gmail.com>
2024-05-14 17:14:49 -0700

    fix signed out state some more

--- src/components/landing-page/HomeOfferCard.tsx
+++ src/components/landing-page/HomeOfferCard.tsx
@@ -29,6 +29,7 @@ import { Button } from "../ui/button";
 import MakeBid from "./bidding/MakeBid";
 import { signIn, useSession } from "next-auth/react";
 import { api } from "@/utils/api";
+import { Plus } from "lucide-react";
 
 function Dot({ isCurrent }: { isCurrent: boolean }) {
   return (
@@ -153,103 +154,136 @@ export default function HomeOfferCard({
   const alreadyBid = propertyIdBids.includes(property.id);
 
   return (
-    <div className="space-y-2">
-      <Carousel setApi={setCarouselApi}>
-        <CarouselContent>
-          {property.imageUrls.slice(0, 5).map((photo, index) => (
-            <CarouselItem key={index}>
-              <CardContent>
-                <Link href={`/property/${property.id}`}>
-                  <Image
-                    src={photo}
-                    height={300}
-                    width={300}
-                    alt=""
-                    className="aspect-square w-full rounded-xl object-cover"
-                  />
-                </Link>
-              </CardContent>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious
-          className="absolute left-2 top-1/2 size-6"
-          variant={"white"}
-        />
-        <CarouselNext
-          className="absolute right-2 top-1/2 size-6"
-          variant={"white"}
-        />
-        <CarouselDots count={count} current={current} />
-      </Carousel>
-      <div className="flex flex-col">
-        <p className="max-w-full overflow-hidden text-ellipsis text-nowrap font-semibold">
-          {property.name}
-        </p>
-        {property.originalNightlyPrice && (
-          <p>
-            <span className="text-xs">Airbnb Price: </span>
-            {formatCurrency(AVG_AIRBNB_MARKUP * property.originalNightlyPrice)}
-            <span className="text-xs">/night</span>
+    <div className="relative">
+      <div className="space-y-2">
+        <Carousel setApi={setCarouselApi}>
+          <CarouselContent>
+            {property.imageUrls.slice(0, 5).map((photo, index) => (
+              <CarouselItem key={index}>
+                <CardContent>
+                  <Link href={`/property/${property.id}`}>
+                    <Image
+                      src={photo}
+                      height={300}
+                      width={300}
+                      alt=""
+                      className="aspect-square w-full rounded-xl object-cover"
+                    />
+                  </Link>
+                </CardContent>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious
+            className="absolute left-2 top-1/2 size-6"
+            variant={"white"}
+          />
+          <CarouselNext
+            className="absolute right-2 top-1/2 size-6"
+            variant={"white"}
+          />
+          <CarouselDots count={count} current={current} />
+        </Carousel>
+        <div className="flex flex-col">
+          <p className="max-w-full overflow-hidden text-ellipsis text-nowrap font-semibold">
+            {property.name}
           </p>
+          {property.originalNightlyPrice && (
+            <p>
+              <span className="text-xs">Airbnb Price: </span>
+              {formatCurrency(
+                AVG_AIRBNB_MARKUP * property.originalNightlyPrice,
+              )}
+              <span className="text-xs">/night</span>
+            </p>
+          )}
+        </div>
+        <p className="text-xs">
+          {plural(property.maxNumGuests, "guest")},{" "}
+          {plural(property.numBedrooms, "bedroom")},{" "}
+          {plural(property.numBeds, "bed")}
+          {property.numBathrooms && (
+            <>, {plural(property.numBathrooms, "bath")}</>
+          )}
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-2"
+          >
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <DateRangePicker
+                      {...field}
+                      propertyId={property.id}
+                      disablePast
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Dialog open={open} onOpenChange={setOpen}>
+              {/* Removed trigger to have control on open and close */}
+              <div>
+                {alreadyBid ? (
+                  <Button
+                    type={"submit"}
+                    className={"w-full rounded-xl"}
+                    disabled={alreadyBid}
+                  >
+                    Already Bid
+                  </Button>
+                ) : (
+                  <Button
+                    type={"submit"}
+                    className={`w-full rounded-xl ${!form.formState.isValid && "bg-black"}`}
+                    // disabled={!form.formState.isValid}
+                  >
+                    Make Offer
+                  </Button>
+                )}
+              </div>
+              <DialogContent className="flex sm:max-w-lg  md:max-w-fit md:px-36 md:py-10">
+                <MakeBid propertyId={property.id} setOpen={setOpen} />
+              </DialogContent>
+            </Dialog>
+          </form>
+        </Form>
+      </div>
+      <div className="absolute right-2 top-2">
+        {!isInBucketList && (
+          <Button
+            tooltip={
+              isLoggedIn ? undefined : "Please log in to add to bucket list"
+            }
+            onClick={() =>
+              isLoggedIn
+                ? addPropertyToBucketList({
+                    propertyId: property.id,
+                  })
+                : signIn()
+            }
+            variant="white"
+            className="rounded-full"
+          >
+            <Plus />
+            Add to bucket list
+          </Button>
+        )}
+        {isInBucketList && (
+          <Button
+            onClick={() => removePropertyFromBucketList(property.id)}
+            className="rounded-full bg-[#333333]/90 hover:bg-[#333333]"
+          >
+            Added to bucket list
+          </Button>
         )}
       </div>
-      <p className="text-xs">
-        {plural(property.maxNumGuests, "guest")},{" "}
-        {plural(property.numBedrooms, "bedroom")},{" "}
-        {plural(property.numBeds, "bed")}
-        {property.numBathrooms && (
-          <>, {plural(property.numBathrooms, "bath")}</>
-        )}
-      </p>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-2"
-        >
-          <FormField
-            control={form.control}
-            name="date"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <DateRangePicker
-                    {...field}
-                    propertyId={property.id}
-                    disablePast
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Dialog open={open} onOpenChange={setOpen}>
-            {/* Removed trigger to have control on open and close */}
-            <div>
-              {alreadyBid ? (
-                <Button
-                  type={"submit"}
-                  className={"w-full rounded-xl"}
-                  disabled={alreadyBid}
-                >
-                  Already Bid
-                </Button>
-              ) : (
-                <Button
-                  type={"submit"}
-                  className={`w-full rounded-xl ${!form.formState.isValid && "bg-black"}`}
-                  // disabled={!form.formState.isValid}
-                >
-                  Make Offer
-                </Button>
-              )}
-            </div>
-            <DialogContent className="flex sm:max-w-lg  md:max-w-fit md:px-36 md:py-10">
-              <MakeBid propertyId={property.id} setOpen={setOpen} />
-            </DialogContent>
-          </Dialog>
-        </form>
-      </Form>
     </div>
   );
 }
