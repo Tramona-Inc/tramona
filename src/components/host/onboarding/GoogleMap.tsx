27a63f416f01bd4e5699ef6208390d34dd45218d
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-05-20 17:41:50 -0700

    merge with dev

--- src/components/host/onboarding/GoogleMap.tsx
+++ src/components/host/onboarding/GoogleMap.tsx
@@ -1,37 +1,39 @@
-import { Map, Marker, InfoWindow, type IMapProps } from "google-maps-react";
+import { Map, Marker, type IMapProps } from "google-maps-react";
 import React, { useState } from "react";
 
 type CoordinateDataProps = {
   lat: number | undefined;
   lng: number | undefined;
-  draggable: boolean;
 };
 
-function GoogleMap({ lat, lng, draggable }: CoordinateDataProps) {
+function GoogleMap({ lat, lng }: CoordinateDataProps) {
   const [markerPosition, setMarkerPosition] = useState({
-  	lat: lat ?? 37.774929,
-  	lng: lng ?? -122.419416,
+    lat: lat ?? 37.774929,
+    lng: lng ?? -122.419416,
   });
 
+  const onMarkerDragend = (coord: {
+    latLng: { lat: () => number; lng: () => number };
+  }) => {
+    const lat = coord.latLng.lat();
+    const lng = coord.latLng.lng();
 
-  const onMarkerDragend = (coord: { latLng: { lat: () => number; lng: () => number; }; }) => {
-  	const lat = coord.latLng.lat();
-  	const lng = coord.latLng.lng();
+    // Update marker position
+    setMarkerPosition({ lat, lng });
 
-  	// Update marker position
-  	setMarkerPosition({ lat, lng });
-
-  	// Display latitude and longitude
-  	console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    // Display latitude and longitude
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
   };
 
-  const onMapClick = (clickEvent: { latLng: { lat: () => number; lng: () => number; }; }, _mapProps?: IMapProps, _map?: google.maps.Map) => {
+  const onMapClick = (
+    clickEvent: { latLng: { lat: () => number; lng: () => number } },
+    _mapProps?: IMapProps,
+    _map?: google.maps.Map,
+  ) => {
     const lat = clickEvent.latLng.lat();
     const lng = clickEvent.latLng.lng();
     setMarkerPosition({ lat, lng });
-  }
-
-
+  };
 
   return (
     <div>
@@ -47,14 +49,14 @@ function GoogleMap({ lat, lng, draggable }: CoordinateDataProps) {
             <Marker
               position={markerPosition}
               draggable={true}
-              onDragend={(coord: { latLng: { lat: () => number; lng: () => number; }; }) => onMarkerDragend(coord)}
-           />
-
-
-            </Map>
-         </div>
+              onDragend={(coord: {
+                latLng: { lat: () => number; lng: () => number };
+              }) => onMarkerDragend(coord)}
+            />
+          </Map>
+        </div>
       )}
-    </>
+    </div>
   );
 }
 
