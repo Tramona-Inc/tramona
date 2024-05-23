fad7705fc62fe74babd499bb866a17d875973736
zachuri <punsalang.zachary@gmail.com>
2024-05-23 16:51:00 -0700

    feature/logout-state-main-listing: default added a size for the loading state

--- src/components/landing-page/bidding/BidPaymentForm.tsx
+++ src/components/landing-page/bidding/BidPaymentForm.tsx
@@ -123,7 +123,7 @@ export default function BidPaymentForm({
   }, [elements]);
 
   return (
-    <div className='flex items-center justify-center'>
+    <div className="relative flex items-center justify-center">
       {!session?.user && (
         <div className="absolute bottom-52 z-10 flex flex-col gap-5 rounded-lg border bg-white p-5">
           <DialogHeader>
@@ -142,6 +142,7 @@ export default function BidPaymentForm({
       )}
       <div className={cn(!session?.user.id && "blur-sm filter")}>
         <form onSubmit={handleSubmit} className="space-y-5">
+          {isStripeLoading && <div className="h-[400px]" />}
           <AddressElement options={{ mode: "billing" }} />
           <PaymentElement />
           <Button type="submit" disabled={!stripe || loading}>
