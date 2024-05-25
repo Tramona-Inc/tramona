63914f01f81984ca3615c37d4ded809b482d435c
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-05-25 12:45:39 -0700

    Fixed nav links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import DashboardLayout from "@/components/_common/Layout/DashboardLayout";
 import SearchPage from "@/components/landing-page/search/SearchPage";
+import TravelerPage from "@/components/landing-page/TravelerPage";
 
 export default function Home() {
   return (
     <DashboardLayout type={"guest"}>
-      <SearchPage />
+      <TravelerPage />
     </DashboardLayout>
   );
 }
