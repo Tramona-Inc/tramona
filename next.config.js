01ea702d47ee1097d081e11b7ee86b4a07539ff6
Ben Tomlin <bentomlin101@gmail.com>
2024-05-28 08:54:31 -0700

    .

--- next.config.js
+++ next.config.js
@@ -102,6 +102,10 @@ const config = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "https",
+        hostname: "tramona-images.s3.amazonaws.com",
+      },
     ],
   },
   experimental: {
