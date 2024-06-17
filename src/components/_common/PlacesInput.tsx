33563ab57277f69dd74a452b713e15cbed60aef7
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-06-17 13:47:06 -0700

    deleted input-button-map ui component

--- src/components/_common/PlacesInput.tsx
+++ src/components/_common/PlacesInput.tsx
@@ -1,7 +1,7 @@
 import { FormField, FormItem, FormMessage } from "../ui/form";
 import { useState } from "react";
-import { InputButtonMap } from "../ui/input-button-map";
 import { type InputVariant } from "../ui/input";
+import { InputButton } from "../ui/input-button";
 import { cn } from "@/utils/utils";
 import { api } from "@/utils/api";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
@@ -23,23 +23,18 @@ export default function PlacesInput({
   placeholder?: string;
   variant?: InputVariant;
   icon?: React.FC<{ className?: string }>;
-  onSave: (location: { lat: number; lng: number; }, radius: number) => void;
+  onSave: (location: { lat: number; lng: number }, radius: number) => void;
 }) {
   const [open, setOpen] = useState(false);
   const utils = api.useUtils();
   const [isMapOpen, setIsMapOpen] = useState(false);
-  const [initialLocation, setInitialLocation] = useState(null);
+  const [initialLocation, setInitialLocation] = useState({lat: 0, lng: 0});
 
   return (
     <FormField
       {...props}
       render={({ field }) => (
-        <FormItem
-          className={cn(
-            "relative",
-            className,
-          )}
-        >
+        <FormItem className={cn("relative", className)}>
           <div className="flex items-center">
             <div className="flex-grow">
               <PlacesPopover
@@ -59,7 +54,7 @@ export default function PlacesInput({
                 }}
                 className="w-96 -translate-y-11 overflow-clip px-0 pt-0"
                 trigger={({ value }) => (
-                  <InputButtonMap
+                  <InputButton
                     withClearBtn
                     variant={variant}
                     label={formLabel}
@@ -70,15 +65,33 @@ export default function PlacesInput({
                     role="combobox"
                     icon={icon}
                     className="bg-white"
-                    onSave={onSave}
-                    initialLocation={initialLocation}
-                    setInitialLocation={setInitialLocation}
                   >
                     {value ? value : "Enter your destination"}
-                  </InputButtonMap>
+                  </InputButton>
                 )}
               />
               <FormMessage />
+            </div>
+            <div className="absolute right-2 top-1/2 -translate-y-1/2 transform">
+              <Dialog open={isMapOpen} onOpenChange={setIsMapOpen}>
+                <DialogTrigger asChild>
+                  <button
+                    type="button"
+                    className="flex items-center rounded-lg border border-[#d9d6d1ff] bg-[#f2f1efff] p-2 hover:bg-gray-200"
+                    onClick={() => setIsMapOpen(true)}
+                  >
+                    <LocateFixed className="h-4 w-4 text-gray-700" />
+                  </button>
+                </DialogTrigger>
+                <DialogContent>
+                  <MapModal
+                    initialLocation={initialLocation}
+                    setInitialLocation={setInitialLocation}
+                    setOpen={setIsMapOpen}
+                    onSave={onSave}
+                  />
+                </DialogContent>
+              </Dialog>
             </div>
           </div>
         </FormItem>
