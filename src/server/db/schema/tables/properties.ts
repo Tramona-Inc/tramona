20449a609f799769821b268c801a3ef84de38b74
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-06-19 12:02:01 -0700

    added bathrooms to offer page and admin form

--- src/server/db/schema/tables/properties.ts
+++ src/server/db/schema/tables/properties.ts
@@ -157,7 +157,7 @@ export const properties = pgTable("properties", {
   maxNumGuests: smallint("max_num_guests").notNull(),
   numBeds: smallint("num_beds").notNull(),
   numBedrooms: smallint("num_bedrooms").notNull(),
-  numBathrooms: doublePrecision("num_bathrooms"),
+  numBathrooms: smallint("num_bathrooms").notNull(),
 
   // for when blake/preju manually upload, otherwise get the host's name via hostId
   hostName: varchar("host_name", { length: 255 }),
