9b7d3c1f4f49db4a6fb0f5ee20872fb856e253cf
Ben Tomlin <bentomlin101@gmail.com>
2024-06-25 11:43:09 -0700

    remove temp default value from property type

--- src/server/db/schema/tables/properties.ts
+++ src/server/db/schema/tables/properties.ts
@@ -182,9 +182,7 @@ export const properties = pgTable("properties", {
   hostId: text("host_id").references(() => users.id, { onDelete: "cascade" }),
   hostTeamId: integer("host_team_id"), //.references(() => hostTeams.id, { onDelete: "cascade" }),
 
-  propertyType: propertyTypeEnum("property_type")
-    .notNull()
-    .default("Apartment"),
+  propertyType: propertyTypeEnum("property_type").notNull(),
   roomType: propertyRoomTypeEnum("room_type").notNull().default("Entire place"),
 
   // how many guests does this property accomodate at most?
