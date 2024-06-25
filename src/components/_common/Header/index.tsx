d11a6cbf2ca97f1290cfc023b1b9f7919e83c768
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-06-24 22:04:44 -0700

    updated dropdown css

--- src/components/_common/Header/index.tsx
+++ src/components/_common/Header/index.tsx
@@ -1,4 +1,14 @@
-import { Menu, MenuIcon } from "lucide-react";
+import {
+  BadgeInfo,
+  BadgePercent,
+  Home,
+  Link2,
+  Menu,
+  MenuIcon,
+  MessageCircleQuestion,
+  Tag,
+  X,
+} from "lucide-react";
 import Link from "next/link";
 import HeaderTopRight from "./HeaderTopRight";
 
@@ -16,8 +26,12 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Icon } from "@/components/_icons/icons";
+import { useState } from "react";
 
 type HeaderProps =
   | {
@@ -52,29 +66,52 @@ const headerLinks2 = [
 ];
 
 const hamburgerLinksDesktop = [
-  { name: "FAQ", href: "/faq" },
-  { name: "Contact", href: "/support" },
+  { name: "FAQ", href: "/faq", icon: <MessageCircleQuestion /> },
+  { name: "Contact", href: "/support", icon: <BadgeInfo /> },
 ];
 
 const hamburgerLinksMobile = [
-  { name: "Become a host", href: "/host-onboarding" },
-  { name: "Unclaimed Offers", href: "/unclaimed-offers" },
-  { name: "Recent Deals", href: "/exclusive-offers" },
-  { name: "Link Input", href: "/link-input" },
+  { name: "Become a host", href: "/host-onboarding", icon: <Home /> },
+  { name: "Unclaimed Offers", href: "/unclaimed-offers", icon: <Tag /> },
+  { name: "Recent Deals", href: "/exclusive-offers", icon: <BadgePercent /> },
+  { name: "Link Input", href: "/link-input", icon: <Link2 /> },
 ];
 
-function HamburgerMenu({ links }: { links: { name: string; href: string }[] }) {
+function HamburgerMenu({
+  links,
+}: {
+  links: {
+    name: string;
+    href: string;
+    icon: React.ReactNode;
+  }[];
+}) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <DropdownMenu>
+    <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger>
         <div className="pl-2">
           <MenuIcon />
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="bg-white">
+        <DropdownMenuLabel className="text-xl font-bold text-teal-900">
+          <div className="flex items-center gap-3">
+            <button
+              className="rounded-full border border-teal-900 bg-zinc-200 p-2"
+              onClick={() => setOpen(!open)}
+            >
+              <X size={20} />
+            </button>
+            <h3>Tramona</h3>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
         {links.map((link) => (
           <Link key={link.href} href={link.href}>
-            <DropdownMenuItem className="px-2 font-bold">
+            <DropdownMenuItem className="my-2 px-2 py-6 font-bold text-teal-900 focus:bg-zinc-100 focus:text-teal-900">
+              <div className="rounded-full bg-zinc-200 p-2">{link.icon}</div>
               {link.name}
             </DropdownMenuItem>
           </Link>
