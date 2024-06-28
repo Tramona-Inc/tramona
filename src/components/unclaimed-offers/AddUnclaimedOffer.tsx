83e8e8dcc4b1e72d8b02b33bffb7610ec8e082e7
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-06-27 21:56:57 -0700

    admin able to delete offers

--- src/components/unclaimed-offers/AddUnclaimedOffer.tsx
+++ src/components/unclaimed-offers/AddUnclaimedOffer.tsx
@@ -1,8 +1,11 @@
 import { Dialog } from "@/components/ui/dialog";
 import { DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { api } from "@/utils/api";
 import { PlusIcon } from "lucide-react";
 
 export default function AddUnclaimedOffer() {
+  const { mutateAsync: addOffer } = api.offers.create.useMutation();
+
   return (
     <Dialog>
       <DialogTrigger className="flex flex-row gap-x-1">
