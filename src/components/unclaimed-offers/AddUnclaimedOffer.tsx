f1a6145affaee91bf5ed7c804e2822218a06bdc8
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-06-28 22:59:23 -0700

    added AdminOfferForm to add unclaimed offer button

--- src/components/unclaimed-offers/AddUnclaimedOffer.tsx
+++ src/components/unclaimed-offers/AddUnclaimedOffer.tsx
@@ -1,18 +1,16 @@
 import { Dialog } from "@/components/ui/dialog";
 import { DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { api } from "@/utils/api";
 import { PlusIcon } from "lucide-react";
+import AdminOfferForm from "../admin/AdminOfferForm";
 
 export default function AddUnclaimedOffer() {
-  const { mutateAsync: addOffer } = api.offers.create.useMutation();
-
   return (
     <Dialog>
       <DialogTrigger className="flex flex-row gap-x-1">
         <p>Add new offer</p>
         <PlusIcon />
       </DialogTrigger>
-      <DialogContent>It works</DialogContent>
+      <DialogContent>{/* <AdminOfferForm /> */}</DialogContent>
     </Dialog>
   );
 }
