9bebdcff85abafac37cd20f39b4d2c06714ad4ab
Zarna Nakrani <zarnanakrani@Zarnas-MacBook-Air.local>
2024-07-01 14:49:05 -0700

    changes

--- src/components/_common/Layout/DashboardLayout/index.tsx
+++ src/components/_common/Layout/DashboardLayout/index.tsx
@@ -3,7 +3,11 @@ import Sidebar from "@/components/dashboard/Sidebar";
 import { useSession } from "next-auth/react";
 import Header from "../../Header";
 import Footer from "../DesktopFooter";
+import { Button } from "@/components/ui/button";
+import { Session, Chatbox } from '@talkjs/react'
 import { useIsMd } from "@/utils/utils";
+import {MessageCircleMore} from 'lucide-react'
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 
 type DashboardLayoutProps = {
   children: React.ReactNode;
@@ -32,6 +36,23 @@ export default function DashboardLayout({
           ) : (
             <MobileNav type={"unlogged"} />
           )}
+          <div className="fixed bottom-10 right-4 z-50">
+            <Popover>
+              <PopoverTrigger asChild>
+            <Button className="border rounded-full p-4 w-18 h-18">
+              <MessageCircleMore />
+            </Button>
+              </PopoverTrigger>  
+              <PopoverContent align="start" className="relative w-[22rem] h-[40rem]">
+              <Session appId="tIu0KQUE" userId="sample_user_alice">
+                <Chatbox
+                  conversationId="sample_conversation"
+                  className="w-full h-full"
+                ></Chatbox>
+              </Session>
+              </PopoverContent>            
+            </Popover>
+          </div>
           {isMd && <Footer />}
         </div>
       </div>
--- src/components/_common/Layout/MainLayout/index.tsx
+++ src/components/_common/Layout/MainLayout/index.tsx
@@ -1,7 +1,10 @@
 import { cn } from "@/utils/utils";
+import { Session, Chatbox } from '@talkjs/react'
 import React from "react";
 import Header from "../../Header";
 import Footer from "../DesktopFooter";
+import { Button } from "@/components/ui/button";
+import {MessageCircleIcon} from 'lucide-react'
 
 type MainLayoutProps = {
   className?: string;
@@ -9,12 +12,18 @@ type MainLayoutProps = {
   type?: "marketing" | "auth";
 };
 
+function Chat() {
+  return 
+  <Session appId="tIu0KQUE" userId="sample_user_alice"></Session>
+}
+
 export default function MainLayout({
   className,
   children,
   type,
 }: MainLayoutProps) {
   return (
+    
     <div vaul-drawer-wrapper="">
       {type === "auth" ? (
         <Header type="dashboard" sidebarType="guest" />
@@ -24,6 +33,17 @@ export default function MainLayout({
       <main className={cn("min-h-screen-minus-header bg-white", className)}>
         {children}
       </main>
+      <div className="sticky bottom-0 right-10 z-50">
+      <Button >
+        <MessageCircleIcon />
+      <Session appId="tIu0KQUE" userId="sample_user_alice">
+        <Chatbox
+          conversationId="sample_conversation"
+          style={{ width: "100%", height: "500px" }}
+        ></Chatbox>
+      </Session>
+      </Button>
+      </div>
       <Footer />
     </div>
   );
