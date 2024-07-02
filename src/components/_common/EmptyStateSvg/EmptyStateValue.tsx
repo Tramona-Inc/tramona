6ddfaf61c3851d7b181d1b499355e9fbdf60d12b
Ben Tomlin <bentomlin101@gmail.com>
2024-07-02 09:35:27 -0700

    messages ui updates from an old stash i had

--- src/components/_common/EmptyStateSvg/EmptyStateValue.tsx
+++ src/components/_common/EmptyStateSvg/EmptyStateValue.tsx
@@ -3,25 +3,32 @@ import Link from "next/link";
 
 type EmptyStateProps = {
   children?: React.ReactNode;
-  title: string;
-  description: string;
-  redirectTitle: string;
-  href: string;
-};
+  title?: string;
+  description?: string;
+} & (
+  | { redirectTitle: string; href: string }
+  | { redirectTitle?: undefined; href?: undefined }
+);
 
 export default function EmptyStateValue(props: EmptyStateProps) {
   return (
     <div className="flex flex-col items-center gap-6 pt-32">
       {props.children}
       <div>
-        <h2 className="text-center font-bold">{props.title}</h2>
-        <p className="max-w-[350px] text-center font-medium">
-          {props.description}
-        </p>
+        {props.title && (
+          <h2 className="text-center font-bold">{props.title}</h2>
+        )}
+        {props.description && (
+          <p className="max-w-[350px] text-center font-medium text-muted-foreground">
+            {props.description}
+          </p>
+        )}
       </div>
-      <Button className="px-8 font-bold" asChild>
-        <Link href={props.href}>{props.redirectTitle}</Link>
-      </Button>
+      {props.href && (
+        <Button className="px-8 font-bold" asChild>
+          <Link href={props.href}>{props.redirectTitle}</Link>
+        </Button>
+      )}
     </div>
   );
 }
