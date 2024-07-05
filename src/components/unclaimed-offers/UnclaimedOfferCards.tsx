e01f2ea49ad4682746df2f23e0e167dd3f61ba84
adamkim1006 <adamkim1006@gmail.com>
2024-07-04 19:16:46 -0700

    updated UI for unclaimed offers card

--- src/components/unclaimed-offers/UnclaimedOfferCards.tsx
+++ src/components/unclaimed-offers/UnclaimedOfferCards.tsx
@@ -5,7 +5,7 @@ import { api } from "@/utils/api";
 import { type RouterOutputs } from "@/utils/api";
 import { AVG_AIRBNB_MARKUP } from "@/utils/constants";
 import { formatDateRange, formatCurrency } from "@/utils/utils";
-import { InfoIcon, TrashIcon } from "lucide-react";
+import { InfoIcon, TrashIcon, ExternalLink } from "lucide-react";
 import { Separator } from "../ui/separator";
 import { Skeleton } from "../ui/skeleton";
 import { useState } from "react";
@@ -19,12 +19,18 @@ export type UnMatchedOffers =
 
 export default function UnclaimedOfferCard() {
   const [showMore, setShowMore] = useState(false);
+  const [displayCount, setDisplayCount] = useState(8);
   const { data: unMatchedOffers, isLoading } =
     api.offers.getAllUnmatchedOffers.useQuery();
   const { data: session } = useSession();
+
+  const handleShowMore = () => {
+    setDisplayCount((prevCount) => prevCount + 8);
+  };
+
   return (
     <div className="w-5/6 space-y-2">
-      <div className="flex flex-col gap-y-1">
+      {/* <div className="flex flex-col gap-y-1">
         <h2 className="text-3xl font-semibold">Amazing deals happening now!</h2>
         <div className="flex flex-row items-center justify-between">
           <div className="mb-10 flex flex-row items-center gap-x-1 text-teal-700">
@@ -46,34 +52,55 @@ export default function UnclaimedOfferCard() {
           </div>
           <Separator className="bg-black" />
         </div>
-      </div>
+      </div> */}
       {!isLoading ? (
         unMatchedOffers ? (
-          unMatchedOffers.map((offer) => (
-            <div key={offer.property.id}>
-              <UnMatchedPropertyCard offer={offer} />
+          <div className="flex items-center justify-center">
+            <div className="grid grid-cols-4 gap-6">
+              {unMatchedOffers.slice(0, displayCount).map((offer) => (
+                <div key={offer.property.id}>
+                  <UnMatchedPropertyCard offer={offer} />
+                </div>
+              ))}
             </div>
-          ))
+          </div>
         ) : (
           <div>Sorry, we currently don&apos;t have any unmatched offers.</div>
         )
       ) : (
-        <div className="flex-1 px-6 text-sm">
-          <div className="flex flex-row gap-x-4 ">
-            <Skeleton className="h-20 w-32 rounded-lg" />
-            <Skeleton className="h-20 w-11/12 rounded-lg" />
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-4 gap-6">
+            {[...Array(8)].map((_, index) => (
+              <div
+                key={index}
+                className="grid h-[505px] w-[313px] grid-cols-1 rounded-xl text-[16px]"
+              >
+                <Skeleton className="h-[313px] w-full rounded-xl" />
+                <div className="mt-2 flex h-[192px] flex-col space-y-1">
+                  <Skeleton className="h-6 w-full" />
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="h-6 w-full" />
+                  <Skeleton className="h-6 w-1/2" />
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="mt-2 h-[36px] w-full" />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       )}
-      <div className="flex justify-center">
-        <Button
-          variant="secondary"
-          size="lg"
-          onClick={() => setShowMore(!showMore)}
-        >
-          {showMore ? "Show Less" : "Show More"}
-        </Button>
-      </div>
+      {unMatchedOffers && unMatchedOffers.length > displayCount && (
+        <div className="flex justify-center">
+          <Button
+            // variant=""
+            // size="lg"
+            onClick={handleShowMore}
+            className="mt-8 h-[52px] w-[151px] rounded-full border-2 border-[#004236] bg-transparent text-[16px] font-bold text-[#004236] hover:bg-inherit hover:opacity-80"
+          >
+            Show more
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
@@ -94,54 +121,64 @@ function UnMatchedPropertyCard({ offer }: { offer: UnMatchedOffers }) {
 
   return (
     <>
-      <div className="grid grid-cols-10 items-center gap-x-2 rounded-xl border text-center">
-        <Image
-          src={offer.property.imageUrls[0] ?? ""}
-          alt=""
-          width={150}
-          height={130}
-          className=" col-span-1 h-24 rounded-l-xl"
-        />
-        <div className="ellipsis col-span-1 flex max-h-24 items-center justify-center px-1 font-bold">
-          {offer.property.name}
+      <div className="grid h-[505px] w-[313px] grid-cols-1 rounded-xl text-[16px]">
+        <div className="flex h-[313px] items-center justify-center">
+          <Image
+            src={offer.property.imageUrls[0] ?? ""}
+            alt=""
+            width={150}
+            height={130}
+            className="flex max-h-[313px] w-full max-w-[313px] items-center justify-center rounded-xl"
+          />
         </div>
-        <div className="col-span-1 flex items-center justify-center text-center font-semibold">
-          {formatCurrency(
-            offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP,
-          )}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {formatCurrency(offer.property.originalNightlyPrice!)}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {formatDateRange(offer.checkIn, offer.checkOut)}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {offer.property.maxNumGuests}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {offer.property.numBedrooms}
-        </div>
-        <div className="col-span-1 flex items-center justify-center">
-          <Link
-            href={`/public-offer/${offer.id}`}
-            className="font-semibold text-teal-800 underline underline-offset-4"
+        <div className="mt-2 flex h-[192px] flex-col space-y-1">
+          <div className="line-clamp-1 overflow-ellipsis font-bold">
+            {offer.property.name}
+          </div>
+          <div className="flex items-center font-semibold">
+            {formatDateRange(offer.checkIn, offer.checkOut)}
+          </div>
+          <div className="flex">
+            <div className="flex items-center font-semibold">
+              {offer.property.maxNumGuests} Guests ∙&nbsp;
+            </div>
+            <div className="flex items-center font-semibold">
+              {offer.property.numBedrooms} Bedrooms ∙&nbsp;
+            </div>
+            <div className="flex items-center font-semibold">Beds ∙&nbsp;</div>
+            <div className="flex items-center font-semibold">Bath</div>
+          </div>
+          <div className="flex items-center font-bold">
+            Our price:&nbsp;
+            {formatCurrency(offer.property.originalNightlyPrice!)}
+          </div>
+          <div className="flex items-center text-center font-semibold">
+            Price on Airbnb:&nbsp;
+            {formatCurrency(
+              offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP,
+            )}
+            &nbsp;
+            <div className="flex items-center">
+              <Link
+                href={`/public-offer/${offer.id}`}
+                className="font-semibold text-teal-800 underline underline-offset-4"
+              >
+                <ExternalLink size={18} />
+              </Link>
+            </div>
+          </div>
+
+          <Button
+            className="h-[36px] bg-[#001410] font-semibold"
+            onClick={handleButtonClick}
           >
-            Property Info
-          </Link>
-        </div>
-        <Button
-          variant="greenPrimary"
-          className="col-span-1 ml-2 px-20 font-semibold"
-          onClick={handleButtonClick}
-        >
-          Book
-        </Button>
-        <div className="flex flex-row gap-x-1">
+            Request to book
+          </Button>
+          {/* <div className="flex flex-row">
           <Button
             variant="ghost"
             size="icon"
-            className="col-span-1  ml-12 font-semibold"
+            className="col-span-1 font-semibold"
             onClick={handleButtonClick}
           >
             <div onClick={handleButtonClick}>
@@ -162,6 +199,7 @@ function UnMatchedPropertyCard({ offer }: { offer: UnMatchedOffers }) {
               <TrashIcon />
             </Button>
           )}
+        </div> */}
         </div>
       </div>
     </>
