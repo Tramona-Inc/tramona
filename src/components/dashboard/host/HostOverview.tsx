fde3b1c459d7a1c86b11919d7af1e65dd8bafb36
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-07-06 11:46:39 -0700

    completed formatting of overviews layout

--- src/components/dashboard/host/HostOverview.tsx
+++ src/components/dashboard/host/HostOverview.tsx
@@ -11,14 +11,14 @@ export default function HostOverview() {
       <h1 className="text-4xl font-bold">At a glance</h1>
       <div className="flex flex-col gap-4 lg:flex-row">
         {/* <HostAnalytics className="contents lg:flex lg:flex-1" /> */}
-        <HostStaysOverview className="contents lg:flex lg:flex-1" />
+        <HostStaysOverview className="contents flex-1 overflow-x-auto lg:flex" />
         <HostRequestsOverview className="contents flex-1 lg:flex" />
-        <HostPropertiesOverview className="contents flex-1 lg:flex" />
+        <HostPropertiesOverview className="contents basis-1/4 lg:flex" />
       </div>
       <div className="flex flex-col gap-4 lg:flex-row">
         <HostFinancesOverview className="contents flex-1 lg:flex" />
-        <HostFinancesOverview className="contents flex-1 lg:flex" />
-        <HostMessagesOverview className="contents flex-1 lg:flex" />
+        <HostFinancesOverview className="contents basis-1/4 lg:flex" />
+        <HostMessagesOverview className="contents basis-1/4 lg:flex" />
       </div>
     </div>
   );
