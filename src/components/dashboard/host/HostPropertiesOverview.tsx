ca352216027db453c07de67d0affdeef8d482217
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-07-06 00:13:07 -0700

    added functionality to properties overview

--- src/components/dashboard/host/HostPropertiesOverview.tsx
+++ src/components/dashboard/host/HostPropertiesOverview.tsx
@@ -3,12 +3,25 @@ import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { EditIcon } from "lucide-react";
+import { api } from "@/utils/api";
 
 export default function HostPropertiesOverview({
   className,
 }: {
   className?: string;
 }) {
+  const { data: properties } = api.properties.getHostProperties.useQuery();
+
+  const listedProperties = properties?.filter(
+    (property) => property.propertyStatus === "Listed",
+  );
+  const archivedProperties = properties?.filter(
+    (property) => property.propertyStatus === "Archived",
+  );
+  const draftedProperties = properties?.filter(
+    (property) => property.propertyStatus === "Drafted",
+  );
+
   return (
     <Card className={className}>
       <CardHeader>
@@ -20,15 +33,15 @@ export default function HostPropertiesOverview({
         <div className="flex flex-col gap-4 text-lg font-semibold">
           <div className="flex items-center justify-between rounded-lg border p-4">
             <p>Listed</p>
-            <p className="text-2xl font-bold">4</p>
+            <p className="text-2xl font-bold">{listedProperties?.length}</p>
           </div>
           <div className="flex items-center justify-between rounded-lg border p-4">
             <p>Drafts</p>
-            <p className="text-2xl font-bold">4</p>
+            <p className="text-2xl font-bold">{draftedProperties?.length}</p>
           </div>
           <div className="flex items-center justify-between rounded-lg border p-4">
             <p>Archive</p>
-            <p className="text-2xl font-bold">4</p>
+            <p className="text-2xl font-bold">{archivedProperties?.length}</p>
           </div>
         </div>
       </CardContent>
