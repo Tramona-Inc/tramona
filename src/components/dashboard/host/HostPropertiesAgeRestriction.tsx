51620763088dc3ef17689ac6e3922d91816e0b13
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-07-10 22:48:25 -0700

    added age restriction tab to host property info

--- src/components/dashboard/host/HostPropertiesAgeRestriction.tsx
+++ src/components/dashboard/host/HostPropertiesAgeRestriction.tsx
@@ -4,7 +4,7 @@ import { HostPropertyEditBtn } from "./HostPropertiesLayout";
 import { useState } from "react";
 import { type Property } from "@/server/db/schema";
 
-export default function HostPropertiesPriceRestriction({
+export default function HostPropertiesAgeRestriction({
   property,
 }: {
   property: Property;
@@ -21,27 +21,27 @@ export default function HostPropertiesPriceRestriction({
         />
       </div>
       <div className="space-y-4">
-        <h2 className="text-xl font-bold">Property bids</h2>
+        <h2 className="text-xl font-bold">Property age restriction</h2>
         <div className="grid grid-cols-1 items-center gap-4 rounded-xl bg-zinc-100 p-4 sm:grid-cols-2 sm:gap-6">
           <div>
-            <h3 className="text-lg font-semibold">Lowest Price</h3>
+            <h3 className="text-lg font-semibold">Minimum age to book</h3>
             <p className="text-muted-foreground">
-              You will not be notified for offers lower than this amount.
+              Travelers must be at least this old to book the property.
             </p>
           </div>
           <div>
-            <Input className="w-full" icon={DollarSign} />
-            <p className="mt-1 text-sm text-muted-foreground">
+            <Input className="w-full" />
+            {/* <p className="mt-1 text-sm text-muted-foreground">
               Includes all additional fees you normally charge
-            </p>
+            </p> */}
           </div>
         </div>
-        <div className="flex items-center gap-2 rounded-xl bg-zinc-100 p-2">
-          <Info className=" text-blue-500" />
+        {/* <div className="flex items-center gap-2 rounded-xl bg-zinc-100 p-2">
+          <Info className="text-blue-500" />
           <p className="text-xs font-semibold sm:text-base">
             Remember you can counteroffer a low offer.
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
