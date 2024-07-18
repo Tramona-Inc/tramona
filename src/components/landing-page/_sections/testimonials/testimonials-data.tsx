13fdfd3851a5a63db551efb2e2e3d45d697be0f8
Ben Tomlin <bentomlin101@gmail.com>
2024-07-18 09:42:57 -0700

    testimonials cleanup + grammar/spelling fixes

---
+++ src/components/landing-page/_sections/testimonials/testimonials-data.tsx
@@ -0,0 +1,56 @@
+export const testimonials = [
+  {
+    quote:
+      "Tramona has been very easy for me. All i did is sign up and now i get requests directly to my phone.",
+    author: "Shawn P.",
+    age: "49",
+    role: "Host",
+    superhost: true,
+    image: "/assets/images/fake-reviews/shawnp.jpg",
+  },
+  {
+    quote:
+      "The 0 fees are really great. I like knowing the price wont double at the last second",
+    author: "Bianca R.",
+    age: "24",
+    role: "Traveler",
+    superhost: false,
+    image: "/assets/images/fake-reviews/biancar.jpg",
+  },
+  {
+    quote:
+      "With close to 10 million properties on Airbnb its hard to get people to find my property, Tramona gives me an equal chance to get my property in front of travelers”",
+    author: "Lamar F.",
+    age: "31",
+    role: "Host",
+    superhost: false,
+    image: "/assets/images/fake-reviews/lamarf.jpg",
+  },
+  {
+    quote:
+      "The biggest thing for me when traveling has always been price. Now i can afford properties i usually could never afford",
+    author: "Susan L.",
+    age: "28",
+    role: "Traveler",
+    superhost: false,
+    image: "/assets/images/fake-reviews/susanl.jpg",
+  },
+  {
+    quote:
+      "I mean, if my property is vacant, ill always take something over nothing",
+    author: "Chaz W.",
+    age: "32",
+    role: "Host",
+    superhost: false,
+    image: "/assets/images/fake-reviews/chazw.jpg",
+  },
+  {
+    quote:
+      "Tramona is very convenient, i just submit a request and wait. That’s it. Next thing i know i have 10 matches.",
+    author: "Kim W.",
+    age: "30",
+    role: "Traveler",
+    superhost: false,
+    image: "/assets/images/fake-reviews/kimw.jpg",
+  },
+];
