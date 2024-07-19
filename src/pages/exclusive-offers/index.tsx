dc02f9efd7b8d58b7052ede97d435a36720e5852
Neal Uehara <79432460+ueharaneal@users.noreply.github.com>
2024-07-19 03:39:38 -0700

    merged dev

--- src/pages/exclusive-offers/index.tsx
+++ src/pages/exclusive-offers/index.tsx
@@ -4,7 +4,7 @@ import { NextSeo } from "next-seo";
 
 export default function ExclusiveOffersPage() {
   const isProduction = process.env.NODE_ENV === "production";
-  const baseUrl = isProduction
+  let baseUrl = isProduction
     ? "https://www.tramona.com"
     : "https://6fb1-104-32-193-204.ngrok-free.app/"; //change to your live server
 
@@ -32,13 +32,6 @@ export default function ExclusiveOffersPage() {
         }}
       />
       <DashboardLayout type="guest">
-        <div className="flex min-h-screen-minus-header items-center justify-center px-4 pb-footer-height pt-5">
-          <div className="mx-auto max-w-7xl">
-            <div className="flex items-center">
-              <h1 className="flex-1 py-4 text-2xl font-bold tracking-tight text-black lg:text-4xl">
-                Recent Deals
-              </h1>
-            </div>
         <div className="min-h-screen-minus-header px-4 pb-32 pt-16">
           <div className="mx-auto max-w-lg">
             <h1 className="flex-1 py-4 text-2xl font-bold tracking-tight text-black lg:text-4xl">
