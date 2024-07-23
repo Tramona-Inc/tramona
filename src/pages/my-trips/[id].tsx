3390926cfe3ca0846012b27a98ff053c1350006a
Howard Zhong <placeholder29@gmail.com>
2024-07-22 21:43:34 -0500

    bugfix

--- src/pages/my-trips/[id].tsx
+++ src/pages/my-trips/[id].tsx
@@ -2,17 +2,29 @@ import Head from "next/head";
 import DashboardLayout from "@/components/_common/Layout/DashboardLayout";
 import TripPage from "@/components/my-trips/TripPage";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 export default function TripDetailsPage() {
   const router = useRouter();
-  const tripId = parseInt(router.query.id as string);
+  const [tripId, setTripId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (router.isReady) {
+      const id = parseInt(router.query.id as string, 10);
+      if (isNaN(id)) {
+        console.error("Invalid trip ID:", router.query.id);
+      } else {
+        setTripId(id);
+      }
+    }
+  }, [router.isReady, router.query.id]);
 
   return (
     <DashboardLayout type="guest">
       <Head>
         <title>My Trips | Tramona</title>
       </Head>
-      <TripPage tripId={tripId} />
+      {tripId && <TripPage tripId={tripId} />}
     </DashboardLayout>
   );
 }
