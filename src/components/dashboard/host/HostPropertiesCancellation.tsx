56ac1cb01751ea7d9d7b1ae2365fb6b283d7315c
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2024-07-26 00:24:14 -0700

    added form functionality

--- src/components/dashboard/host/HostPropertiesCancellation.tsx
+++ src/components/dashboard/host/HostPropertiesCancellation.tsx
@@ -1,6 +1,25 @@
 import { useState } from "react";
 import { HostPropertyEditBtn } from "./HostPropertiesLayout";
 import { type Property } from "@/server/db/schema";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodString } from "@/utils/zod-utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import ErrorMsg from "@/components/ui/ErrorMsg";
 
 export default function HostPropertiesCancellation({
   property,
@@ -9,6 +28,21 @@ export default function HostPropertiesCancellation({
 }) {
   const [editing, setEditing] = useState(false);
 
+  const formSchema = z.object({
+    policy: zodString(),
+  });
+
+  type FormValues = z.infer<typeof formSchema>;
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: FormValues) => {
+    console.log("form submitted");
+    console.log("form values:", values.policy);
+  };
+
   return (
     <div className="my-6">
       <div className="text-end">
@@ -16,56 +50,49 @@ export default function HostPropertiesCancellation({
           editing={editing}
           setEditing={setEditing}
           property={property}
+          onSubmit={form.handleSubmit(onSubmit)}
         />
       </div>
       <div className="space-y-4">
         <h2 className="text-xl font-bold">Your policy</h2>
         <div className="rounded-xl bg-zinc-100 p-6">
-          <div>
-            <h3 className="font-bold">1. Cancellation Period:</h3>
-            <ul className="ml-10 list-disc">
-              <li>
-                Guests must notify us of any cancellation in writing within the
-                designated cancellation period.
-              </li>
-            </ul>
-
-            <h3 className="mt-4 font-bold">2. Cancellation Fees:</h3>
-            <ul className="ml-10 list-disc">
-              <li>
-                If cancellation is made <strong>14 days</strong> or more prior
-                to the scheduled arrival date, guests will receive a full refund
-                of the booking deposit.
-              </li>
-              <li>
-                If cancellation is made within <strong>7 days</strong> of the
-                scheduled arrival date, guests will forfeit the booking deposit.
-              </li>
-              <li>
-                In the event of a no-show or cancellation on the day of
-                check-in, guests will be charged the full amount of the
-                reservation.
-              </li>
-            </ul>
-
-            <h3 className="mt-4 font-bold">3. Reservation Modifications:</h3>
-            <ul className="ml-10 list-disc">
-              <li>
-                Guests may request modifications to their reservation dates,
-                subject to availability. Any changes must be requested in
-                writing and approved by us.
-              </li>
-            </ul>
-
-            <h3 className="mt-4 font-bold">4. Refunds:</h3>
-            <ul className="ml-10 list-disc">
-              <li>
-                Refunds, if applicable, will be processed within{" "}
-                <strong>30 business days</strong> from the date of cancellation
-                confirmation.
-              </li>
-            </ul>
-          </div>
+          <Form {...form}>
+            <ErrorMsg>{form.formState.errors.root?.message}</ErrorMsg>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="policy"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        disabled={!editing}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Please choose your cancellation policy" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="flexible">Flexible</SelectItem>
+                          <SelectItem value="firm">Firm</SelectItem>
+                          <SelectItem value="moderate">Moderate</SelectItem>
+                          <SelectItem value="strict">Strict</SelectItem>
+                          <SelectItem value="super-strict-30">
+                            Super Strict 30 Days
+                          </SelectItem>
+                          <SelectItem value="super-strict-60">
+                            Super Strict 60 Days
+                          </SelectItem>
+                          <SelectItem value="long-term">Long Term</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </div>
       </div>
     </div>
