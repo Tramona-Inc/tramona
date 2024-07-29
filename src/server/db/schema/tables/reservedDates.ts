8d6402e098ae4acd9d5afc2dee52c508444974d6
Ben Tomlin <bentomlin101@gmail.com>
2024-07-29 10:13:01 -0700

    add fk constraint to reserved dates

--- src/server/db/schema/tables/reservedDates.ts
+++ src/server/db/schema/tables/reservedDates.ts
@@ -1,20 +1,18 @@
 // db/schema/tables/reservedDates.ts
 
-import {
-  pgTable,
-  integer,
-  date,
-  serial,
-} from "drizzle-orm/pg-core";
+import { pgTable, integer, date, serial } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { properties } from "./properties";
 
 export const reservedDates = pgTable("reserved_dates", {
-  id: serial('id').primaryKey(),
-  propertyId: integer("property_id").primaryKey(),
+  id: serial("id").primaryKey(),
+  propertyId: integer("property_id")
+    .notNull()
+    .references(() => properties.id),
   checkIn: date("check_in", { mode: "date" }).notNull(),
   checkOut: date("check_out", { mode: "date" }).notNull(),
 });
 
 export type ReservedDate = typeof reservedDates.$inferSelect;
-export const SelectSchema = createSelectSchema(reservedDates);
+export const reservedDateSelectSchema = createSelectSchema(reservedDates);
 export const reservedDateInsertSchema = createInsertSchema(reservedDates);
