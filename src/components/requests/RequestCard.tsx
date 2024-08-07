5bd8c394cd8dfbeb1461bb87ff26242b64ddaf14
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-08-07 14:57:00 -0700

    added map with zoom onto request card, might have to change things because I didnt test what it would like like for other request cards

--- src/components/requests/RequestCard.tsx
+++ src/components/requests/RequestCard.tsx
@@ -1,3 +1,204 @@
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "../ui/dropdown-menu";
+// import { type RouterOutputs } from "@/utils/api";
+// import { getFmtdFilters } from "@/utils/formatters";
+// import {
+//   formatCurrency,
+//   formatDateRange,
+//   formatInterval,
+//   getNumNights,
+//   plural,
+// } from "@/utils/utils";
+// import {
+//   CalendarIcon,
+//   EllipsisIcon,
+//   LinkIcon,
+//   MapPinIcon,
+//   TrashIcon,
+//   Users2Icon,
+// } from "lucide-react";
+// import { Card, CardContent, CardFooter } from "../ui/card";
+// import RequestGroupAvatars from "./RequestGroupAvatars";
+// import RequestCardBadge from "./RequestCardBadge";
+// import { Button } from "../ui/button";
+// import { useEffect, useState } from "react";
+// import WithdrawRequestDialog from "./WithdrawRequestDialog";
+
+// import { Badge } from "../ui/badge";
+// import UserAvatar from "../_common/UserAvatar";
+// import { TravelerVerificationsDialog } from "./TravelerVerificationsDialog";
+// import { getTime } from "date-fns";
+// import { LinkInputPropertyCard } from "../_common/LinkInputPropertyCard";
+// import SingleLocationMap from "../_common/GoogleMaps/SingleLocationMap";
+// import { api } from "@/utils/api";
+
+// import { Separator } from "../ui/separator";
+
+// export type GuestDashboardRequest = RouterOutputs["requests"]["getMyRequests"][
+//   | "activeRequestGroups"
+//   | "inactiveRequestGroups"][number]["requests"][number];
+
+// export type AdminDashboardRequst = RouterOutputs["requests"]["getAll"][
+//   | "incomingRequests"
+//   | "pastRequests"][number];
+
+// export type HostDashboardRequest =
+//   RouterOutputs["properties"]["getHostPropertiesWithRequests"][number]["requests"][number]["request"];
+
+// export default function RequestCard({
+//   request,
+//   type,
+//   children,
+// }: (
+//   | { type: "guest"; request: GuestDashboardRequest }
+//   | { type: "admin"; request: AdminDashboardRequst }
+//   | { type: "host"; request: HostDashboardRequest }
+// ) & {
+//   children?: React.ReactNode;
+// }) {
+//   const utils = api.useUtils();
+//   const [lat, setLat] = useState<number | null>(null);
+//   const [lng, setLng] = useState<number | null>(null);
+
+//   const pricePerNight =
+//     request.maxTotalPrice / getNumNights(request.checkIn, request.checkOut);
+//   const fmtdPrice = formatCurrency(pricePerNight);
+//   const fmtdDateRange = formatDateRange(request.checkIn, request.checkOut);
+//   const fmtdNumGuests = plural(request.numGuests, "guest");
+
+//   const fmtdFilters = getFmtdFilters(request, {
+//     withoutNote: true,
+//     excludeDefaults: true,
+//   });
+
+//   const showAvatars =
+//     (request.numGuests > 1 && type !== "host") || type === "admin";
+
+//   const [open, setOpen] = useState(false);
+
+//   useEffect(() => {
+//     const fetchCoordinates = async () => {
+//       if (!request.location || request.lat || request.lng) return;
+//       const { coordinates } = await utils.offers.getCoordinates.fetch({ location: request.location });
+//       const { lat, lng } = coordinates.location!;
+//       setLat(lat);
+//       setLng(lng);
+//       // Do something with lat and lng here
+//     };
+
+//     void fetchCoordinates();
+//   }, []);
+
+//   return (
+//     <Card>
+//       <WithdrawRequestDialog
+//         requestId={request.id}
+//         open={open}
+//         onOpenChange={setOpen}
+//       />
+//       <div>
+//         <div className="flex flex-wrap gap-2">
+//           {type !== "host" && <RequestCardBadge request={request} />}
+//           {type === "guest" && request.linkInputProperty && (
+//             <Badge variant="pink">
+//               <LinkIcon className="size-4" />
+//               Airbnb Link
+//             </Badge>
+//           )}
+//         </div>
+//         {type === "host" && (
+//           <div className="flex items-center gap-2">
+//             <UserAvatar
+//               size="sm"
+//               name={request.traveler.name}
+//               image={request.traveler.image}
+//             />
+//             <TravelerVerificationsDialog request={request} />
+//             <p>&middot;</p>
+//             <p>{formatInterval(Date.now() - getTime(request.createdAt))} ago</p>
+//           </div>
+//         )}
+//         <div className="absolute right-2 top-2 flex items-center gap-2">
+//           {showAvatars && (
+//             <RequestGroupAvatars
+//               request={request}
+//               isAdminDashboard={type === "admin"}
+//             />
+//           )}
+//           {type === "guest" && !request.resolvedAt && (
+//             <DropdownMenu>
+//               <DropdownMenuTrigger asChild>
+//                 <Button variant="ghost" size="icon" className="rounded-full">
+//                   <EllipsisIcon />
+//                 </Button>
+//               </DropdownMenuTrigger>
+//               <DropdownMenuContent align="end">
+//                 <DropdownMenuItem red onClick={() => setOpen(true)}>
+//                   <TrashIcon />
+//                   Withdraw
+//                 </DropdownMenuItem>
+//               </DropdownMenuContent>
+//             </DropdownMenu>
+//           )}
+//         </div>
+//       </div>
+//       <CardContent className="space-y-1">
+//         <div className="flex items-start gap-1">
+//           <MapPinIcon className="shrink-0 text-primary" />
+//           <h2 className="text-base font-bold text-primary md:text-lg">
+//             {request.location}
+//           </h2>
+//         </div>
+//         <div>
+//           <p>Requested {fmtdPrice}/night</p>
+//           <p className="flex items-center gap-2">
+//             <span className="flex items-center gap-1">
+//               <CalendarIcon className="size-4" />
+//               {fmtdDateRange}
+//             </span>
+//             &middot;
+//             <span className="flex items-center gap-1">
+//               <Users2Icon className="size-4" />
+//               {fmtdNumGuests}
+//             </span>
+//           </p>
+//         </div>
+//         {lat && lng && (
+//           <div className="rounded-lg bg-zinc-100 w-100 h-30">
+//             <SingleLocationMap
+//               lat={request.lat}
+//               lng={request.lng}
+//             />
+//             hi
+//           </div>
+//         )}
+//         {fmtdFilters && <p>{fmtdFilters}</p>}
+//         <div className="flex flex-wrap gap-1">
+//           {request.amenities.map((amenity) => (
+//             <Badge key={amenity}>{amenity}</Badge>
+//           ))}
+//         </div>
+//         {request.note && (
+//           <div className="rounded-lg bg-zinc-100 px-4 py-2">
+//             <p className="text-xs text-muted-foreground">Note</p>
+//             <p>&ldquo;{request.note}&rdquo;</p>
+//           </div>
+//         )}
+//         {type === "guest" && request.linkInputProperty && (
+//           <LinkInputPropertyCard property={request.linkInputProperty} />
+//         )}
+
+//       </CardContent>
+//       <CardFooter>{children}</CardFooter>
+//     </Card>
+//   );
+// }
+
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -25,7 +226,7 @@ import { Card, CardContent, CardFooter } from "../ui/card";
 import RequestGroupAvatars from "./RequestGroupAvatars";
 import RequestCardBadge from "./RequestCardBadge";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WithdrawRequestDialog from "./WithdrawRequestDialog";
 
 import { Badge } from "../ui/badge";
@@ -33,6 +234,9 @@ import UserAvatar from "../_common/UserAvatar";
 import { TravelerVerificationsDialog } from "./TravelerVerificationsDialog";
 import { getTime } from "date-fns";
 import { LinkInputPropertyCard } from "../_common/LinkInputPropertyCard";
+import SingleLocationMap from "../_common/GoogleMaps/SingleLocationMap";
+import { api } from "@/utils/api";
+
 import { Separator } from "../ui/separator";
 
 export type GuestDashboardRequest = RouterOutputs["requests"]["getMyRequests"][
@@ -57,6 +261,10 @@ export default function RequestCard({
 ) & {
   children?: React.ReactNode;
 }) {
+  const utils = api.useUtils();
+  const [lat, setLat] = useState<number | null>(null);
+  const [lng, setLng] = useState<number | null>(null);
+
   const pricePerNight =
     request.maxTotalPrice / getNumNights(request.checkIn, request.checkOut);
   const fmtdPrice = formatCurrency(pricePerNight);
@@ -73,97 +281,118 @@ export default function RequestCard({
 
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      if (!request.location || request.lat || request.lng) return;
+      const { coordinates } = await utils.offers.getCoordinates.fetch({ location: request.location });
+      const { lat, lng } = coordinates.location!;
+      setLat(lat);
+      setLng(lng);
+    };
+
+    void fetchCoordinates();
+  }, []);
+
   return (
-    <Card>
+    <Card className="max-w-4xl">
       <WithdrawRequestDialog
         requestId={request.id}
         open={open}
         onOpenChange={setOpen}
       />
-      <div>
-        <div className="flex flex-wrap gap-2">
-          {type !== "host" && <RequestCardBadge request={request} />}
-          {type === "guest" && request.linkInputProperty && (
-            <Badge variant="pink">
-              <LinkIcon className="size-4" />
-              Airbnb Link
-            </Badge>
-          )}
+      <div className="flex">
+        <div className="flex-1">
+          <div className= "flex flex-row justify-between">
+            <div className="flex flex-wrap gap-2">
+              {type !== "host" && <RequestCardBadge request={request} />}
+              {type === "guest" && request.linkInputProperty && (
+                <Badge variant="pink">
+                  <LinkIcon className="size-4" />
+                  Airbnb Link
+                </Badge>
+              )}
+            </div>
+            {type === "host" && (
+              <div className="flex items-center gap-2">
+                <UserAvatar
+                  size="sm"
+                  name={request.traveler.name}
+                  image={request.traveler.image}
+                />
+                <TravelerVerificationsDialog request={request} />
+                <p>&middot;</p>
+                <p>{formatInterval(Date.now() - getTime(request.createdAt))} ago</p>
+              </div>
+            )}
+            <div className="">
+              {showAvatars && (
+                <RequestGroupAvatars
+                  request={request}
+                  isAdminDashboard={type === "admin"}
+                />
+              )}
+              {type === "guest" && !request.resolvedAt && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon" className="rounded-full">
+                      <EllipsisIcon />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem red onClick={() => setOpen(true)}>
+                      <TrashIcon />
+                      Withdraw
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
+            </div>
+          </div>
+          <CardContent className="space-y-1">
+            <div className="flex items-start gap-1">
+              <MapPinIcon className="shrink-0 text-primary" />
+              <h2 className="text-base font-bold text-primary md:text-lg">
+                {request.location}
+              </h2>
+            </div>
+            <div>
+              <p>Requested {fmtdPrice}/night</p>
+              <p className="flex items-center gap-2">
+                <span className="flex items-center gap-1">
+                  <CalendarIcon className="size-4" />
+                  {fmtdDateRange}
+                </span>
+                &middot;
+                <span className="flex items-center gap-1">
+                  <Users2Icon className="size-4" />
+                  {fmtdNumGuests}
+                </span>
+              </p>
+            </div>
+            {fmtdFilters && <p>{fmtdFilters}</p>}
+            <div className="flex flex-wrap gap-1">
+              {request.amenities.map((amenity) => (
+                <Badge key={amenity}>{amenity}</Badge>
+              ))}
+            </div>
+            {request.note && (
+              <div className="rounded-lg bg-zinc-100 px-4 py-2">
+                <p className="text-xs text-muted-foreground">Note</p>
+                <p>&ldquo;{request.note}&rdquo;</p>
+              </div>
+            )}
+            {type === "guest" && request.linkInputProperty && (
+              <LinkInputPropertyCard property={request.linkInputProperty} />
+            )}
+          </CardContent>
+          <CardFooter>{children}</CardFooter>
         </div>
-        {type === "host" && (
-          <div className="flex items-center gap-2">
-            <UserAvatar
-              size="sm"
-              name={request.traveler.name}
-              image={request.traveler.image}
-            />
-            <TravelerVerificationsDialog request={request} />
-            <p>&middot;</p>
-            <p>{formatInterval(Date.now() - getTime(request.createdAt))} ago</p>
+        {lat && lng && (
+          <div className="hidden lg:block ml-4 w-64">
+            <SingleLocationMap lat={lat} lng={lng} icon={true} />
           </div>
         )}
-        <div className="absolute right-2 top-2 flex items-center gap-2">
-          {showAvatars && (
-            <RequestGroupAvatars
-              request={request}
-              isAdminDashboard={type === "admin"}
-            />
-          )}
-          {type === "guest" && !request.resolvedAt && (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon" className="rounded-full">
-                  <EllipsisIcon />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem red onClick={() => setOpen(true)}>
-                  <TrashIcon />
-                  Withdraw
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
-        </div>
       </div>
-      <CardContent className="space-y-1">
-        <div className="flex items-start gap-1">
-          <MapPinIcon className="shrink-0 text-primary" />
-          <h2 className="text-base font-bold text-primary md:text-lg">
-            {request.location}
-          </h2>
-        </div>
-        <div>
-          <p>Requested {fmtdPrice}/night</p>
-          <p className="flex items-center gap-2">
-            <span className="flex items-center gap-1">
-              <CalendarIcon className="size-4" />
-              {fmtdDateRange}
-            </span>
-            &middot;
-            <span className="flex items-center gap-1">
-              <Users2Icon className="size-4" />
-              {fmtdNumGuests}
-            </span>
-          </p>
-        </div>
-        {fmtdFilters && <p>{fmtdFilters}</p>}
-        <div className="flex flex-wrap gap-1">
-          {request.amenities.map((amenity) => (
-            <Badge key={amenity}>{amenity}</Badge>
-          ))}
-        </div>
-        {request.note && (
-          <div className="rounded-lg bg-zinc-100 px-4 py-2">
-            <p className="text-xs text-muted-foreground">Note</p>
-            <p>&ldquo;{request.note}&rdquo;</p>
-          </div>
-        )}
-        {type === "guest" && request.linkInputProperty && (
-          <LinkInputPropertyCard property={request.linkInputProperty} />
-        )}
-      </CardContent>
-      <CardFooter>{children}</CardFooter>
     </Card>
   );
 }
