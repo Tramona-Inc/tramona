1e7efbe0f8710b29d4a5d34e187e0876ec4ec634
adamkim1006 <adamkim1006@gmail.com>
2024-08-07 19:37:54 -0700

    'How-to Sync' dialog for syncing iCal

--- src/components/dashboard/host/HostPropertyInfo.tsx
+++ src/components/dashboard/host/HostPropertyInfo.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { type Property } from "@/server/db/schema";
 import HostPropertiesCancellation from "./HostPropertiesCancellation";
 import HostPropertiesDetails from "./HostPropertiesDetails";
-import { AlertCircle, ChevronLeft, Edit2 } from "lucide-react";
+import { AlertCircle, ChevronLeft, Edit2, Info } from "lucide-react";
 import Link from "next/link";
 import HostAvailability from "./HostAvailability";
 import { useState } from "react";
@@ -12,6 +12,13 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { api } from "@/utils/api";
 import HostPropertiesRestrictions from "./HostPropertiesRestrictions";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 export default function HostPropertyInfo({ property }: { property: Property }) {
   const [iCalLink, setiCalLink] = useState("");
@@ -61,8 +68,85 @@ export default function HostPropertyInfo({ property }: { property: Property }) {
       <div>
         {!property.iCalLink && (
           <div className="mb-10 space-y-4">
-            <h1 className="text-4xl font-bold">Sync your iCal</h1>
-            <Label className="font-semibold">iCal URL</Label>
+            <div className="flex items-center space-x-2">
+              <h1 className="text-4xl font-bold">Sync your iCal</h1>
+              <Dialog>
+                <DialogTrigger className="size-sm inline-flex items-center justify-center text-sm text-muted-foreground underline underline-offset-2">
+                  <Info className="size-4" />
+                </DialogTrigger>
+                <DialogContent className="max-w-3xl p-8">
+                  <DialogHeader>
+                    <DialogTitle className="mb-4 text-2xl font-bold">
+                      How to Sync Your Property's iCal
+                    </DialogTitle>
+                  </DialogHeader>
+                  <div className="space-y-6">
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 1: Get Your Airbnb iCal Link
+                      </h3>
+                      <ol className="list-inside list-decimal space-y-1">
+                        <li>Log in to your Airbnb account</li>
+                        <li>Go to "Manage Listings" from your profile menu</li>
+                        <li>Click on the "Calendar" tab</li>
+                        <li>Select the property you want to sync</li>
+                        <li>
+                          On the right-hand side, navigate to "Availability"
+                        </li>
+                        <li>
+                          Scroll to "Connect to another website" and copy the
+                          iCal link
+                        </li>
+                      </ol>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 2: Enter the iCal Link on Our Website
+                      </h3>
+                      <ol className="list-inside list-decimal space-y-1">
+                        <li>Log in to your host account on our website</li>
+                        <li>Find "Properties" on the left-hand side</li>
+                        <li>
+                          Navigate to your listing corresponding to the copied
+                          iCal
+                        </li>
+                        <li>Paste the Airbnb iCal link</li>
+                        <li>Click "Sync"</li>
+                      </ol>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 3: Verify the Sync
+                      </h3>
+                      <ul className="list-inside list-disc space-y-1">
+                        <li>Wait a few minutes for synchronization</li>
+                        <li>
+                          Check that Airbnb bookings appear on our platform's
+                          calendar
+                        </li>
+                      </ul>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Tips for Smooth Syncing
+                      </h3>
+                      <ul className="list-inside list-disc space-y-1">
+                        <li>Keep your iCal link handy for future updates</li>
+                        <li>Regularly check for booking conflicts</li>
+                      </ul>
+                    </section>
+
+                    <p className="text-sm text-muted-foreground">
+                      If you have any questions or issues, please contact our
+                      support team. Happy hosting!
+                    </p>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
             <Input
               id="iCalLink"
               type="url"
@@ -80,7 +164,86 @@ export default function HostPropertyInfo({ property }: { property: Property }) {
         )}
         {property.iCalLink && (
           <div className="mb-10 space-y-4">
-            <h1 className="text-4xl font-bold">Sync your iCal</h1>
+            <div className="flex items-center space-x-2">
+              <h1 className="text-4xl font-bold">Sync your iCal</h1>
+              <Dialog>
+                <DialogTrigger className="size-sm inline-flex items-center justify-center text-sm text-muted-foreground underline underline-offset-2">
+                  <Info className="size-4" />
+                </DialogTrigger>
+                <DialogContent className="max-w-3xl p-8">
+                  <DialogHeader>
+                    <DialogTitle className="mb-4 text-2xl font-bold">
+                      How to Sync Your Property's iCal
+                    </DialogTitle>
+                  </DialogHeader>
+                  <div className="space-y-6">
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 1: Get Your Airbnb iCal Link
+                      </h3>
+                      <ol className="list-inside list-decimal space-y-1">
+                        <li>Log in to your Airbnb account</li>
+                        <li>Go to "Manage Listings" from your profile menu</li>
+                        <li>Click on the "Calendar" tab</li>
+                        <li>Select the property you want to sync</li>
+                        <li>
+                          On the right-hand side, navigate to "Availability"
+                        </li>
+                        <li>
+                          Scroll to "Connect to another website" and copy the
+                          iCal link
+                        </li>
+                      </ol>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 2: Enter the iCal Link on Our Website
+                      </h3>
+                      <ol className="list-inside list-decimal space-y-1">
+                        <li>Log in to your host account on our website</li>
+                        <li>Find "Properties" on the left-hand side</li>
+                        <li>
+                          Navigate to your listing corresponding to the copied
+                          iCal
+                        </li>
+                        <li>Paste the Airbnb iCal link</li>
+                        <li>Click "Sync"</li>
+                      </ol>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Step 3: Verify the Sync
+                      </h3>
+                      <ul className="list-inside list-disc space-y-1">
+                        <li>Wait a few minutes for synchronization</li>
+                        <li>
+                          Check that Airbnb bookings appear on our platform's
+                          calendar
+                        </li>
+                      </ul>
+                    </section>
+
+                    <section>
+                      <h3 className="mb-2 text-lg font-semibold">
+                        Tips for Smooth Syncing
+                      </h3>
+                      <ul className="list-inside list-disc space-y-1">
+                        <li>Keep your iCal link handy for future updates</li>
+                        <li>Regularly check for booking conflicts</li>
+                      </ul>
+                    </section>
+
+                    <p className="text-sm text-muted-foreground">
+                      If you have any questions or issues, please contact our
+                      support team. Happy hosting!
+                    </p>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
+
             <Label className="font-semibold">iCal URL</Label>
             <Input
               id="iCalLink"
