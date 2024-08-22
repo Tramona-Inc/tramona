61b420bb8a042e67c2600a156991ffc0cc8665d6
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-08-21 18:50:47 -0700

    added packages

--- src/server/scrapePrice.ts
+++ src/server/scrapePrice.ts
@@ -37,7 +37,7 @@ export async function scrapeAirbnbPrice({
 export async function scrapeTest(url: string) {
   let browser = null;
   try {
-    browser = await chromium.puppeteer.launch({
+    browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
