e020ea2674ad53e6209d6f08d7d510d70bdadbb6
adamkim1006 <adamkim1006@gmail.com>
2024-08-23 12:04:08 -0700

    baseline UI for unclaimed offers

--- src/components/unclaimed-offers/UnclaimedOfferCards.tsx
+++ src/components/unclaimed-offers/UnclaimedOfferCards.tsx
@@ -5,7 +5,7 @@ import { api } from "@/utils/api";
 import { type RouterOutputs } from "@/utils/api";
 import { AVG_AIRBNB_MARKUP } from "@/utils/constants";
 import { formatDateRange, formatCurrency } from "@/utils/utils";
-import { InfoIcon, TrashIcon } from "lucide-react";
+import { InfoIcon, TrashIcon, ExternalLink, CirclePlus } from "lucide-react";
 import { Separator } from "../ui/separator";
 import { Skeleton } from "../ui/skeleton";
 import { useState } from "react";
@@ -19,61 +19,68 @@ export type UnMatchedOffers =
 
 export default function UnclaimedOfferCard() {
   const [showMore, setShowMore] = useState(false);
+  const [displayCount, setDisplayCount] = useState(8);
   const { data: unMatchedOffers, isLoading } =
     api.offers.getAllUnmatchedOffers.useQuery();
   const { data: session } = useSession();
+
+  const handleShowMore = () => {
+    setDisplayCount((prevCount) => prevCount + 8);
+  };
+
   return (
     <div className="w-5/6 space-y-2">
-      <div className="flex flex-col gap-y-1">
-        <h2 className="text-3xl font-semibold">Amazing deals happening now!</h2>
-        <div className="flex flex-row items-center justify-between">
-          <div className="mb-10 flex flex-row items-center gap-x-1 text-teal-700">
-            <InfoIcon size={18} strokeWidth={2.4} />
-            <Link href="/how-it-works" className="underline underline-offset-2">
-              How it works
-            </Link>
-          </div>
-          {session && session.user.role === "admin" && <AddUnclaimedOffer />}
-        </div>
-        <div className="relative">
-          <div className="sticky top-0 grid grid-cols-10 gap-x-2 bg-white text-center font-bold">
-            <div className="col-span-2 text-left font-bold">Listing</div>
-            <div className="col-span-1">Airbnb Price</div>
-            <div className="col-span-1">Tramona Price</div>
-            <div className="col-span-1">Dates</div>
-            <div className="col-span-1">Guests</div>
-            <div className="col-span-1">Beds</div>
-          </div>
-          <Separator className="bg-black" />
-        </div>
-      </div>
       {!isLoading ? (
         unMatchedOffers ? (
-          unMatchedOffers.map((offer) => (
-            <div key={offer.property.id}>
-              <UnMatchedPropertyCard offer={offer} />
+          <div className="flex flex-col items-center justify-center">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {unMatchedOffers.slice(0, displayCount).map((offer) => (
+                <div key={offer.property.id}>
+                  <UnMatchedPropertyCard offer={offer} />
+                </div>
+              ))}
             </div>
-          ))
+            {session?.user.role === "admin" && (
+              <div className="mt-20 border-4 rounded-xl p-4">
+              <AddUnclaimedOffer />
+              </div>
+            )}
+          </div>
         ) : (
           <div>Sorry, we currently don&apos;t have any unmatched offers.</div>
         )
       ) : (
-        <div className="flex-1 px-6 text-sm">
-          <div className="flex flex-row gap-x-4 ">
-            <Skeleton className="h-20 w-32 rounded-lg" />
-            <Skeleton className="h-20 w-11/12 rounded-lg" />
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-4 gap-6">
+            {[...Array(8)].map((_, index) => (
+              <div
+                key={index}
+                className="grid h-[505px] w-80 grid-cols-1 rounded-xl text-base"
+              >
+                <Skeleton className="h-80 w-full rounded-xl" />
+                <div className="mt-2 flex h-48 flex-col space-y-1">
+                  <Skeleton className="h-6 w-full" />
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="h-6 w-full" />
+                  <Skeleton className="h-6 w-1/2" />
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="mt-2 h-9 w-full" />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       )}
-      <div className="flex justify-center">
-        <Button
-          variant="secondary"
-          size="lg"
-          onClick={() => setShowMore(!showMore)}
-        >
-          {showMore ? "Show Less" : "Show More"}
-        </Button>
-      </div>
+      {unMatchedOffers && unMatchedOffers.length > displayCount && (
+        <div className="flex justify-center">
+          <Button
+            onClick={handleShowMore}
+            className="mt-8 h-14 w-40 rounded-full border-2 border-primaryGreen bg-transparent text-base font-bold text-primaryGreen hover:bg-inherit hover:opacity-80"
+          >
+            Show more
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
@@ -94,74 +101,110 @@ function UnMatchedPropertyCard({ offer }: { offer: UnMatchedOffers }) {
 
   return (
     <>
-      <div className="grid grid-cols-10 items-center gap-x-2 rounded-xl border text-center">
-        <Image
-          src={offer.property.imageUrls[0] ?? ""}
-          alt=""
-          width={150}
-          height={130}
-          className=" col-span-1 h-24 rounded-l-xl"
-        />
-        <div className="ellipsis col-span-1 flex max-h-24 items-center justify-center px-1 font-bold">
-          {offer.property.name}
+      <div className="grid max-h-[505px] max-w-80 grid-cols-1 rounded-xl text-base">
+        <div className="relative flex h-80 flex-col items-center justify-center">
+          <Image
+            src={offer.property.imageUrls[0] ?? ""}
+            alt=""
+            width={150}
+            height={130}
+            className="flex h-full w-full items-center justify-center rounded-xl"
+          />
+          <div className="absolute bottom-0 left-0 right-0 flex w-full items-center justify-center rounded-b-xl bg-blue-500 text-base text-[#FFFFFF]">
+            {!isNaN(
+              Math.floor(
+                ((offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP -
+                  offer.property.originalNightlyPrice!) /
+                  (offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP)) *
+                  100,
+              ),
+            )
+              ? Math.floor(
+                  ((offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP -
+                    offer.property.originalNightlyPrice!) /
+                    (offer.property.originalNightlyPrice! *
+                      AVG_AIRBNB_MARKUP)) *
+                    100,
+                )
+              : 0}
+            % off
+          </div>
         </div>
-        <div className="col-span-1 flex items-center justify-center text-center font-semibold">
-          {formatCurrency(
-            offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP,
-          )}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {formatCurrency(offer.property.originalNightlyPrice!)}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {formatDateRange(offer.checkIn, offer.checkOut)}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {offer.property.maxNumGuests}
-        </div>
-        <div className="col-span-1 flex items-center justify-center font-semibold">
-          {offer.property.numBedrooms}
-        </div>
-        <div className="col-span-1 flex items-center justify-center">
-          <Link
-            href={`/public-offer/${offer.id}`}
-            className="font-semibold text-teal-800 underline underline-offset-4"
-          >
-            Property Info
-          </Link>
-        </div>
-        <Button
-          variant="greenPrimary"
-          className="col-span-1 ml-2 px-20 font-semibold"
-          onClick={handleButtonClick}
-        >
-          Book
-        </Button>
-        <div className="flex flex-row gap-x-1">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="col-span-1  ml-12 font-semibold"
-            onClick={handleButtonClick}
-          >
-            <div onClick={handleButtonClick}>
-              <ShareOfferDialog
-                id={offer.id}
-                isRequest={false}
-                propertyName={offer.property.name}
-              />
+        <div className="mt-2 flex h-48 flex-col space-y-1">
+          <div className="line-clamp-1 overflow-ellipsis font-bold">
+            {offer.property.name}
+          </div>
+          <div className="flex items-center font-semibold">
+            {formatDateRange(offer.checkIn, offer.checkOut)}
+          </div>
+          <div className="flex">
+            <div className="flex items-center font-semibold">
+              {offer.property.maxNumGuests} Guests ∙&nbsp;
             </div>
-          </Button>
-          {session?.user.role === "admin" && (
+            <div className="flex items-center font-semibold">
+              {offer.property.numBedrooms} Bedrooms ∙&nbsp;
+            </div>
+            <div className="flex items-center font-semibold">Beds ∙&nbsp;</div>
+            <div className="flex items-center font-semibold">Bath</div>
+          </div>
+          <div className="flex items-center font-bold">
+            Our price:&nbsp;
+            {formatCurrency(offer.property.originalNightlyPrice!)}
+          </div>
+          <div className="flex items-center text-center font-semibold">
+            Price on Airbnb:&nbsp;
+            <div className="line-through">
+              {formatCurrency(
+                offer.property.originalNightlyPrice! * AVG_AIRBNB_MARKUP,
+              )}
+            </div>
+            &nbsp;
+            <div className="flex items-center">
+              <Link
+                href={`/public-offer/${offer.id}`}
+                className="font-semibold text-primaryGreen underline underline-offset-4"
+              >
+                <ExternalLink size={18} />
+              </Link>
+            </div>
+          </div>
+          <div className="">
+            <Link href={`/public-offer/${offer.id}`}>
+              <Button
+                className="h-9 w-full bg-black font-semibold hover:opacity-80"
+                onClick={handleButtonClick}
+              >
+                Request to book
+              </Button>
+            </Link>
+          </div>
+
+          <div className="flex flex-row items-center">
             <Button
-              size="icon"
               variant="ghost"
-              className="text-red-600"
-              onClick={() => handleRemoveProperty(offer.id)}
+              size="icon"
+              className="col-span-1 font-semibold"
+              onClick={handleButtonClick}
             >
-              <TrashIcon />
+              <div onClick={handleButtonClick}>
+                <ShareOfferDialog
+                  id={offer.id}
+                  isRequest={false}
+                  propertyName={offer.property.name}
+                />
+              </div>
             </Button>
-          )}
+            {session?.user.role === "admin" && (
+              <Button
+                size="icon"
+                variant="ghost"
+                className="text-red-600"
+                onClick={() => handleRemoveProperty(offer.id)}
+              >
+                <TrashIcon />
+              </Button>
+            )}
+          </div>
         </div>
       </div>
     </>
