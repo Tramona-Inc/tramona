5411eb10193eafd0dcef624aee6cac345516eb50
Howard Zhong <placeholder29@gmail.com>
2024-08-26 19:54:42 -0500

    halfway to fix error 552

--- src/pages/api/az-scraper.ts
+++ src/pages/api/az-scraper.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { scrapeDirectListings } from '../../server/direct-sites-scraping';
 import { arizonaScraper } from "@/server/direct-sites-scraping/integrity-arizona";
+import axios from 'axios';
+import { proxyAgent } from "@/server/server-utils";
 
 // This is a testing API route that scrapes listings from the Integrity Arizona website
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -10,6 +12,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       console.log('API route: Starting scraping process');
       const today = new Date();
       const twoDaysLater = new Date(today.getTime() + 2 * 24 * 60 * 60 * 1000);
+      // const rr = await axios.get("https://randomuser.me/api/", { httpsAgent: proxyAgent })
+      // // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      // .then((response) => response.data)
+      // .then((data) => console.log(data));
+      // res.status(200).json({ message: `Successfully, data: ` });
       const listings = await scrapeDirectListings({
         checkIn: today,
         checkOut: twoDaysLater,
