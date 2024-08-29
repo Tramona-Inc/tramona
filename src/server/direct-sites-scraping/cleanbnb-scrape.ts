13081d5c55c7f1accc7206f935b4ca900e38e488
Ben Tomlin <bentomlin101@gmail.com>
2024-08-28 22:01:05 -0700

    type errs

--- src/server/direct-sites-scraping/cleanbnb-scrape.ts
+++ src/server/direct-sites-scraping/cleanbnb-scrape.ts
@@ -3,7 +3,7 @@ import { DirectSiteScraper } from ".";
 import * as cheerio from "cheerio";
 import { v2 } from "@google-cloud/translate";
 import { env } from "@/env";
-import { NewProperty, PropertyType } from "../db/schema";
+import { PropertyType } from "../db/schema";
 
 const { Translate } = v2;
 
@@ -247,7 +247,7 @@ export const cleanbnbScraper: DirectSiteScraper = async ({
   const checkInDate = formatDate(checkIn);
   const checkOutDate = formatDate(checkOut);
 
-  const res: NewProperty[] = [];
+  const res: Awaited<ReturnType<DirectSiteScraper>> = [];
   const baseUrl = "https://www.cleanbnb.house/it/appartamenti";
 
   const mainPageData = await fetch(
@@ -316,235 +316,219 @@ export const cleanbnbScraper: DirectSiteScraper = async ({
   const validData = fetchedData.filter((data) => data !== null);
 
   // Process the fetched data
-  // eslint-disable-next-line @typescript-eslint/prefer-for-of
-  for (let i = 0; i < validData.length; i++) {
-    const response = validData[i]?.text;
+  for (const { text: response, url } of validData) {
+    const $response = cheerio.load(response);
 
-    // Parse the response and extract the required information
-    if (response) {
-      const $response = cheerio.load(response);
+    const jsonLdScripts = $response('script[type="application/ld+json"]');
 
-      const jsonLdScripts = $response('script[type="application/ld+json"]');
-
-      // Process each script tag
-      const jsonLdData = jsonLdScripts
-        .map((_, script) => {
-          const scriptContent = $(script).html()?.trim(); // Extract script content and trim whitespace
-          if (scriptContent) {
-            try {
-              // Parse JSON content
-              return JSON.parse(scriptContent);
-            } catch (e) {
-              console.error("Error parsing JSON from script content:", e);
-              return null;
-            }
+    // Process each script tag
+    const jsonLdData = jsonLdScripts
+      .map((_, script) => {
+        const scriptContent = $(script).html()?.trim(); // Extract script content and trim whitespace
+        if (scriptContent) {
+          try {
+            // Parse JSON content
+            return JSON.parse(scriptContent);
+          } catch (e) {
+            console.error("Error parsing JSON from script content:", e);
+            return null;
           }
-          return null;
-        })
-        .get() as JsonLdData[];
-
-      // Grab the specific JSON-LD data object we need
-      const info = jsonLdData.filter(
-        (data) =>
-          data.hasOwnProperty("@type") && data["@type"] === "VacationRental",
-      );
-      if (info[0] === undefined) {
-        // This hits a lot, 429 errors
-        console.log("No valid data found:", jsonLdData);
-        continue;
-      }
-      const scrapedData = info[0] as unknown as ScrapedData;
-
-      //const avgRating
-      //const numRatings
-
-      // let price = '';
-
-      // $response('div.min-price').each((_, element) => {
-      //   const infoButton = $(element).find('a.infobutton');
-      //   const dataContent = infoButton.attr('data-content');
-
-      //   if (dataContent && dataContent === "Cancellazione non consentita") {
-      //     const priceText = $(element).find('div.avgTotPriceRow').text().trim();
-      //     price = priceText.replace('€', '').replace(',', '.').trim();
-      //     return false; // Exit the loop once we find the matching element
-      //   }
-      // });
-
-      // let priceService = 0;
-      // let foundMandatoryServicesRates = false;
-
-      // $response('script').each((_, script) => {
-      //   const scriptContent = $(script).html();
-
-      //   if (scriptContent && scriptContent.includes("var mandatory_services_rates")) {
-      //     foundMandatoryServicesRates = true;
-      //   }
-
-      //   if (foundMandatoryServicesRates && scriptContent && scriptContent.includes("mandatory_services_rates[")) {
-      //     try {
-      //       const regex = /mandatory_services_rates\[\d+\]\s*=\s*(\[.*?\]);/s;
-      //       const match = scriptContent.match(regex);
-
-      //       if (match?.[1]) {
-      //         const servicesData = JSON.parse(match[1]) as { price_service: number }[];
-      //         if (Array.isArray(servicesData) && servicesData.length > 0 && servicesData[0]) {
-      //           priceService = servicesData[0].price_service;
-      //         }
-      //       } else {
-      //         console.log('No match found or match[1] is undefined');
-      //       }
-      //     } catch (e) {
-      //       console.error("Error parsing price service data:", e);
-      //     }
-      //     return false; // Exit the loop once we find the matching script
-      //   }
-      // });
-
-      // let checkInTime: string | null = null;
-      // let checkOutTime: string | null = null;
-
-      // // Find the check-in time
-      // $response('.checkInOutBadge:contains("Check-in:")').each((_, element) => {
-      //   const text = $(element).text().trim();
-      //   const match = text.match(/Check-in:\s*(\d{2}:\d{2})/);
-      //   if (match?.[1]) {
-      //     checkInTime = match[1];
-      //     return false; // Exit the loop once we find the check-in time
-      //   }
-      // });
-
-      // // Find the check-out time
-      // $response('.checkInOutBadge:contains("Check-out:")').each((_, element) => {
-      //   const text = $(element).text().trim();
-      //   const match = text.match(/Check-out:\s*(\d{2}:\d{2})/);
-      //   if (match?.[1]) {
-      //     checkOutTime = match[1];
-      //     return false; // Exit the loop once we find the check-out time
-      //   }
-      // });
-
-      // let totalBeds = 0;
-
-      // $response('.apt-amenity').each((_, element) => {
-      //   const amenity = $(element).text().trim();
-      //   const bedMatches = amenity.match(/(\d+)\s*x\s*/i);
-
-      //   if (bedMatches) {
-      //     totalBeds += parseInt(bedMatches[1]!);
-      //     // bedMatches.forEach((match) => {
-      //     //     const [count] = match.split('x').map(part => parseInt(part.trim()));
-      //     //     if (!isNaN(count)) {
-      //     //         numBeds += count!;
-      //     //     }
-      //     // });
-      //   }
-      //   return false;
-      // });
-
-      const price = extractPrice($response);
-      const cleaningFee = extractCleaningFee($response);
-      const { checkInTime, checkOutTime } =
-        extractCheckInCheckOutTime($response);
-      const extractedPropertyType = $response(".apt-address span")
-        .text()
-        .trim();
-      const propertyType = mapPropertyType(
-        await translateText(extractedPropertyType),
-      );
-      const totalBeds = extractTotalBeds($response);
-
-      // This is not working rn - also only fixes a few cases so might not be worth it
-      const normalizeString = (str: string) => {
-        if (str) {
-          return str
-            .normalize("NFKC")
-            .replace(/\u00a0/g, " ")
-            .trim();
         }
-      };
+        return null;
+      })
+      .get() as JsonLdData[];
 
-      const property = properties.filter(
-        (property) => property.url.split("?")[0] === scrapedData.url,
-      )[0]
-        ? properties.filter(
-            (property) => property.url.split("?")[0] === scrapedData.url,
-          )[0]
-        : properties.filter(
-            (property) =>
-              normalizeString(property.name) ===
-              normalizeString(scrapedData.name),
-          )[0];
-
-      if (property === undefined) {
-        if (info.length > 0) {
-          // This is the case that isnt hit very frequently
-          console.log(
-            "GGGGGGGGGGGG; ",
-            scrapedData.name,
-            validData[i]?.url,
-            validData[i]?.name,
-          );
-        } else {
-          console.log("Property not found:", info, validData[i]?.url, response);
-        }
-        continue;
-      }
-      let amenities;
-
-      const name = await translateText(scrapedData.name);
-      const about = await translateText(scrapedData.description);
-      const address = property.address;
-      const city = property.city;
-      const latitude = scrapedData.latitude;
-      const longitude = scrapedData.longitude;
-      const maxNumGuests = scrapedData.containsPlace.occupancy.value;
-      const numBeds = totalBeds;
-      const numBedrooms = property.maxBedrooms;
-      const numBathrooms = property.maxBathrooms;
-      try {
-        if (
-          scrapedData.amenityFeature &&
-          scrapedData.amenityFeature.length > 0
-        ) {
-          amenities = await Promise.all(
-            scrapedData.amenityFeature.map(async (amenity) =>
-              translateText(amenity.name),
-            ),
-          );
-        } else {
-          amenities = [];
-        }
-      } catch (error) {
-        console.error("Error translating amenities:", error);
-      }
-      const imageUrls = scrapedData.image;
-      const originalListingUrl = property.url;
-      const originalNightlyPrice =
-        parseFloat(price) + cleaningFee / getNumNights(checkIn, checkOut);
-
-      res.push({
-        name,
-        about,
-        address,
-        city,
-        latitude,
-        longitude,
-        maxNumGuests,
-        numBeds,
-        numBedrooms,
-        numBathrooms,
-        amenities,
-        imageUrls,
-        originalListingUrl,
-        originalNightlyPrice,
-        propertyType,
-        checkInTime,
-        checkOutTime,
-        currency: "EUR",
-        cancellationPolicy: "Non-refundable",
-      });
+    // Grab the specific JSON-LD data object we need
+    const info = jsonLdData.filter(
+      (data) =>
+        data.hasOwnProperty("@type") && data["@type"] === "VacationRental",
+    );
+    if (info[0] === undefined) {
+      // This hits a lot, 429 errors
+      console.log("No valid data found:", jsonLdData);
+      continue;
     }
+    const scrapedData = info[0] as unknown as ScrapedData;
+
+    //const avgRating
+    //const numRatings
+
+    // let price = '';
+
+    // $response('div.min-price').each((_, element) => {
+    //   const infoButton = $(element).find('a.infobutton');
+    //   const dataContent = infoButton.attr('data-content');
+
+    //   if (dataContent && dataContent === "Cancellazione non consentita") {
+    //     const priceText = $(element).find('div.avgTotPriceRow').text().trim();
+    //     price = priceText.replace('€', '').replace(',', '.').trim();
+    //     return false; // Exit the loop once we find the matching element
+    //   }
+    // });
+
+    // let priceService = 0;
+    // let foundMandatoryServicesRates = false;
+
+    // $response('script').each((_, script) => {
+    //   const scriptContent = $(script).html();
+
+    //   if (scriptContent && scriptContent.includes("var mandatory_services_rates")) {
+    //     foundMandatoryServicesRates = true;
+    //   }
+
+    //   if (foundMandatoryServicesRates && scriptContent && scriptContent.includes("mandatory_services_rates[")) {
+    //     try {
+    //       const regex = /mandatory_services_rates\[\d+\]\s*=\s*(\[.*?\]);/s;
+    //       const match = scriptContent.match(regex);
+
+    //       if (match?.[1]) {
+    //         const servicesData = JSON.parse(match[1]) as { price_service: number }[];
+    //         if (Array.isArray(servicesData) && servicesData.length > 0 && servicesData[0]) {
+    //           priceService = servicesData[0].price_service;
+    //         }
+    //       } else {
+    //         console.log('No match found or match[1] is undefined');
+    //       }
+    //     } catch (e) {
+    //       console.error("Error parsing price service data:", e);
+    //     }
+    //     return false; // Exit the loop once we find the matching script
+    //   }
+    // });
+
+    // let checkInTime: string | null = null;
+    // let checkOutTime: string | null = null;
+
+    // // Find the check-in time
+    // $response('.checkInOutBadge:contains("Check-in:")').each((_, element) => {
+    //   const text = $(element).text().trim();
+    //   const match = text.match(/Check-in:\s*(\d{2}:\d{2})/);
+    //   if (match?.[1]) {
+    //     checkInTime = match[1];
+    //     return false; // Exit the loop once we find the check-in time
+    //   }
+    // });
+
+    // // Find the check-out time
+    // $response('.checkInOutBadge:contains("Check-out:")').each((_, element) => {
+    //   const text = $(element).text().trim();
+    //   const match = text.match(/Check-out:\s*(\d{2}:\d{2})/);
+    //   if (match?.[1]) {
+    //     checkOutTime = match[1];
+    //     return false; // Exit the loop once we find the check-out time
+    //   }
+    // });
+
+    // let totalBeds = 0;
+
+    // $response('.apt-amenity').each((_, element) => {
+    //   const amenity = $(element).text().trim();
+    //   const bedMatches = amenity.match(/(\d+)\s*x\s*/i);
+
+    //   if (bedMatches) {
+    //     totalBeds += parseInt(bedMatches[1]!);
+    //     // bedMatches.forEach((match) => {
+    //     //     const [count] = match.split('x').map(part => parseInt(part.trim()));
+    //     //     if (!isNaN(count)) {
+    //     //         numBeds += count!;
+    //     //     }
+    //     // });
+    //   }
+    //   return false;
+    // });
+
+    const price = extractPrice($response);
+    const cleaningFee = extractCleaningFee($response);
+    const { checkInTime, checkOutTime } = extractCheckInCheckOutTime($response);
+    const extractedPropertyType = $response(".apt-address span").text().trim();
+    const propertyType = mapPropertyType(
+      await translateText(extractedPropertyType),
+    );
+    const totalBeds = extractTotalBeds($response);
+
+    // This is not working rn - also only fixes a few cases so might not be worth it
+    const normalizeString = (str: string) => {
+      if (str) {
+        return str
+          .normalize("NFKC")
+          .replace(/\u00a0/g, " ")
+          .trim();
+      }
+    };
+
+    const property = properties.filter(
+      (property) => property.url.split("?")[0] === scrapedData.url,
+    )[0]
+      ? properties.filter(
+          (property) => property.url.split("?")[0] === scrapedData.url,
+        )[0]
+      : properties.filter(
+          (property) =>
+            normalizeString(property.name) ===
+            normalizeString(scrapedData.name),
+        )[0];
+
+    if (property === undefined) {
+      if (info.length > 0) {
+        // This is the case that isnt hit very frequently
+        console.log("GGGGGGGGGGGG; ", scrapedData.name, url, response);
+      } else {
+        console.log("Property not found:", info, url, response);
+      }
+      continue;
+    }
+    let amenities;
+
+    const name = await translateText(scrapedData.name);
+    const about = await translateText(scrapedData.description);
+    const address = property.address;
+    const city = property.city;
+    const latitude = scrapedData.latitude;
+    const longitude = scrapedData.longitude;
+    const maxNumGuests = scrapedData.containsPlace.occupancy.value;
+    const numBeds = totalBeds;
+    const numBedrooms = property.maxBedrooms;
+    const numBathrooms = property.maxBathrooms;
+    try {
+      if (scrapedData.amenityFeature && scrapedData.amenityFeature.length > 0) {
+        amenities = await Promise.all(
+          scrapedData.amenityFeature.map(async (amenity) =>
+            translateText(amenity.name),
+          ),
+        );
+      } else {
+        amenities = [];
+      }
+    } catch (error) {
+      console.error("Error translating amenities:", error);
+    }
+    const imageUrls = scrapedData.image;
+    const originalListingUrl = property.url;
+    const originalNightlyPrice =
+      parseFloat(price) + cleaningFee / getNumNights(checkIn, checkOut);
+
+    res.push({
+      name,
+      about,
+      address,
+      city,
+      latitude,
+      longitude,
+      maxNumGuests,
+      numBeds,
+      numBedrooms,
+      numBathrooms,
+      amenities,
+      imageUrls,
+      originalListingUrl,
+      originalNightlyPrice,
+      propertyType,
+      checkInTime,
+      checkOutTime,
+      currency: "EUR",
+      cancellationPolicy: "Non-refundable",
+      reviews: [],
+    });
   }
   return res;
 };
