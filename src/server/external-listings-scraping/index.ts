549f5a27d3fef6f269d1f70b3185c3c9adfd0494
Ben Tomlin <bentomlin101@gmail.com>
2024-09-01 18:21:58 -0700

    delete externallistings stuff

--- src/server/external-listings-scraping/index.ts
+++ src/server/external-listings-scraping/index.ts
@@ -1,24 +1,18 @@
 import { sortBy } from "lodash";
 import { db } from "../db";
-import {
-  ExternalListing,
-  externalListings,
-  MinimalRequest,
-} from "../db/schema";
-import { scrapeAirbnbListings } from "./scrapeAirbnbListings";
+import { MinimalRequest, NewProperty, offers, properties } from "../db/schema";
 import { getNumNights } from "@/utils/utils";
+import { HOST_MARKUP, TRAVELER__MARKUP } from "@/utils/constants";
+
+// maximum discount we want to give, we don't want to go too low
+const MAX_DISCOUNT = 0.3;
 
 export type ExternalListingScraper = (
   request: MinimalRequest,
-) => Promise<ExternalListing[]>;
+) => Promise<{ property: NewProperty; totalPrice: number }[]>;
 
-const listingScrapers: ExternalListingScraper[] = [scrapeAirbnbListings];
+const listingScrapers: ExternalListingScraper[] = [];
 
-/**
- * Scrapes external listings for a request so travelers can see what kind of prices
- * are already out there -- but they won't be booking these properties, hence the new
- * ExternalListing type/table
- */
 export async function scrapeExternalListings(request: MinimalRequest) {
   const numNights = getNumNights(request.checkIn, request.checkOut);
   const requstedNightlyPrice = request.maxTotalPrice / numNights;
@@ -27,12 +21,30 @@ export async function scrapeExternalListings(request: MinimalRequest) {
     listingScrapers.map((scrapeListings) => scrapeListings(request)),
   )
     .then((r) => r.flat())
-    .then((listings) =>
-      sortBy(listings, (l) => Math.abs(l.nightlyPrice - requstedNightlyPrice)),
-    )
+    .then((listings) => {
+      const filtered = listings.filter(
+        (l) => l.totalPrice >= requstedNightlyPrice * (1 - MAX_DISCOUNT),
+      );
+
+      return sortBy(filtered, (p) => p.totalPrice);
+    })
     .then((listings) => listings.slice(0, 10));
 
-  await db
-    .insert(externalListings)
-    .values(listings.map((l) => ({ ...l, requestId: request.id })));
+  const propertyIds = await db
+    .insert(properties)
+    .values(listings.map(({ property }) => property))
+    .returning({ id: properties.id })
+    .then((res) => res.map((p) => p.id));
+
+  await db.insert(offers).values(
+    listings.map(({ totalPrice }, index) => ({
+      propertyId: propertyIds[index]!,
+      requestId: request.id,
+      totalPrice,
+      checkIn: request.checkIn,
+      checkOut: request.checkOut,
+      hostPayout: totalPrice * HOST_MARKUP,
+      travelerOfferedPrice: totalPrice * TRAVELER__MARKUP,
+    })),
+  );
 }
