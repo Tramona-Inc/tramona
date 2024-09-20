d10731cadd746dea3ff6d27ba162fcb2c87253fb
Ben Tomlin <bentomlin101@gmail.com>
2024-09-20 13:25:50 -0700

    pass check in/out

--- src/server/external-listings-scraping/airbnbScraper.ts
+++ src/server/external-listings-scraping/airbnbScraper.ts
@@ -59,8 +59,10 @@ export const airbnbScraper: DirectSiteScraper = async ({
   return await Promise.all(
     sortedListings.map(async ({ originalListingId }) => {
       try {
-        const { property, reviews, nightlyPrice } =
-          await scrapeAirbnbListing(originalListingId);
+        const { property, reviews, nightlyPrice } = await scrapeAirbnbListing(
+          originalListingId,
+          { checkIn, checkOut, numGuests },
+        );
 
         const completeProperty: NewProperty = {
           ...property,
