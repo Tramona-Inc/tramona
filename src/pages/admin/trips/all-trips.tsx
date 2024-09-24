c9ca7b536d8227aa58e8967c7e3376dbbc6773e1
Neal Uehara <ueharaneal@gmail.com>
2024-09-24 14:53:21 -0700

    Fixed dumb superhog error

--- src/pages/admin/trips/all-trips.tsx
+++ src/pages/admin/trips/all-trips.tsx
@@ -12,7 +12,7 @@ export default function Page() {
     <DashboardLayout>
       <div className="items-around mx-20 my-8 flex w-5/6 flex-col justify-center gap-y-4">
         <h1 className="mx-10 text-3xl font-bold">Trips</h1>
-        <Tabs>
+        <Tabs defaultValue="current-trips">
           <TabsList>
             <TabsTrigger value="current-trips">Current Trips</TabsTrigger>
             <TabsTrigger value="upcoming-trips">Upcoming Trips</TabsTrigger>
