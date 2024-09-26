9033cc94c5d79b55953c9c9ec01b0d412d8628b4
adamkim1006 <adamkim1006@gmail.com>
2024-09-26 14:26:22 -0700

    email fix

--- packages/transactional/emails/HostTeamInviteEmail.tsx
+++ packages/transactional/emails/HostTeamInviteEmail.tsx
@@ -1,5 +1,6 @@
 import { Text } from "@react-email/components";
 import { Layout } from "./EmailComponents";
+import Link from "next/link";
 
 export default function HostTeamInviteEmail({
   email,
@@ -8,13 +9,17 @@ export default function HostTeamInviteEmail({
   email: string;
   name: string | null;
 }) {
+  const isProduction = process.env.NODE_ENV === "production";
+  const baseUrl = isProduction
+    ? "https://www.tramona.com"
+    : "http://localhost:3000";
   return (
     <Layout title_preview="Tramona hostTeam invitation">
       <div className="pt-2" style={{ textAlign: "center" }}>
         <div className="text-brand px-6 text-left text-base">
           <Text className="text-brand text-left">
             Hello, {name ?? email} invited you to their host team on Tramona!
-            Sign up at https://tramona.com/auth/signup with this email to be
+            Sign up at <Link href={`${baseUrl}/auth/signup`}>https://tramona.com/auth/signup</Link> with this email to be
             added to the team.
           </Text>
         </div>
