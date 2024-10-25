55b5b2dc2d0c0917af74d37a854a2e88e8e2c4c0
rockuutree <vurya22@gmail.com>
2024-10-25 12:57:01 -0700

    Text

--- src/components/landing-page/search/MobileSearchDialog.tsx
+++ src/components/landing-page/search/MobileSearchDialog.tsx
@@ -75,7 +75,9 @@ export default function MobileSearchDialog({
       >
         <Search className="mr-3 h-5 w-5 text-gray-400" />
         <span className="text-gray-500">
-          {activeOption === "price" ? "Name your price" : "Search properties"}
+          {activeOption === "price"
+            ? "Name your own price"
+            : "Search properties"}
         </span>
       </div>
 
