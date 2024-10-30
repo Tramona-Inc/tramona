9218b244bdd7a535fcae8fe70cf9a2149d3efa04
rockuutree <vurya22@gmail.com>
2024-10-29 20:51:42 -0700

    Host dialog and tabs

--- src/components/dashboard/host/HostRequestsLayout.tsx
+++ src/components/dashboard/host/HostRequestsLayout.tsx
@@ -10,13 +10,13 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { SkeletonText } from "@/components/ui/skeleton";
 import { api } from "@/utils/api";
 import { range } from "lodash";
-import { HandshakeIcon, MapPinIcon } from "lucide-react";
+import { HandshakeIcon } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { type SeparatedData } from "@/server/server-utils";
 import { separateByPriceRestriction, plural } from "@/utils/utils";
 import { useRouter } from "next/router";
-import { HostRequestsPageData } from "@/server/api/routers/propertiesRouter";
+import { type HostRequestsPageData } from "@/server/api/routers/propertiesRouter";
 
 export default function HostRequestsLayout({
   children,
@@ -28,7 +28,8 @@ export default function HostRequestsLayout({
     "normal" | "outsidePriceRestriction"
   >("normal");
   const [selectedCity, setSelectedCity] = useState<string | null>(null);
-  const [isDataLoading, setIsDataLoading] = useState(true); // New state to track data loading
+  const [isDataLoading, setIsDataLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState<"city" | "book">("city");
   const router = useRouter();
 
   const { data: fetchedProperties, isLoading } =
@@ -37,54 +38,61 @@ export default function HostRequestsLayout({
         const separatedProperties =
           separateByPriceRestriction(fetchedProperties);
         setSeparatedData(separatedProperties);
-        setIsDataLoading(false); // Set loading state to false when data is fetched
+        setIsDataLoading(false);
       },
     });
 
   useEffect(() => {
     if (isLoading) {
-      setIsDataLoading(true); // If API starts loading again, mark the data loading as true
+      setIsDataLoading(true);
     }
   }, [isLoading]);
 
   const displayedData = separatedData ? separatedData[selectedOption] : [];
 
   return (
-    <div className="flex">
+    <div className="flex bg-white">
       <div className="sticky top-20 h-screen-minus-header-n-footer w-full overflow-auto border-r px-4 py-8 xl:w-96">
         <ScrollArea>
           <div className="pb-4">
             <h1 className="text-3xl font-bold">Requests</h1>
-            <div className="mt-4 flex flex-row gap-2">
-              <Button
-                variant={selectedOption === "normal" ? "primary" : "secondary"}
-                className="rounded-full"
-                onClick={() => setSelectedOption("normal")}
-              >
-                Normal
-              </Button>
-              {separatedData?.outsidePriceRestriction &&
-                separatedData.outsidePriceRestriction.length > 0 && (
-                  <Button
-                    variant={
-                      selectedOption === "outsidePriceRestriction"
-                        ? "primary"
-                        : "secondary"
-                    }
-                    className="rounded-full"
-                    onClick={() => setSelectedOption("outsidePriceRestriction")}
-                  >
-                    Outside Price Restriction
-                  </Button>
-                )}
+
+            {/* Tab Navigation */}
+            <div className="mt-6 border-b">
+              <div className="flex w-full">
+                <button
+                  onClick={() => setActiveTab("city")}
+                  className={`text-md relative flex-1 pb-4 font-medium transition-colors ${
+                    activeTab === "city"
+                      ? "text-foreground"
+                      : "text-muted-foreground hover:text-foreground"
+                  }`}
+                >
+                  City Requests
+                  {activeTab === "city" && (
+                    <span className="absolute bottom-0 left-0 right-0 h-1 bg-primaryGreen" />
+                  )}
+                </button>
+                <button
+                  onClick={() => setActiveTab("book")}
+                  className={`text-md relative flex-1 pb-4 font-medium transition-colors ${
+                    activeTab === "book"
+                      ? "text-foreground"
+                      : "text-muted-foreground hover:text-foreground"
+                  }`}
+                >
+                  Requests to book
+                  {activeTab === "book" && (
+                    <span className="absolute bottom-0 left-0 right-0 h-1 bg-primaryGreen" />
+                  )}
+                </button>
+              </div>
             </div>
           </div>
           <div className="pt-4">
             {isDataLoading ? (
-              // Show skeletons while loading
               range(10).map((i) => <SidebarPropertySkeleton key={i} />)
             ) : displayedData.length > 0 ? (
-              // Show the list of cities if data is available
               displayedData.map((cityData) => (
                 <SidebarCity
                   key={cityData.city}
@@ -95,7 +103,6 @@ export default function HostRequestsLayout({
                 />
               ))
             ) : (
-              // Show the empty state only when not loading and no data is available
               <EmptyState
                 icon={HandshakeIcon}
                 className="h-[calc(100vh-280px)]"
@@ -114,16 +121,54 @@ export default function HostRequestsLayout({
           </div>
         </ScrollArea>
       </div>
-      <div className="hidden flex-1 xl:block">
+      <div className="hidden flex-1 bg-[#fafafa] xl:block">
         {children ? (
-          <div className="px-4 pb-32 pt-8">
-            <div className="mx-auto max-w-5xl">{children}</div>
+          <div className="pb-30 px-4 pt-8">
+            <div className="mx-auto max-w-5xl">
+              <div className="mb-4 flex flex-row gap-2">
+                <Button
+                  variant={
+                    activeTab === "city" && selectedOption === "normal"
+                      ? "primary"
+                      : "white"
+                  }
+                  className="rounded-full shadow-md"
+                  onClick={() => {
+                    setSelectedOption("normal");
+                    setActiveTab("city");
+                  }}
+                >
+                  City Request
+                </Button>
+                <Button
+                  variant={
+                    activeTab === "book" ||
+                    selectedOption === "outsidePriceRestriction"
+                      ? "primary"
+                      : "white"
+                  }
+                  className="rounded-full shadow-md"
+                  onClick={() => {
+                    setSelectedOption("outsidePriceRestriction");
+                    setActiveTab("book");
+                  }}
+                >
+                  Request to Book
+                </Button>
+              </div>
+              {children}
+            </div>
           </div>
         ) : (
-          <div className="grid h-screen-minus-header flex-1 place-items-center">
-            <p className="font-medium text-muted-foreground">
-              Select a city to view its requests
+          <div className="flex h-[calc(100vh-280px)] flex-col items-center justify-center gap-2">
+            <h2 className="text-xl font-semibold">No requests found</h2>
+            <p className="text-center text-muted-foreground">
+              Consider loosen requirements or allow for more ways to book to see
+              more requests.
             </p>
+            <Button variant="secondary" className="mt-4">
+              Change requirements
+            </Button>
           </div>
         )}
       </div>
@@ -148,18 +193,22 @@ function SidebarCity({
       : `/host/requests/${cityData.city}?priceRestriction=true`;
 
   const isSelected = selectedCity === cityData.city;
+
   return (
-    <Link href={href} className="mb-4 block">
+    <Link href={href} className="block">
       <div
-        className={`flex items-center gap-2 rounded-lg p-4 hover:bg-muted ${
-          isSelected ? "bg-muted" : ""
+        className={`flex items-center justify-between rounded-xl p-4 ${
+          isSelected ? "bg-primaryGreen text-white" : ""
         }`}
         onClick={() => setSelectedCity(cityData.city)}
       >
-        <MapPinIcon className="h-8 w-8 text-gray-600" />
-        <div className="flex-1">
-          <h3 className="font-semibold">{cityData.city}</h3>
-          <Badge size="md">{plural(cityData.requests.length, "request")}</Badge>
+        <div>
+          <h3 className="font-medium">{cityData.city}</h3>
+        </div>
+        <div
+          className={`text-sm ${isSelected ? "text-white" : "text-muted-foreground"}`}
+        >
+          {cityData.requests.length} requests
         </div>
       </div>
     </Link>
