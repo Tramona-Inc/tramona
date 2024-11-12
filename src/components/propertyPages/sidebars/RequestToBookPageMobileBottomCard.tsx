23c779b6bebf0cb76c8c5bd06154b37a55167c98
Neal Uehara <ueharaneal@gmail.com>
2024-11-12 09:50:52 -0800

    completed mobile price card

--- src/components/propertyPages/sidebars/RequestToBookPageMobileBottomCard.tsx
+++ src/components/propertyPages/sidebars/RequestToBookPageMobileBottomCard.tsx
@@ -3,8 +3,11 @@ import { Card, CardContent } from "@/components/ui/card";
 import PriceDetailsBeforeTax from "@/components/_common/PriceDetailsBeforeTax";
 import { formatDateRange } from "@/utils/utils";
 import ReserveBtn, { PropertyPageData } from "./actionButtons/RequestToBookBtn";
-import type { RequestToBookDetails } from "./actionButtons/RequestToBookBtn";
+import RequestToBookOrBookNowPriceCard from "./priceCards/RequestToBookOrBookNowPriceCard";
+import BookNowBtn from "./actionButtons/BookNowBtn";
 import { useRouter } from "next/router";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 export default function RequestToBookPageMobileBottomCard({
   property,
 }: {
@@ -38,7 +41,7 @@ export default function RequestToBookPageMobileBottomCard({
     api.users.myVerificationStatus.useQuery();
 
   return (
-    <Card className="fixed bottom-16 left-0 w-full md:hidden">
+    <Card className="fixed bottom-16 left-0 w-full border-t md:hidden">
       <CardContent className="flex flex-row items-center justify-between px-4 py-1 text-sm">
         <div className="flex basis-1/2 flex-col">
           <PriceDetailsBeforeTax
@@ -49,12 +52,24 @@ export default function RequestToBookPageMobileBottomCard({
             {formatDateRange(requestToBook.checkIn, requestToBook.checkOut)}
           </p>
         </div>
-        <div className="flex-1">
-          <ReserveBtn
-            btnSize="sm"
-            requestToBook={requestToBook}
-            property={property}
-          />
+        <div className="flex flex-row gap-x-2">
+          <Dialog>
+            <DialogTrigger>
+              <Button variant="outline" size="sm" className="tracking-tight">
+                Request To Book
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <RequestToBookOrBookNowPriceCard property={property} />
+            </DialogContent>
+          </Dialog>
+          {!property.bookItNowEnabled && (
+            <BookNowBtn
+              btnSize="sm"
+              property={property}
+              requestToBook={requestToBook}
+            />
+          )}
           {verificationStatus?.isIdentityVerified === "false" &&
             property.stripeVerRequired === true && (
               <p className="text-center text-xs font-semibold text-red-500">
