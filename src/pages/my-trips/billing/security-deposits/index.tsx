235c8b09ac4cf414fdd3585b374ca15b9489f658
Neal Uehara <ueharaneal@gmail.com>
2024-11-22 14:40:51 -0800

    fixed build errors

--- src/pages/my-trips/billing/security-deposits/index.tsx
+++ src/pages/my-trips/billing/security-deposits/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import SecurityDepositOverview from "@/components/my-trips/billing/travelerClaims/SecurityDepositOverview";
 import DashboardLayout from "@/components/_common/Layout/DashboardLayout";
-import { useRouter } from "next/router";
 
 function Page() {
-  const router = useRouter();
   return (
     <DashboardLayout>
       <SecurityDepositOverview />
