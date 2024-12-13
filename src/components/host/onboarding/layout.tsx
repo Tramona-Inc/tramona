6084b027f3959d238aee9bda050bd5b62e6dda92
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-12-13 14:50:21 -0800

    changed host onboarding header

--- src/components/host/onboarding/layout.tsx
+++ src/components/host/onboarding/layout.tsx
@@ -1,4 +1,6 @@
 import { Header } from "@/components/_common/Layout/header/Header";
+import { StickyTopBar } from "@/pages/for-hosts";
+import { MobileStickyBar } from "@/pages/for-hosts";
 import { useSession } from "next-auth/react";
 
 export default function OnboardingLayout({
@@ -10,8 +12,14 @@ export default function OnboardingLayout({
 
   return (
     <>
-      <Header />
-      <div className="flex min-h-screen-minus-header flex-col">{children}</div>
+      {/* <Header /> */}
+      <div className="md:hidden">
+        <MobileStickyBar />
+      </div>
+      <div className="hidden md:block">
+        <StickyTopBar />
+      </div>
+      <div className="min-h-screen-minus-header flex flex-col">{children}</div>
     </>
   );
 }
