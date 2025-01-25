93c3fc77f1f1430af8f14ea2c6554f7ea1eed0b8
Neal Uehara <ueharaneal@gmail.com>
2025-01-24 17:30:01 -0800

    fixed edit button

--- src/components/dashboard/host/HostPropertiesLayout.tsx
+++ src/components/dashboard/host/HostPropertiesLayout.tsx
@@ -298,7 +298,7 @@ export function HostPropertyEditBtn({
   };
 
   return (
-    <div className="z-40 block space-x-2 sm:absolute sm:right-1 sm:top-1">
+    <div className="z-40 block space-x-2 sm:absolute sm:right-3 sm:top-1">
       {editing ? (
         <>
           <Button
