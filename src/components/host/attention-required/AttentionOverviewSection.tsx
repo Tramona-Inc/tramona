42c66ecc170ea3e449972649557f55a52fd0860a
Neal Uehara <ueharaneal@gmail.com>
2025-01-25 16:23:32 -0800

    fixed roles

--- src/components/host/attention-required/AttentionOverviewSection.tsx
+++ src/components/host/attention-required/AttentionOverviewSection.tsx
@@ -17,28 +17,31 @@ export default function AttentionOverviewSection({
       { enabled: !!currentHostTeamId },
     );
 
+  // Render null only after loading is complete
+  if (!isLoading && (!allNotifications || allNotifications.length === 0)) {
+    return null;
+  }
+
   return (
     <div className="grid grid-cols-2 gap-4 px-4 sm:px-0 lg:grid-cols-4">
       {/* Notification Cards */}
       {!isLoading ? (
-        allNotifications && allNotifications.length !== 0 ? (
-          <>
-            <NotificationCard
-              action="Sync Calendar for"
-              title={allNotifications[0]!.name}
-              href={`/host/calendar?propertyId=${allNotifications[0]!.id}`}
-              className="col-span-1 sm:col-span-1"
-              length={allNotifications.length}
-            />
-            <NotificationCard
-              action="Enable Book-it-now for"
-              title={allNotifications[0]!.name}
-              href={`/host/calendar?propertyId=${allNotifications[0]!.id}`}
-              className="col-span-1"
-              length={allNotifications.length}
-            />
-          </>
-        ) : null
+        <>
+          <NotificationCard
+            action="Sync Calendar for"
+            title={allNotifications[0]!.name}
+            href={`/host/calendar?propertyId=${allNotifications[0]!.id}`}
+            className="col-span-1 sm:col-span-1"
+            length={allNotifications.length}
+          />
+          <NotificationCard
+            action="Enable Book-it-now for"
+            title={allNotifications[0]!.name}
+            href={`/host/calendar?propertyId=${allNotifications[0]!.id}`}
+            className="col-span-1"
+            length={allNotifications.length}
+          />
+        </>
       ) : (
         <>
           <NotificationCardSkeleton className="col-span-full sm:col-span-1" />
@@ -47,7 +50,7 @@ export default function AttentionOverviewSection({
       )}
 
       {/* Attention Cards */}
-      <AttentionCard
+      {/* <AttentionCard
         icon={ZapIcon}
         title="Edit Auto Pricing"
         description="Optimize pricing for higher booking chances."
@@ -62,7 +65,7 @@ export default function AttentionOverviewSection({
         subtitle="Allow guests to book automatically or require approval."
         href="/host/calendar"
         className="col-span-full w-full sm:col-span-1 md:row-start-2"
-      />
+      /> */}
     </div>
   );
 }
