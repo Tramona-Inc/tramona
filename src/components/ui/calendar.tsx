d2d52386bfac8ae3978e5607ee25a5d101c346b6
Neal Uehara <ueharaneal@gmail.com>
2025-01-27 12:10:55 -0800

    fixed the color and the error message

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,12 +19,12 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-1 px-1", className)}
+      className={cn("bg-white px-1 text-white", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-2 relative items-center",
-        caption_label: "text-sm font-semibold text-primaryGreen",
+        caption_label: "text-base font-semibold text-primaryGreen",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "ghost", className: "rounded-full" }),
@@ -34,23 +34,24 @@ function Calendar({
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell: "text-primaryGreen rounded-md w-10 text-sm font-semibold",
-        row: "flex w-full mt-1",
+        head_cell:
+          "text-primaryGreen rounded-md w-10 text-sm font-semibold uppercase",
+        row: "flex w-full mt-1 ",
         cell: "h-10 w-10 text-center  text-sm p-0 relative [&:has([aria-selected].day-range-start)]:rounded-l-md [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-10 w-10 p-0 l aria-selected:opacity-100 font-semibold",
         ),
-        day_range_start: "day-range-start text-primary-foreground",
-        day_range_end: "day-range-end !text-primary-foreground",
+        day_range_start: "day-range-start text-primaryGreen",
+        day_range_end: "day-range-end text-primaryGreen",
         day_selected:
           "bg-primaryGreen text-white hover:bg-primaryGreen [&>*]:rounded-none hover:text-primary-foreground focus:bg-primaryGreen focus:text-white",
-        day_today: highlightToday ? "bg-accent text-accent-foreground" : "",
+        day_today: highlightToday ? "bg-accent text-primaryGreen" : "",
         // day_outside:
         // 	'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
-        day_disabled: "text-muted-foreground opacity-80",
+        day_disabled: "text-muted-foreground opacity-80 text-white",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "aria-selected:bg-accent aria-selected:text-primaryGreen",
         day_hidden: "invisible",
         ...classNames,
       }}
